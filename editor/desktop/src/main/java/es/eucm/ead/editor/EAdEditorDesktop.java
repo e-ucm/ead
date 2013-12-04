927ca7c654b825a24fb9db80c68295190821e0cc
anserran <angelserranolaguna@gmail.com>
2013-12-04 16:51:12 +0100

    Editor is created with the same window size as the screen

--- editor/desktop/src/main/java/es/eucm/ead/editor/EAdEditorDesktop.java
+++ editor/desktop/src/main/java/es/eucm/ead/editor/EAdEditorDesktop.java
@@ -40,12 +40,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import es.eucm.ead.core.EditorEngine;
 
+import java.awt.*;
+
 public class EAdEditorDesktop {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
-		config.width = 800;
-		config.height = 600;
+		config.width = (int) Toolkit.getDefaultToolkit().getScreenSize()
+				.getWidth();
+		config.height = (int) Toolkit.getDefaultToolkit().getScreenSize()
+				.getHeight();
 
 		EditorEngine.debug = false;
 		for (String arg : args) {
