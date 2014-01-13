e5567607745110f7c3d4031d99e3af47775a0fe9
anserran <angelserranolaguna@gmail.com>
2014-01-13 16:36:26 +0100

    Added infrastructure to test the engine

---
+++ engine/desktop/src/test/java/es/eucm/ead/engine/TestGame.java
@@ -0,0 +1,92 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2013 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import es.eucm.ead.engine.application.TestApplication;
+import es.eucm.ead.schema.actions.Action;
+
+public class TestGame {
+
+	private Actor actor;
+
+	private TestApplication application;
+
+	public TestGame() {
+		Engine engine = new Engine("@testgame");
+		application = new TestApplication(engine, 800, 600);
+		application.start();
+	}
+
+	public void act() {
+		application.act();
+	}
+
+	/**
+	 * @return Returns a dummy actor, with position set to (0, 0), scale set to
+	 *         (1, 1), rotation set to 0, width and height set to 0 and color
+	 *         set to #FFFFFFFF
+	 */
+	public Actor getDummyActor() {
+		if (actor == null) {
+			actor = new Actor();
+			Engine.stage.addActor(actor);
+		}
+		return actor;
+	}
+
+	public void addActionToDummyActor(Action action) {
+		Actor actor = getDummyActor();
+		actor.addAction((com.badlogic.gdx.scenes.scene2d.Action) Engine.factory
+				.getEngineObject(action));
+	}
+
+	/**
+	 * Resets the dummy actor, setting its position to (0, 0), its scale to (1, 1),
+	 * its rotation to 0, its width and height to 0 and its color to #FFFFFFFF
+	 */
+	public void resetDummyActor() {
+		Actor actor = getDummyActor();
+		actor.setPosition(0, 0);
+		actor.setScale(1, 1);
+		actor.setRotation(0);
+		actor.setSize(0, 0);
+		actor.setColor(Color.WHITE);
+	}
+}
---
+++ engine/desktop/src/test/java/es/eucm/ead/engine/application/TestApplication.java
@@ -0,0 +1,229 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2013 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.application;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
+import com.badlogic.gdx.backends.lwjgl.LwjglPreferences;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Clipboard;
+import com.badlogic.gdx.utils.GdxNativesLoader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class is a no-GUI wrapper for the engine, intended for test
+ */
+public class TestApplication implements Application {
+
+	private ApplicationListener listener;
+	private Input input;
+	private Files files;
+	private Graphics graphics;
+	private Audio audio;
+
+	protected final Array<Runnable> runnables = new Array<Runnable>();
+
+	public TestApplication(ApplicationListener listener, int width, int height) {
+		this.listener = listener;
+		// Create stub objects
+		files = new LwjglFiles();
+		audio = new TestAudio();
+		input = new TestInput();
+		graphics = new TestGraphics(width, height);
+
+		Gdx.app = this;
+		Gdx.graphics = graphics;
+		Gdx.files = files;
+		Gdx.audio = audio;
+		Gdx.input = input;
+		Gdx.gl = graphics.getGLCommon();
+		Gdx.gl10 = graphics.getGL10();
+		Gdx.gl20 = graphics.getGL20();
+		GdxNativesLoader.load();
+	}
+
+	/**
+	 * Start the application
+	 */
+	public void start() {
+		listener.create();
+		listener.resize(graphics.getWidth(), graphics.getHeight());
+	}
+
+	/**
+	 * Make an update
+	 */
+	public void act() {
+		for (int i = 0; i < runnables.size; i++) {
+			runnables.get(i).run();
+		}
+		runnables.clear();
+		listener.render();
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener() {
+		return listener;
+	}
+
+	@Override
+	public Graphics getGraphics() {
+		return Gdx.graphics;
+	}
+
+	@Override
+	public Audio getAudio() {
+		return audio;
+	}
+
+	@Override
+	public Input getInput() {
+		return input;
+	}
+
+	@Override
+	public Files getFiles() {
+		return files;
+	}
+
+	@Override
+	public Net getNet() {
+		return null;
+	}
+
+	@Override
+	public void log(String tag, String message) {
+		System.out.println(tag + ": " + message);
+	}
+
+	@Override
+	public void log(String tag, String message, Throwable exception) {
+		log(tag, message);
+		exception.printStackTrace();
+	}
+
+	@Override
+	public void error(String tag, String message) {
+		log("[ERROR] " + tag, message);
+	}
+
+	@Override
+	public void error(String tag, String message, Throwable exception) {
+		log("[ERROR] " + tag, message, exception);
+	}
+
+	@Override
+	public void debug(String tag, String message) {
+		log("[DEBUG] " + tag, message);
+	}
+
+	@Override
+	public void debug(String tag, String message, Throwable exception) {
+		log("[DEBUG] " + tag, message, exception);
+	}
+
+	@Override
+	public void setLogLevel(int logLevel) {
+	}
+
+	@Override
+	public int getLogLevel() {
+		return 0;
+	}
+
+	@Override
+	public ApplicationType getType() {
+		return ApplicationType.Desktop;
+	}
+
+	@Override
+	public int getVersion() {
+		return 0;
+	}
+
+	@Override
+	public long getJavaHeap() {
+		return 0;
+	}
+
+	@Override
+	public long getNativeHeap() {
+		return 0;
+	}
+
+	Map<String, Preferences> preferences = new HashMap<String, Preferences>();
+
+	@Override
+	public Preferences getPreferences(String name) {
+		if (preferences.containsKey(name)) {
+			return preferences.get(name);
+		} else {
+			Preferences prefs = new LwjglPreferences(name,
+					new LwjglApplicationConfiguration().preferencesDirectory);
+			preferences.put(name, prefs);
+			return prefs;
+		}
+	}
+
+	@Override
+	public Clipboard getClipboard() {
+		return null;
+	}
+
+	@Override
+	public void postRunnable(Runnable runnable) {
+		synchronized (runnables) {
+			runnables.add(runnable);
+		}
+	}
+
+	@Override
+	public void exit() {
+	}
+
+	@Override
+	public void addLifecycleListener(LifecycleListener listener) {
+	}
+
+	@Override
+	public void removeLifecycleListener(LifecycleListener listener) {
+	}
+
+}
