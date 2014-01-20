c6d9313cb80317f9c5f2de7a46bb6d725face26b
RotaruDan <drotaru@ucm.es>
2014-01-20 22:01:44 +0100

    Refactoring and layouting: -> ProjectMenu Screen -> MenuButton

--- mockup/core/src/main/java/es/eucm/ead/mockup/core/view/ui/MenuButton.java
+++ mockup/core/src/main/java/es/eucm/ead/mockup/core/view/ui/MenuButton.java
@@ -41,17 +41,34 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
+/**
+ * A button displayed in the MainMenu and PanelMenu Screens.
+ */
 public class MenuButton extends Button {
+	
+	private final float PAD_TOP = 17f, 
+						PAD_LEFT = 17f, 
+						PAD_BOTTOM = 10f, 
+						PAD_RIGHT = 17f;
 
-	public MenuButton(String name, Skin skin, String string) {
+	public MenuButton(String name, Skin skin, String styleName) {
 		super(skin);
-		pad(17, 17, 10, 17);
-		Image sceneIcon = new Image(skin.getRegion(string));
+		setSize(getPrefWidth(), getPrefHeight());
+		initialize(name, skin, styleName);
+	}
+	
+	public MenuButton(String name, Skin skin, String styleName, float width, float height) {
+		super(skin);
+		setSize(width, height);
+		initialize(name, skin, styleName);
+	}
+	
+	private void initialize(String name, Skin skin, String styleName){
+		pad(PAD_TOP, PAD_LEFT, PAD_BOTTOM, PAD_RIGHT);
+		Image sceneIcon = new Image(skin.getRegion(styleName));
 		Label scene = new Label(name, skin);
 		add(sceneIcon).expand();
-		;
 		row();
 		add(scene);
-		pack();
 	}
 }
