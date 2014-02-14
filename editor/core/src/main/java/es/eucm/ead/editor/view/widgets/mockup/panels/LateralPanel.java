b0c1fcd32c2e141f03b5bd8dd4e67c8f0ec089b2
gorco <antcal01@ucm.es>
2014-02-14 14:21:51 +0100

    Added file headers

--- editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/panels/LateralPanel.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/panels/LateralPanel.java
@@ -1,3 +1,39 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2013 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package es.eucm.ead.editor.view.widgets.mockup.panels;
 
 import java.awt.Panel;
@@ -67,7 +103,7 @@ public class LateralPanel extends Table {
 	}
 
 	@Override
-	protected void drawBackground(Batch batch, float parentAlpha) {
+	protected void drawBackground(Batch batch, float parentAlpha, float x, float y) {
 		if (stageBackground != null) {
 			Color color = getColor();
 			batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
@@ -75,7 +111,7 @@ public class LateralPanel extends Table {
 			stageBackground.draw(batch, 0, 0, stage.getWidth(), stage.getHeight());
 
 		}
-		super.drawBackground(batch, parentAlpha);
+		super.drawBackground(batch, parentAlpha, x, y);
 	}
 
 	/**
