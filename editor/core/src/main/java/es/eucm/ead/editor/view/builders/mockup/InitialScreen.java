e9566d940a60122f8e63851cb86e4e9cee2ac77a
RotaruDan <drotaru@ucm.es>
2014-02-21 23:00:29 +0100

    Added BottomProjectMenuButton that extends MenuButton.

--- editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/InitialScreen.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/InitialScreen.java
@@ -68,7 +68,7 @@ public class InitialScreen implements ViewBuilder, PreferenceListener {
 	private static final String IC_NEWPROJECT = "ic_newproject",
 			IC_GALLERY = "ic_gallery";
 
-	private final FileHandle MOCKUP_PROJECT_FILE = Gdx.files
+	private static final FileHandle MOCKUP_PROJECT_FILE = Gdx.files
 			.external("/eAdventureMockup/");
 	private RecentProjects recents;
 	private Controller controller;
