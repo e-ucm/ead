d99adaaf1aca1788a988b2cd6ae8b57b74b33312
RotaruDan <drotaru@ucm.es>
2014-02-22 17:40:52 +0100

    Added headers to some files.

--- editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/camera/Picture.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/camera/Picture.java
@@ -1,17 +1,65 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2013 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package es.eucm.ead.editor.view.builders.mockup.camera;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.platform.mockup.DevicePictureControl;
 import es.eucm.ead.editor.view.builders.ViewBuilder;
 import es.eucm.ead.editor.view.widgets.mockup.Navigation;
+import es.eucm.ead.editor.view.widgets.mockup.buttons.IconButton;
 
 public class Picture implements ViewBuilder {
 
 	public static final String NAME = "mockup_picture";
-	
+
+	private static final float DEFAULT_PAD = 10f;
+
+	private Button takePicButton;
+
+	private DevicePictureControl pictureControl;
+
 	@Override
 	public String getName() {
 		return NAME;
@@ -20,23 +68,49 @@ public class Picture implements ViewBuilder {
 	@Override
 	public Actor build(Controller controller) {
 		Skin skin = controller.getEditorAssets().getSkin();
-		Table window = new Table(skin).debug();
+		pictureControl = controller.getPictureControl();
+
+		takePicButton = new IconButton(skin, "ic_photocamera");
+		takePicButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				takePic();
+			}
+		});
+
+		String[] res = { "800x600", "1280x720", "1920x1080", "4000x3000" };
+		SelectBox resolution = new SelectBox(res, skin);
+		resolution.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				// TODO change resolution here
+				super.clicked(event, x, y);
+			}
+		});
+
+		Table window = new Table(skin).debug().pad(DEFAULT_PAD);
 		window.setFillParent(true);
-		window.add(NAME);
+		window.add(resolution).right().top();
+		window.row();
+		window.add(takePicButton).bottom().expand().padBottom(5f);
 		window.addActor(new Navigation(controller, skin));
 		return window;
 	}
 
+	private void takePic() {
+		this.pictureControl.takePictureAsync();
+
+	}
+
 	@Override
 	public void initialize(Controller controller) {
-		// TODO Auto-generated method stub
-		
+		this.pictureControl.prepareCameraAsync();
 	}
 
 	@Override
 	public void release(Controller controller) {
-		// TODO Auto-generated method stub
-		
+		this.pictureControl.stopPreviewAsync();
+
 	}
 
 }
--- editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/buttons/Icon.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/buttons/Icon.java
@@ -1,3 +1,39 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2013 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package es.eucm.ead.editor.view.widgets.mockup.buttons;
 
 import com.badlogic.gdx.Gdx;
@@ -5,14 +41,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Scaling;
 
-
 /**
  * An Image used in the NavigationPanel.
  */
-public class Icon extends Image{
+public class Icon extends Image {
 
 	private final float prefWidth;
-	
+
 	/**
 	 * Creates a squared icon with a size of 0.075 * screen's width.
 	 */
@@ -21,13 +56,13 @@ public class Icon extends Image{
 		setScaling(Scaling.fit);
 		this.prefWidth = 0.075f;
 	}
-	
+
 	public Icon(Drawable drawable, float prefWidth) {
 		super(drawable);
 		setScaling(Scaling.fit);
 		this.prefWidth = prefWidth;
 	}
-	
+
 	@Override
 	public float getPrefWidth() {
 		return Gdx.graphics.getWidth() * this.prefWidth;
