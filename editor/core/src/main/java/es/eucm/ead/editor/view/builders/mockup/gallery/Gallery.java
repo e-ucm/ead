5270c78b41d1a6d27a0b12bf7bab69323b841e32
gorco <antcal01@ucm.es>
2014-02-26 21:39:11 +0100

    Added filter tags panel

--- editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/gallery/Gallery.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/gallery/Gallery.java
@@ -37,11 +37,16 @@
 package es.eucm.ead.editor.view.builders.mockup.gallery;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import es.eucm.ead.editor.control.Controller;
 import es.eucm.ead.editor.control.actions.ChangeView;
@@ -50,6 +55,7 @@ import es.eucm.ead.editor.view.builders.mockup.camera.Video;
 import es.eucm.ead.editor.view.widgets.GridLayout;
 import es.eucm.ead.editor.view.widgets.mockup.ToolBar;
 import es.eucm.ead.editor.view.widgets.mockup.buttons.BottomProjectMenuButton;
+import es.eucm.ead.editor.view.widgets.mockup.panels.HiddenPanel;
 import es.eucm.ead.engine.I18N;
 
 public class Gallery extends BaseGallery {
@@ -61,10 +67,85 @@ public class Gallery extends BaseGallery {
 
 	private static final float PREF_BOTTOM_BUTTON_WIDTH = .25F;
 	private static final float PREF_BOTTOM_BUTTON_HEIGHT = .2F;
+	
+	private HiddenPanel filterPanel;
 
 	@Override
 	public String getName() {
 		return NAME;
+	}
+	
+	@Override
+	public Actor build(Controller controller) {
+		Table ac = (Table) super.build(controller);
+		ac.addActor(filterPanel(controller.getEditorAssets().getI18N(), controller.getEditorAssets().getSkin()));
+		return ac;
+	}
+	
+	protected HiddenPanel filterPanel(I18N i18n, Skin skin){ 
+		filterPanel = new HiddenPanel(skin);
+		filterPanel.setFillParent(true);
+		filterPanel.setVisible(false);
+		
+		Button applyFilter = new TextButton(i18n.m("general.gallery.accept-filter"), skin);
+		applyFilter.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+					Gallery.this.filterPanel.hide();
+				return false;
+			}
+		});
+
+		// FIXME load real tags 
+		CheckBox[] tags = new CheckBox[] { new CheckBox("Almohada", skin),
+				new CheckBox("Camilla", skin), new CheckBox("Doctor", skin),
+				new CheckBox("Enfermera", skin), new CheckBox("Guantes", skin),
+				new CheckBox("Habitación", skin),
+				new CheckBox("Hospital", skin),
+				new CheckBox("Quirófano", skin),
+				new CheckBox("Medicamentos", skin),
+				new CheckBox("Médico", skin), new CheckBox("Paciente", skin),
+				new CheckBox("Vehículo", skin) };
+		Table tagList = new Table(skin);
+		tagList.left();
+		tagList.defaults().left();
+		for (int i = 0; i < tags.length; ++i) {
+			tagList.add(tags[i]);
+			if (i < tags.length - 1)
+				tagList.row();
+		}
+		// END FIXME
+		
+		ScrollPane tagScroll = new ScrollPane(tagList, skin, "opaque");
+		
+		filterPanel.add(tagScroll).fill().colspan(3).left();
+		filterPanel.row();
+		filterPanel.add(applyFilter).colspan(3).expandX();
+		return filterPanel;
+	}
+	
+	@Override
+	protected WidgetGroup topWidget(Vector2 viewport, I18N i18n, Skin skin, Controller controller) {
+		Table top = (Table) super.topWidget(viewport, i18n, skin, controller);
+		
+		Button filterButton = new TextButton(i18n.m("general.gallery.accept-filter"), skin);
+		filterButton.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				if(Gallery.this.filterPanel.isVisible()){
+					Gallery.this.filterPanel.hide();
+				}else {
+					Gallery.this.filterPanel.show();
+				}
+				return false;
+			}
+		});
+		
+		top.add(filterButton);
+		
+		return top;
 	}
 	
 	@Override
