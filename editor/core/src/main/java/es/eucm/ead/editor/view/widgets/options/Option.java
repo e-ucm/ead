69cbedb57c9f92f5605ed190a38a52f50c807d86
anserran <angelserranolaguna@gmail.com>
2014-02-26 11:06:24 +0100

    Added valid state and error message to option; also javadoc

--- editor/core/src/main/java/es/eucm/ead/editor/view/widgets/options/Option.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/options/Option.java
@@ -37,6 +37,7 @@
 package es.eucm.ead.editor.view.widgets.options;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -47,6 +48,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import es.eucm.ead.editor.view.widgets.AbstractWidget;
 
+/**
+ * Represents a row in an {@link OptionsPanel}. Contains a label, an optional
+ * tooltip (in the left column) and the option widget itself (in the right
+ * column).
+ */
 public class Option extends AbstractWidget {
 
 	private OptionStyle style;
@@ -57,16 +63,39 @@ public class Option extends AbstractWidget {
 
 	private Label tooltipText;
 
-	private Actor option;
+	private Label errorMessage;
+
+	private Actor optionWidget;
 
 	private float leftWidth;
+	private boolean valid;
 
+	/**
+	 * Creates an empty option
+	 * 
+	 * @param style
+	 *            the style for the option
+	 */
 	public Option(OptionStyle style) {
 		this(null, null, null, style);
 	}
 
-	public Option(String label, String tooltip, Actor option, OptionStyle style) {
+	/**
+	 * Creates an option
+	 * 
+	 * @param label
+	 *            the label for the option (can be null)
+	 * @param tooltip
+	 *            the tooltip for the option (can be null)
+	 * @param optionWidget
+	 *            the option widget (can be null)
+	 * @param style
+	 *            the style for the option
+	 */
+	public Option(String label, String tooltip, Actor optionWidget,
+			OptionStyle style) {
 		this.style = style;
+
 		if (label != null) {
 			label(label);
 		}
@@ -75,59 +104,143 @@ public class Option extends AbstractWidget {
 			tooltip(tooltip);
 		}
 
-		if (option != null) {
-			option(option);
+		if (optionWidget != null) {
+			option(optionWidget);
 		}
 	}
 
-	public void setLeftWidth(float leftWidth) {
-		this.leftWidth = leftWidth;
+	/**
+	 * 
+	 * @return if the option is valid in the current state
+	 */
+	public boolean isValid() {
+		return valid;
 	}
 
+	/**
+	 * @param valid
+	 *            Sets if the option is valid in the current state. Also
+	 *            shows/hide the error message
+	 */
+	public void setValid(boolean valid) {
+		this.valid = valid;
+		if (errorMessage != null) {
+			if (valid) {
+				errorMessage.remove();
+			} else {
+				addActor(errorMessage);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @return the option widget
+	 */
+	public Actor getOptionWidget() {
+		return optionWidget;
+	}
+
+	/**
+	 * Sets the text label for the option
+	 * 
+	 * @param label
+	 *            the text for the label
+	 * @return this option
+	 */
 	public Option label(String label) {
-		title = new Label(label, new LabelStyle(style.font, style.fontColor));
-		addActor(title);
+		if (title == null) {
+			title = new Label(label,
+					new LabelStyle(style.font, style.fontColor));
+			addActor(title);
+		}
+		title.setText(label);
 		return this;
 	}
 
+	/**
+	 * Sets the tooltip for the option
+	 * 
+	 * @param text
+	 *            the text showed in the tooltip
+	 * @return this option
+	 */
 	public Option tooltip(String text) {
-		tooltipButton = new Image(style.tooltipIcon);
-		addActor(tooltipButton);
+		if (tooltipButton == null) {
+			tooltipButton = new Image(style.tooltipIcon);
+			addActor(tooltipButton);
 
-		LabelStyle tooltipStyle = new LabelStyle();
-		tooltipStyle.fontColor = style.tooltipFontColor == null ? style.fontColor
-				: style.tooltipFontColor;
-		tooltipStyle.font = style.tooltipFont == null ? style.font
-				: style.tooltipFont;
-		tooltipStyle.background = style.tooltipBackground;
+			LabelStyle tooltipStyle = new LabelStyle();
+			tooltipStyle.fontColor = style.tooltipFontColor == null ? style.fontColor
+					: style.tooltipFontColor;
+			tooltipStyle.font = style.tooltipFont == null ? style.font
+					: style.tooltipFont;
+			tooltipStyle.background = style.tooltipBackground;
 
-		tooltipText = new Label(text, tooltipStyle);
+			tooltipText = new Label(text, tooltipStyle);
+			tooltipButton.addListener(new InputListener() {
+				@Override
+				public void enter(InputEvent event, float x, float y,
+						int pointer, Actor fromActor) {
+					Option.this.addActor(tooltipText);
+					Option.this.toFront();
+					tooltipText.toFront();
+				}
 
-		tooltipButton.addListener(new InputListener() {
-			@Override
-			public void enter(InputEvent event, float x, float y, int pointer,
-					Actor fromActor) {
-				Option.this.addActor(tooltipText);
-				Option.this.toFront();
-				tooltipText.toFront();
-			}
-
-			@Override
-			public void exit(InputEvent event, float x, float y, int pointer,
-					Actor toActor) {
-				tooltipText.remove();
-			}
-		});
+				@Override
+				public void exit(InputEvent event, float x, float y,
+						int pointer, Actor toActor) {
+					tooltipText.remove();
+				}
+			});
+		}
+		tooltipText.setText(text);
 		return this;
 	}
 
+	public Option errorMessage(String message) {
+		if (errorMessage == null) {
+			LabelStyle errorStyle = new LabelStyle(
+					style.errorFont == null ? style.font : style.errorFont,
+					style.errorFontColor == null ? style.fontColor
+							: style.errorFontColor);
+			errorMessage = new Label(message, errorStyle);
+			addActor(errorMessage);
+		}
+		errorMessage.setText(message);
+		return this;
+	}
+
+	/**
+	 * Sets the option widget for the option
+	 * 
+	 * @param option
+	 *            the widget
+	 * @return this option
+	 */
 	public Option option(Actor option) {
-		this.option = option;
+		if (this.optionWidget != null) {
+			optionWidget.remove();
+		}
+		this.optionWidget = option;
 		addActor(option);
 		return this;
 	}
 
-	public float getLeftPrefWidth() {
+	/**
+	 * Sets the width for the left column of the option
+	 * 
+	 * @param leftWidth
+	 *            the width
+	 */
+	void setLeftWidth(float leftWidth) {
+		this.leftWidth = leftWidth;
+	}
+
+	/**
+	 * @return Returns the preferred width for the left column of this option
+	 */
+	float getLeftPrefWidth() {
 		return title.getPrefWidth()
 				+ (tooltipButton == null ? 0 : tooltipButton.getPrefWidth())
 				+ style.pad + style.margin / 2.0f;
@@ -140,8 +253,17 @@ public class Option extends AbstractWidget {
 
 	@Override
 	public float getPrefHeight() {
-		return Math.max(title.getPrefHeight(), getPrefHeight(option))
+		return Math.max(title.getPrefHeight(), getPrefHeight(optionWidget))
 				+ style.pad * 2;
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		if (!isValid()) {
+			style.invalidBackground.draw(batch, getX(), getY(), getWidth(),
+					getHeight());
+		}
+		super.draw(batch, parentAlpha);
 	}
 
 	@Override
@@ -167,21 +289,43 @@ public class Option extends AbstractWidget {
 		}
 		// Option
 		width = Math.min(getWidth() - leftWidth - style.pad - style.margin
-				/ 2.0f, getMaxWidth(option));
+				/ 2.0f, getMaxWidth(optionWidget));
 		x = leftWidth + style.margin / 2.0f;
-		height = getPrefHeight(option);
+		height = getPrefHeight(optionWidget);
 		y = (getHeight() - height) / 2.0f;
-		option.setBounds(x, y, width, height);
+		optionWidget.setBounds(x, y, width, height);
+		// Error
+		if (errorMessage != null) {
+			errorMessage.setPosition(leftWidth + style.margin / 2.0f, 0);
+		}
 	}
 
 	public static class OptionStyle {
-		public Drawable tooltipIcon;
-		public Drawable tooltipBackground;
-		public BitmapFont font, tooltipFont;
-		public Color fontColor, tooltipFontColor;
-		public float pad = 10.0f;
 		/**
-		 * Margin between the label and the opton
+		 * Image for the tooltip icon
+		 */
+		public Drawable tooltipIcon;
+
+		/**
+		 * Background for the tooltip text
+		 */
+		public Drawable tooltipBackground;
+
+		/**
+		 * Background drawn when the option is invalid
+		 */
+		public Drawable invalidBackground;
+
+		public BitmapFont font, tooltipFont, errorFont;
+		public Color fontColor, tooltipFontColor, errorFontColor;
+
+		/**
+		 * f* Padding of the option as a row
+		 */
+		public float pad = 10.0f;
+
+		/**
+		 * Margin between the label and the option widget
 		 */
 		public float margin = 10.0f;
 	}
