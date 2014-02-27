02f04394aaa628da7bd9c01b12d146cb676ff076
RotaruDan <drotaru@ucm.es>
2014-02-27 01:49:20 +0100

    New functionality: change project title. closes #71

--- editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/menu/ProjectScreen.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/menu/ProjectScreen.java
@@ -36,15 +36,26 @@
  */
 package es.eucm.ead.editor.view.builders.mockup.menu;
 
+import java.io.File;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener;
+import com.esotericsoftware.tablelayout.Cell;
 
+import es.eucm.ead.editor.assets.ProjectAssets;
 import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.control.Preferences;
 import es.eucm.ead.editor.control.actions.ChangeView;
+import es.eucm.ead.editor.control.commands.Command;
+import es.eucm.ead.editor.control.commands.FieldCommand;
+import es.eucm.ead.editor.model.Project;
 import es.eucm.ead.editor.view.builders.ViewBuilder;
 import es.eucm.ead.editor.view.builders.mockup.camera.Picture;
 import es.eucm.ead.editor.view.builders.mockup.camera.Video;
@@ -65,9 +76,17 @@ public class ProjectScreen implements ViewBuilder {
 			IC_GALLERY = "ic_gallery", IC_PHOTOCAMERA = "ic_photocamera",
 			IC_VIDEOCAMERA = "ic_videocamera";
 
+	private static final float INITIALSCENEBUTTON_FONT_SCALE = .6F;
 	private static final float PREF_BOTTOM_BUTTON_WIDTH = .25F;
 	private static final float PREF_BOTTOM_BUTTON_HEIGHT = .2F;
-	private static final float INITIALSCENEBUTTON_FONT_SCALE = .6F;
+	private static final float TEXT_WIDTH_SCALAR = 1.35F;
+	private static final int MAX_PROJ_TITLE_CHARACTERS = 30;
+	private TextField projectTitleField;
+	/**
+	 * Cell that holds the {@link projectTitleField} TextField. Used to change
+	 * its size when we change project's title.
+	 */
+	private Cell<?> projectTitleCell;
 
 	@Override
 	public String getName() {
@@ -75,22 +94,99 @@ public class ProjectScreen implements ViewBuilder {
 	}
 
 	@Override
-	public Actor build(Controller controller) {
-		Skin skin = controller.getEditorAssets().getSkin();
-		I18N i18n = controller.getEditorAssets().getI18N();
+	public Actor build(final Controller controller) {
+		final Skin skin = controller.getEditorAssets().getSkin();
+		final I18N i18n = controller.getEditorAssets().getI18N();
 		final Vector2 viewport = controller.getPlatform().getSize();
 
 		final Button backButton = new IconButton(viewport, skin, "ic_goback",
 				controller, ChangeView.NAME, InitialScreen.NAME);
 
-		final TextField projectNameField = new TextField("", skin);
+		projectTitleField = new TextField("", skin);
 		String msg = i18n.m("project.untitled");
-		projectNameField.setMessageText(msg);
-		Table topLeftWidgets = new Table().left().top().debug();
+		projectTitleField.setMessageText(msg);
+		projectTitleField.setMaxLength(MAX_PROJ_TITLE_CHARACTERS);
+		projectTitleField.setTextFieldListener(new TextFieldListener() {
+			private static final String PROJECT_TITLE_FIELD = "Project title";
+
+			public void keyTyped(TextField textField, char key) {
+				if (key == '\n' || key == '\r') {
+					final Project currProj = controller.getModel().getProject();
+					final String oldTitle = currProj.getTitle();
+					final String newTitle = projectTitleField.getText();
+					if (newTitle.equals(oldTitle)) {
+						Gdx.app.log(PROJECT_TITLE_FIELD,
+								"Old title equals new title!");
+						return;
+					}
+
+					final Command changeTitleCom = new FieldCommand(currProj,
+							"title", newTitle, false);
+					controller.command(changeTitleCom);
+					controller.getEditorIO().save(
+							controller.getModel().getProject());
+
+					final ProjectAssets projectAssets = controller
+							.getProjectAssets();
+					final String oldProjPath = projectAssets.getLoadingPath();
+					final FileHandle projectDir = projectAssets
+							.absolute(oldProjPath);
+					if (!projectDir.exists()) {
+						Gdx.app.error(PROJECT_TITLE_FIELD,
+								"Project path doesn't exist!");
+						return;
+					}
+					if (!projectDir.isDirectory()) {
+						Gdx.app.error(PROJECT_TITLE_FIELD,
+								"Project path isn't a directory!");
+						return;
+					}
+
+					final String projectDirName = projectDir.name();
+					if (projectDirName.equals(newTitle)) {
+						Gdx.app.error(PROJECT_TITLE_FIELD,
+								"Project's folder has the same title!");
+						return;
+					}
+					final FileHandle parentDir = projectDir.parent();
+					for (FileHandle child : parentDir.list()) {
+						if (child != projectDir && child.isDirectory()
+								&& child.name().equals(newTitle)) {
+							Gdx.app.error(PROJECT_TITLE_FIELD,
+									"There is another project with the same title!");
+							return;
+						}
+					}
+					final String newPath = parentDir.file().getAbsolutePath()
+							+ File.separator + newTitle + File.separator;
+					projectDir.moveTo(projectAssets.absolute(newPath));
+					projectAssets.setLoadingPath(newPath, false);
+					// XXX Shouldn't there be a method like
+					// Preferences#addRecent ?
+					final Preferences prefs = controller.getPreferences();
+					prefs.putString(Preferences.RECENT_GAMES, newPath + ";"
+							+ prefs.getString(Preferences.RECENT_GAMES, ""));
+					Gdx.app.log(PROJECT_TITLE_FIELD,
+							"Project renamed and preferences updated!");
+
+					// Now we resize the text field to match it's new text
+					projectTitleField.getStage().unfocusAll();
+					projectTitleField.setCursorPosition(0);
+					final float newWidth = skin.getFont("default-font")
+							.getBounds(newTitle).width * TEXT_WIDTH_SCALAR;
+					projectTitleCell.width(Math.max(
+							projectTitleField.getMinWidth(), newWidth));
+					projectTitleCell.getLayout().invalidateHierarchy();
+				}
+			}
+		});
+		final Table topLeftWidgets = new Table().left().top().debug();
 		topLeftWidgets.setFillParent(true);
 		topLeftWidgets.add(backButton);
-		topLeftWidgets.add(projectNameField).width(
-				skin.getFont("default-font").getBounds(msg).width * 1.2f);
+		projectTitleCell = topLeftWidgets
+				.add(projectTitleField)
+				.width(skin.getFont("default-font").getBounds(msg).width
+						* TEXT_WIDTH_SCALAR).expandX().left();
 
 		final Button scene, element, play, gallery, takePictureButton, recordVideoButton;
 		final MenuButton initialSceneButton;
@@ -139,6 +235,10 @@ public class ProjectScreen implements ViewBuilder {
 
 	@Override
 	public void initialize(Controller controller) {
+		controller.getProjectAssets().finishLoading();
+		projectTitleField
+				.setText(controller.getModel().getProject().getTitle());
+
 	}
 
 	@Override
