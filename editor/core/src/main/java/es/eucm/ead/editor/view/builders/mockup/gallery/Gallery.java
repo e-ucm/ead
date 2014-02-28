6e4fc176d3e1f0d3d6f3660b8a6beefd0ae6babe
gorco <antcal01@ucm.es>
2014-02-28 15:55:37 +0100

    Use the new MenuButton and deleted useless code

--- editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/gallery/Gallery.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/gallery/Gallery.java
@@ -55,6 +55,8 @@ import es.eucm.ead.editor.view.builders.mockup.camera.Video;
 import es.eucm.ead.editor.view.widgets.GridLayout;
 import es.eucm.ead.editor.view.widgets.mockup.ToolBar;
 import es.eucm.ead.editor.view.widgets.mockup.buttons.BottomProjectMenuButton;
+import es.eucm.ead.editor.view.widgets.mockup.buttons.MenuButton;
+import es.eucm.ead.editor.view.widgets.mockup.buttons.MenuButton.POSITION;
 import es.eucm.ead.editor.view.widgets.mockup.panels.HiddenPanel;
 import es.eucm.ead.engine.I18N;
 
@@ -66,7 +68,7 @@ public class Gallery extends BaseGallery {
 			IC_VIDEOCAMERA = "ic_videocamera";
 
 	private static final float PREF_BOTTOM_BUTTON_WIDTH = .25F;
-	private static final float PREF_BOTTOM_BUTTON_HEIGHT = .2F;
+	private static final float PREF_BOTTOM_BUTTON_HEIGHT = .12F;
 	
 	private HiddenPanel filterPanel;
 
@@ -129,7 +131,7 @@ public class Gallery extends BaseGallery {
 	protected WidgetGroup topWidget(Vector2 viewport, I18N i18n, Skin skin, Controller controller) {
 		Table top = (Table) super.topWidget(viewport, i18n, skin, controller);
 		
-		Button filterButton = new TextButton(i18n.m("general.gallery.accept-filter"), skin);
+		Button filterButton = new TextButton(i18n.m("general.gallery.filter"), skin);
 		filterButton.addListener(new ClickListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
@@ -155,20 +157,11 @@ public class Gallery extends BaseGallery {
 		galleryTable.pad(2);
 		galleryTable.setFillParent(true);
 
-		Table tIn = new Table().debug();
-		tIn.pad(2);
-		tIn.setFillParent(true);
-
 		// FIXME (Testing GridLayout)
 		for (int i = 10; i < 40; i++) {
 			galleryTable.addActor(new TextButton("proyecto" + i, skin));
-			tIn.add(new TextButton("proyecto " + i, skin));
-			tIn.row();
 		}
 		// END FIXME
-
-		Table window = new Table();
-		window.setFillParent(true);
 
 		ScrollPane sp = new ScrollPane(galleryTable);
 		sp.setScrollingDisabled(true, false);
@@ -179,16 +172,16 @@ public class Gallery extends BaseGallery {
 
 	@Override
 	protected WidgetGroup bottomWidget(Vector2 viewport, I18N i18n, Skin skin, Controller controller) {
-		ToolBar botBar = new ToolBar(viewport, skin);
+		ToolBar botBar = new ToolBar(viewport, skin, 0.04f);
 
-		BottomProjectMenuButton pictureButton = new BottomProjectMenuButton(
+		MenuButton pictureButton = new BottomProjectMenuButton(
 				viewport, i18n.m("general.mockup.photo"), skin, IC_PHOTOCAMERA,
-				PREF_BOTTOM_BUTTON_WIDTH, PREF_BOTTOM_BUTTON_HEIGHT,
-				controller, ChangeView.NAME, Picture.NAME);
-		BottomProjectMenuButton videoButton = new BottomProjectMenuButton(
+				PREF_BOTTOM_BUTTON_WIDTH, PREF_BOTTOM_BUTTON_HEIGHT, 
+				 POSITION.right, controller, ChangeView.NAME, Picture.NAME);
+		MenuButton videoButton = new BottomProjectMenuButton(
 				viewport, i18n.m("general.mockup.video"), skin, IC_VIDEOCAMERA,
 				PREF_BOTTOM_BUTTON_WIDTH, PREF_BOTTOM_BUTTON_HEIGHT,
-				controller, ChangeView.NAME, Video.NAME);
+				POSITION.left, controller, ChangeView.NAME, Video.NAME);
 
 		botBar.add(pictureButton).left();
 		botBar.add("").expandX();
