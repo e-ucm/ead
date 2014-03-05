c2c21e62e480e6cc84015caf1a996c36f02f6884
anserran <angelserranolaguna@gmail.com>
2014-03-05 10:35:03 +0100

    Added GameMetaData (as substitute of Project)

--- editor/schema/src/main/java/es/eucm/ead/schema/game/GameMetadata.java
+++ editor/schema/src/main/java/es/eucm/ead/schema/game/GameMetadata.java
@@ -1,3 +1,39 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2013 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package es.eucm.ead.schema.game;
 
 import javax.annotation.Generated;
@@ -14,6 +50,16 @@ public class GameMetadata {
 	 * 
 	 */
 	private String title;
+	/**
+	 * Game description
+	 * 
+	 */
+	private String description;
+	/**
+	 * The last scene edited
+	 * 
+	 */
+	private String editScene;
 
 	/**
 	 * Game title
@@ -29,6 +75,38 @@ public class GameMetadata {
 	 */
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	/**
+	 * Game description
+	 * 
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Game description
+	 * 
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * The last scene edited
+	 * 
+	 */
+	public String getEditScene() {
+		return editScene;
+	}
+
+	/**
+	 * The last scene edited
+	 * 
+	 */
+	public void setEditScene(String editScene) {
+		this.editScene = editScene;
 	}
 
 }
