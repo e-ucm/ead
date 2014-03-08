dc642c16e062add25778189eb9b205de748ceda9
gorco <antcal01@ucm.es>
2014-03-08 14:16:19 +0100

    Now use the i18n

--- editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/editionComponents/PaintComponent.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/editionComponents/PaintComponent.java
@@ -1,4 +1,4 @@
-package es.eucm.ead.editor.view.widgets.mockup.EditionComponents;
+package es.eucm.ead.editor.view.widgets.mockup.editionComponents;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -7,6 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
 
 import es.eucm.ead.editor.view.builders.mockup.edition.EditionWindow;
 import es.eucm.ead.editor.view.widgets.mockup.buttons.ToolbarButton;
+import es.eucm.ead.editor.view.widgets.mockup.panels.SamplePanel;
 import es.eucm.ead.engine.I18N;
 
 public class PaintComponent extends EditionComponent{
@@ -16,15 +17,19 @@ public class PaintComponent extends EditionComponent{
 	public PaintComponent(EditionWindow parent, Vector2 viewport, I18N i18n, Skin skin){
 		super(skin, parent);
 		
-		super.button = new ToolbarButton(viewport, skin.getDrawable(IC_PAINT), "Pintar", skin); //Use i18n and ToolbarButton
+		super.button = new ToolbarButton(viewport, skin.getDrawable(IC_PAINT), i18n.m("edition.brush"), skin);
 		super.button.addListener(buttonListener());
 		
 		
-		Label label = new Label("Herramienta de pincel", skin, "default-thin-opaque"); //use i28n
+		Label label = new Label(i18n.m("edition.tool.brush"), skin, "default-thin-opaque");
 		label.setWrap(false);
 		label.setAlignment(Align.center);
 		label.setFontScale(0.7f);
 		
 		this.add(label).center();
+		this.row();
+		this.add(new SamplePanel(i18n, skin, 3, false, true));
 	}
+	
+	//TODO add functionality
 }
--- editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/editionComponents/TextComponent.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/editionComponents/TextComponent.java
@@ -1,4 +1,4 @@
-package es.eucm.ead.editor.view.widgets.mockup.EditionComponents;
+package es.eucm.ead.editor.view.widgets.mockup.editionComponents;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -7,6 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
 
 import es.eucm.ead.editor.view.builders.mockup.edition.EditionWindow;
 import es.eucm.ead.editor.view.widgets.mockup.buttons.ToolbarButton;
+import es.eucm.ead.editor.view.widgets.mockup.panels.SamplePanel;
 import es.eucm.ead.engine.I18N;
 
 public class TextComponent extends EditionComponent{
@@ -16,15 +17,19 @@ public class TextComponent extends EditionComponent{
 	public TextComponent(EditionWindow parent, Vector2 viewport, I18N i18n, Skin skin){
 		super(skin, parent);
 		
-		super.button = new ToolbarButton(viewport, skin.getDrawable(IC_TEXT), "Texto", skin); //Use i18n and ToolbarButton
+		super.button = new ToolbarButton(viewport, skin.getDrawable(IC_TEXT), i18n.m("edition.text"), skin);
 		super.button.addListener(buttonListener());
 		
 		
-		Label label = new Label("Herramienta de texto", skin, "default-thin-opaque"); //use i28n
+		Label label = new Label(i18n.m("edition.tool.text"), skin, "default-thin-opaque");
 		label.setWrap(false);
 		label.setAlignment(Align.center);
 		label.setFontScale(0.7f);
 		
 		this.add(label).center();
+		this.row();
+		this.add(new SamplePanel(i18n, skin, 3, true, true));
 	}
+	
+	//TODO add functionality
 }
