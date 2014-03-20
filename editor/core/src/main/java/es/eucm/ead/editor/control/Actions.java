e1c10a852b3b53674773ce422c30411cb2c5ac46
jtorrente <jtorrente@e-ucm.es>
2014-03-20 01:20:31 +0100

    logging actions for bug reporting

--- editor/core/src/main/java/es/eucm/ead/editor/control/Actions.java
+++ editor/core/src/main/java/es/eucm/ead/editor/control/Actions.java
@@ -38,12 +38,18 @@ package es.eucm.ead.editor.control;
 
 import com.badlogic.gdx.Gdx;
 
+import com.badlogic.gdx.utils.SerializationException;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
 import es.eucm.ead.editor.control.actions.*;
 import es.eucm.ead.editor.control.actions.EditorAction.EditorActionListener;
+import es.eucm.ead.editor.control.appdata.EditorActionsLog;
+import es.eucm.ead.editor.control.appdata.SerializedEditorAction;
+import es.eucm.ead.editor.control.appdata.TimestampedEditorAction;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -55,9 +61,15 @@ public class Actions {
 
 	private Controller controller;
 
+    /**
+     * The actions log. Used for bug reporting and analytics
+     */
+    private EditorActionsLog editorActionsLog;
+
 	public Actions(Controller controller) {
 		actionsMap = new HashMap<Class, EditorAction>();
 		this.controller = controller;
+        editorActionsLog = new EditorActionsLog();
 	}
 
 	/**
@@ -108,10 +120,38 @@ public class Actions {
 	public void perform(Class actionClass, Object... args) {
 		EditorAction action = getAction(actionClass);
 		if (action != null && action.isEnabled()) {
-			action.perform(args);
+            serializeAction(actionClass, args);
+            action.perform(args);
 		} else {
 			Gdx.app.error("Actions", "Action with class " + actionClass
 					+ (action == null ? " does not exist." : " is disabled"));
 		}
 	}
+
+    /**
+     * Saves a {@link es.eucm.ead.editor.control.appdata.TimestampedEditorAction} just before each
+     * action is performed.
+     * @param actionClass   The class of the action (e.g. es.eucm.ead.editor.core.contro.AddSccene)
+     * @param args  The arguments the action received
+     */
+    private void serializeAction(Class actionClass, Object... args){
+        TimestampedEditorAction serializedEditorAction = new TimestampedEditorAction();
+        serializedEditorAction.setTimestamp(System.currentTimeMillis()+"");
+        serializedEditorAction.setActionClass(actionClass.getCanonicalName());
+        List<Object> arguments = new ArrayList<Object>();
+        for (Object arg: args){
+            arguments.add(arg);
+        }
+        serializedEditorAction.setArguments(arguments);
+        editorActionsLog.getEditorActions().add(serializedEditorAction);
+    }
+
+    /**
+     * Returns the list of actions logged so far.
+     *
+     * Used for bug reporting.
+     */
+    public EditorActionsLog getEditorActionsLog(){
+        return editorActionsLog;
+    }
 }
