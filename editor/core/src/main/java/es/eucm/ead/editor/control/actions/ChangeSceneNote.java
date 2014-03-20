b22a8302d973a29c960dbabb172abb20d9012621
RotaruDan <drotaru@ucm.es>
2014-03-20 23:30:14 +0100

    Added ChangeSceneNote action, test and convenience listener.

---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/ChangeSceneNote.java
@@ -0,0 +1,139 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions;
+
+import com.badlogic.gdx.Gdx;
+
+import es.eucm.ead.editor.control.commands.FieldCommand;
+import es.eucm.ead.editor.model.FieldNames;
+import es.eucm.ead.schema.editor.actors.EditorScene;
+import es.eucm.ead.schema.editor.components.Note;
+
+/**
+ * This class changes the {@link Note} of a {@link EditorScene}. args[0] can be
+ * a {@link EditorScene} or a {@link String sceneId}. args[1] must be a
+ * {@link FieldNames#NOTE_TITLE} or {@link FieldNames#NOTE_DESCRIPTION}. args[2]
+ * is the new value.
+ * 
+ * Created by Cristian Rotaru on 20/03/14.
+ */
+public class ChangeSceneNote extends EditorAction {
+
+	private static final String CHANGESCENENAME_LOGTAG = "ChangeSceneNote";
+
+	/**
+	 * 0: object to be renamed 1: new name
+	 * 
+	 * @param args
+	 */
+	@Override
+	public void perform(Object... args) {
+		// There should be at least one argument
+		if (args.length != 3) {
+			throw new EditorActionException("Error in action "
+					+ this.getClass().getCanonicalName()
+					+ ": expected 3 arguments, received ~> " + args.length);
+		}
+
+		Note objectToRename = null;
+		// IF first argument is null or a String, it has to be found. In this
+		// case, findObjectById is invoked. Subclasses should override this
+		// method
+		// If they want to support this feature
+		if (args[0] != null) {
+			if (args[0] instanceof String) {
+				objectToRename = controller.getModel().getScenes()
+						.get(args[0].toString()).getNotes();
+			}
+			// If the first argument is not to be found, it should have a name
+			// attribute declared. Otherwise, throw exception
+			else if (args[0] instanceof EditorScene) {
+				objectToRename = ((EditorScene) args[0]).getNotes();
+			}
+		}
+
+		// At this point, if objectToRename is still null, return without doing
+		// anything
+		if (objectToRename == null) {
+			Gdx.app.error(CHANGESCENENAME_LOGTAG, "Error in action "
+					+ this.getClass().getCanonicalName()
+					+ ": unexpected args[0] ~> " + args[0]);
+			return;
+		}
+
+		FieldNames field = null;
+		String oldValue = null;
+		if (args[1] != null) {
+			if (args[1] instanceof FieldNames) {
+				field = (FieldNames) args[1];
+				switch (field) {
+				case NOTE_DESCRIPTION:
+					oldValue = objectToRename.getDescription();
+					break;
+				case NOTE_TITLE:
+					oldValue = objectToRename.getTitle();
+					break;
+				default:
+					field = null;
+					break;
+				}
+			}
+		}
+		if (field == null) {
+			Gdx.app.error(CHANGESCENENAME_LOGTAG, "Error in action "
+					+ this.getClass().getCanonicalName()
+					+ ": unexpected args[1] ~> " + args[1]);
+			return;
+		}
+
+		// Now check the second argument.
+		String newValue = null;
+		if (args[2] != null) {
+			newValue = args[2].toString();
+		} else {
+			Gdx.app.error(CHANGESCENENAME_LOGTAG, "Error in action "
+					+ this.getClass().getCanonicalName() + ": args[2] is null.");
+			return;
+		}
+
+		if (newValue != null
+				&& (oldValue == null || !oldValue.equals(newValue))) {
+			controller.command(new FieldCommand(objectToRename, field,
+					newValue, true));
+		}
+	}
+}
---
+++ editor/core/src/test/java/es/eucm/ead/editor/actions/ChangeSceneNoteTest.java
@@ -0,0 +1,126 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.files.FileHandle;
+
+import es.eucm.ead.editor.control.actions.AddScene;
+import es.eucm.ead.editor.control.actions.ChangeSceneNote;
+import es.eucm.ead.editor.control.actions.NewGame;
+import es.eucm.ead.editor.model.FieldNames;
+import es.eucm.ead.editor.model.events.FieldEvent;
+import es.eucm.ead.editor.view.listeners.ChangeNoteFieldListener;
+import es.eucm.ead.schema.editor.actors.EditorScene;
+import es.eucm.ead.schema.editor.components.Note;
+import es.eucm.ead.schema.editor.game.EditorGame;
+
+/**
+ * Created by Cristian Rotaru on 20/03/14.
+ */
+public class ChangeSceneNoteTest extends EditorActionTest {
+
+	private boolean somethingChanged;
+
+	@Test
+	public void testDescriptionAndTitle() {
+		// Create a new project
+		FileHandle projectFile = FileHandle
+				.tempDirectory("eadtest-changescenenotes");
+		mockModel.setGame(new EditorGame());
+		mockController.action(NewGame.class, projectFile.file()
+				.getAbsolutePath(), mockModel.getGame(), mockModel.getGame());
+
+		// Initialize the new value that must be used
+		final String newTitle = "MY_NEW_TITLE";
+		final String newDescription = "MY_NEW_DESCRIPTION";
+
+		// Add a scene to be renamed
+		mockController.action(AddScene.class);
+
+		// Get the sceneMetadata & the notes
+		final EditorScene editScene = mockModel.getEditScene();
+		final Note changingNotes = editScene.getNotes();
+
+		// Add a listener that reacts to changes in scene data. This is
+		// given as a parameter
+		mockController.getModel().addFieldListener(changingNotes,
+				new ChangeNoteFieldListener() {
+
+					@Override
+					public void modelChanged(FieldEvent event) {
+						ChangeSceneNoteTest.this.somethingChanged = true;
+						super.modelChanged(event);
+					}
+
+					@Override
+					public void descriptionChanged(FieldEvent event) {
+						assertEquals(changingNotes.getDescription(),
+								newDescription);
+					}
+
+					@Override
+					public void titleChanged(FieldEvent event) {
+						assertEquals(changingNotes.getTitle(), newTitle);
+					}
+				});
+
+		this.somethingChanged = false;
+		mockController.action(action, editScene, FieldNames.NOTE_TITLE,
+				newTitle);
+		// Check something actually changed (if the model is not changed the
+		// listener is not notified and then no assertions are made)
+		assertTrue(this.somethingChanged);
+
+		this.somethingChanged = false;
+		mockController.action(action, editScene, FieldNames.NOTE_DESCRIPTION,
+				newDescription);
+		assertTrue(this.somethingChanged);
+
+		// Release resources
+		projectFile.deleteDirectory();
+	}
+
+	@Override
+	protected Class getEditorAction() {
+		return ChangeSceneNote.class;
+	}
+}
