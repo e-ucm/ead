354c7eb4a3d32f5d7d5dd7195254f73f018cb2cf
jtorrente <jtorrente@e-ucm.es>
2014-03-21 09:02:45 +0100

    Adding logic for exporting games to EditorIO: zipping dirs, copying images, reading and writing jars... that sort of stuff Also: the saveGameForExport() method has been redesigned to fit into the exportation process and to integrate Ivan's comments

--- editor/core/src/main/java/es/eucm/ead/editor/control/EditorIO.java
+++ editor/core/src/main/java/es/eucm/ead/editor/control/EditorIO.java
@@ -43,17 +43,37 @@ import com.badlogic.gdx.files.FileHandle;
 import es.eucm.ead.editor.assets.EditorGameAssets;
 import es.eucm.ead.editor.control.commands.ModelCommand;
 import es.eucm.ead.editor.model.Model;
-import es.eucm.ead.engine.GameAssets;
+import es.eucm.ead.engine.I18N;
 import es.eucm.ead.schema.actors.Scene;
 import es.eucm.ead.schema.editor.actors.EditorScene;
 import es.eucm.ead.schema.editor.game.EditorGame;
 import es.eucm.ead.schema.game.Game;
 
-import java.lang.reflect.Field;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.zip.*;
 
 public class EditorIO implements LoadedCallback {
+
+    /**
+     *  Internal folder where the game is stored when it is exported as a Jar.
+     *  This constant should be the same than the one defined in EngineJarGame,
+     *  the class that launches jar games.
+     *
+     *  All the game contents (e.g. "scenes/", "game.json") should be placed
+     *  under this folder in the jar file generated.
+     */
+    private static final String JAR_GAME_FOLDER = "assets/";
+
+    private static final String JSON_EXTENSION = ".json";
 
 	private Controller controller;
 
@@ -137,108 +157,291 @@ public class EditorIO implements LoadedCallback {
 		saveScenes(model.getScenes());
 	}
 
-	/**
-	 * Saves the given model casted to the basic schema so all the editor's
-	 * extra parameters are removed. It is intended for exporting the game for
-	 * final release only.
-	 * 
-	 * First, this method creates a temp directory to store the saved model.
-	 * This temp directory is returned so this method can be chained to others
-	 * during the exportation process.
-	 * 
-	 * Second, it casts the {@code model}'s editorgame to
-	 * {@link es.eucm.ead.schema.game.Game} and saves it to the temp folder.
-	 * 
-	 * Finally, it casts the {@code model}'s editorscenes to
-	 * {@link es.eucm.ead.schema.actors.Scene} and saves them to the temp
-	 * folder.
-	 */
-	public FileHandle saveGameForExport(Model model) {
-		// Temp dir
-		FileHandle tempDir = FileHandle.tempDirectory("ead-export-");
-		tempDir.mkdirs();
+    /**
+     * Exports the given {@code model} embedded into the engine library as a single
+     * self-contained jar file.
+     *
+     * @param destiny   The path of the destiny jar file where the output is to be saved.
+     * @param model     The model representing the game to be saved.
+     * @param callback  A simple callback to provide updates on the exportation progress.
+     */
+    public void exportAsJar(String destiny, Model model, ExportCallback callback) {
+        I18N i18N = controller.getApplicationAssets().getI18N();
 
-		// Save simplified game
-		Game simplifiedGame = (Game) upcastToEngineSchema(model.getGame());
-		// Game simplifiedGame = (Game)model.getGame();
-		String basePath = editorGameAssets.toCanonicalPath(tempDir.path());
-		if (!basePath.endsWith("/")) {
-			basePath = basePath + "/";
-		}
-		String gamePath = basePath + EditorGameAssets.GAME_FILE;
-		editorGameAssets.toJsonPath(simplifiedGame, gamePath);
+        if (controller.getEngineLibPath()==null){
+            callback.error(i18N.m("export.error.libnotfound"));
+            Gdx.app.error(this.getClass().getCanonicalName(), "Exportation failed: the engine library could not be resolved or was not defined");
+            return;
+        }
 
-		// Save simplified scenes
-		String scenesPath = basePath + EditorGameAssets.SCENES_PATH;
-		FileHandle scenesFH = new FileHandle(scenesPath);
-		scenesFH.mkdirs();
-		for (Map.Entry<String, EditorScene> entry : model.getScenes()
-				.entrySet()) {
-			Scene simplifiedScene = (Scene) upcastToEngineSchema(entry
-					.getValue());
-			String scenePath = scenesPath + entry.getKey();
-			if (!scenePath.toLowerCase().endsWith(".json")) {
-				scenePath += ".json";
-			}
-			editorGameAssets.toJsonPath(simplifiedScene, scenePath);
-		}
+        // Create a temp directory that will hold the copy of the game (/)
+        FileHandle tempDir = FileHandle.tempDirectory("ead-export-");
+        tempDir.mkdirs();
+        // Create a subfolder that means the root of the game in the Jar (/assets/)
+        FileHandle tempGameDir = tempDir.child(JAR_GAME_FOLDER);
+        tempGameDir.mkdirs();
+        callback.progress(15, i18N.m("export.progress.saving"));
 
-		// return the tempDir
-		return tempDir;
-	}
+        try {
+            // 1) Save the game upcasted to the Engine's schema
+            saveGameForExport(tempGameDir, model);
 
-	/**
-	 * This method creates a shallow copy of the given object using its
-	 * superclass' constructor to instantiate the new object. For example, if
-	 * the {@code object} given as an argument is of type
-	 * {@link es.eucm.ead.schema.editor.game.EditorGame}, the returned object
-	 * will be of type {@link es.eucm.ead.schema.game.Game}. The copy is created
-	 * using reflection.
-	 * 
-	 * This is required by
-	 * {@link #saveGameForExport(es.eucm.ead.editor.model.Model)}. The
-	 * underlying {@link com.badlogic.gdx.utils.Json} class invokes the object's
-	 * getClass() method which always returns the class used to instantiate the
-	 * object and therefore simple upcasting does not work.
-	 * 
-	 * @param object
-	 *            The editor's schema object that has to be cloned as an
-	 *            engine's schema object
-	 * @return The engine's schema object containing a shallow copy of
-	 *         {@code object}. May be null if either {@code object} is null or
-	 *         if an internal reflection exception is thrown.
-	 */
-	private Object upcastToEngineSchema(Object object) {
-		if (object == null)
-			return null;
+            // Copy non json files
+            callback.progress(30, i18N.m("export.progress.copying"));
+            this.copyNonJsonFiles(tempGameDir);
 
-		final Class clazz = object.getClass().getSuperclass();
+            // Destiny file
+            FileHandle destinyJarFile = new FileHandle( destiny );
 
-		try {
-			Object copy = clazz.newInstance();
+            // Create output stream for destiny file
+            OutputStream fileOutputStream = destinyJarFile.write(false);
+            ZipOutputStream zipOutputStream = new ZipOutputStream( fileOutputStream );
 
-			for (Field declaredField : clazz.getDeclaredFields()) {
-				declaredField.setAccessible(true);
-				declaredField.set(copy, declaredField.get(object));
-				declaredField.setAccessible(false);
-			}
+            // Merge game directory and engine jar into output stream
+            FileHandle engineJarFile = new FileHandle(controller.getEngineLibPath());
+            callback.progress(60, i18N.m("export.progress.zipping"));
+            mergeZipsAndDirsToJar(zipOutputStream, engineJarFile, tempDir);
 
-			return copy;
-		} catch (InstantiationException e) {
-			Gdx.app.debug(this.getClass().getCanonicalName(),
-					"Error while upcasting object of type " + object.getClass()
-							+ " to type " + object.getClass().getSuperclass(),
-					e);
-		} catch (IllegalAccessException e) {
-			Gdx.app.debug(this.getClass().getCanonicalName(),
-					"Error while upcasting object of type " + object.getClass()
-							+ " to type " + object.getClass().getSuperclass(),
-					e);
-		}
-		return null;
-	}
+            zipOutputStream.close();
+            callback.progress(100, i18N.m("export.progress.completed"));
+            callback.complete(i18N.m("export.successful"));
+        }
+        catch( FileNotFoundException e ) {
+            callback.error(i18N.m("export.failed"));
+            Gdx.app.error(this.getClass().getCanonicalName(), "The engine library was not found", e);
+        }
+        catch( IOException e ) {
+            callback.error(i18N.m("export.failed"));
+            Gdx.app.error(this.getClass().getCanonicalName(), "An error occurred while writing the jar file while exporting", e);
+        }
 
-	private void saveGame(Object game) {
+
+    }
+
+
+
+    /////////////////////////////////////////////
+    /// PRIVATE EXPORTATION METHODS
+    ////////////////////////////////////////////
+
+
+    /**
+     * Saves the given {@code model} casted to the basic schema so all the editor's
+     * extra parameters are removed. The output of this is saved into {@code destiny}
+     *
+     * This method is intended for exporting the game for final release only.
+     *
+     * First, it casts the {@code model}'s editorgame to
+     * {@link es.eucm.ead.schema.game.Game} and saves it to the temp folder.
+     *
+     * Second, it casts the {@code model}'s editorscenes to
+     * {@link es.eucm.ead.schema.actors.Scene} and saves them to the temp
+     * folder.
+     */
+    private void saveGameForExport(FileHandle destiny, Model model) {
+        // Save simplified game
+        Game simplifiedGame = (Game) upcastToEngineSchema(model.getGame());
+        editorGameAssets.toJson(simplifiedGame, destiny.child(EditorGameAssets.GAME_FILE));
+
+        // Save simplified scenes
+        FileHandle scenesFH = destiny.child(EditorGameAssets.SCENES_PATH);
+        scenesFH.mkdirs();
+        for (Map.Entry<String, EditorScene> entry : model.getScenes()
+                .entrySet()) {
+            Scene simplifiedScene = (Scene) upcastToEngineSchema(entry
+                    .getValue());
+
+            String scenePath = entry.getKey();
+            if (!scenePath.toLowerCase().endsWith(JSON_EXTENSION)) {
+                scenePath += JSON_EXTENSION;
+            }
+            editorGameAssets.toJson(simplifiedScene, scenesFH.child(scenePath));
+        }
+    }
+
+    /**
+     * This method creates a shallow copy of the given object using its
+     * superclass' constructor to instantiate the new object. For example, if
+     * the {@code object} given as an argument is of type
+     * {@link es.eucm.ead.schema.editor.game.EditorGame}, the returned object
+     * will be of type {@link es.eucm.ead.schema.game.Game}. The copy is created
+     * using reflection and introspection.
+     *
+     * This is required by
+     * {@link #saveGameForExport(com.badlogic.gdx.files.FileHandle, es.eucm.ead.editor.model.Model)}. The
+     * underlying {@link com.badlogic.gdx.utils.Json} class invokes the object's
+     * getClass() method which always returns the class used to instantiate the
+     * object and therefore simple upcasting does not work.
+     *
+     * @param object
+     *            The editor's schema object that has to be cloned as an
+     *            engine's schema object. Note: this object should exposed a
+     *            Java Bean exposing all its properties through getters and
+     *            setters.
+     *
+     * @return The engine's schema object containing a shallow copy of
+     *         {@code object}. May be null if either {@code object} is null or
+     *         if an internal reflection exception is thrown.
+     */
+    private Object upcastToEngineSchema(Object object) {
+        if (object == null)
+            return null;
+
+        final Class clazz = object.getClass().getSuperclass();
+
+        try {
+            Object copy = clazz.newInstance();
+            BeanInfo beanInfo = Introspector.getBeanInfo(clazz);
+            PropertyDescriptor[] properties = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor property: properties){
+                Method getter = property.getReadMethod();
+                Method setter = property.getWriteMethod();
+                if (getter!=null && setter!=null){
+                    setter.invoke(copy, getter.invoke(object));
+                }
+            }
+            return copy;
+        } catch (InstantiationException e) {
+            Gdx.app.debug(this.getClass().getCanonicalName(),
+                    "Error while upcasting object of type " + object.getClass()
+                            + " to type " + object.getClass().getSuperclass(),
+                    e);
+        } catch (IllegalAccessException e) {
+            Gdx.app.debug(this.getClass().getCanonicalName(),
+                    "Error while upcasting object of type " + object.getClass()
+                            + " to type " + object.getClass().getSuperclass(),
+                    e);
+        } catch (IntrospectionException e) {
+            Gdx.app.debug(this.getClass().getCanonicalName(),
+                    "Error while upcasting object of type " + object.getClass()
+                            + " to type " + object.getClass().getSuperclass(),
+                    e);
+        } catch (InvocationTargetException e) {
+            Gdx.app.debug(this.getClass().getCanonicalName(),
+                    "Error while upcasting object of type " + object.getClass()
+                            + " to type " + object.getClass().getSuperclass(),
+                    e);
+        }
+        return null;
+    }
+
+    private void copyNonJsonFiles(FileHandle destiny){
+        FileHandle source = new FileHandle(editorGameAssets.getLoadingPath());
+        copyNonJsonFiles(destiny, source);
+    }
+
+    private void copyNonJsonFiles(FileHandle destiny, FileHandle source){
+        for (FileHandle child:source.list()){
+            if (child.isDirectory()){
+                boolean nonJsonContent = false;
+                for (FileHandle grandSon: child.list()){
+                    if (!grandSon.path().toLowerCase().endsWith(".json")){
+                        nonJsonContent = true; break;
+                    }
+                }
+                if (nonJsonContent){
+                    FileHandle targetChild = destiny.child(child.name());
+                    targetChild.mkdirs();
+                    copyNonJsonFiles(child, targetChild);
+                }
+            } else if (!child.path().toLowerCase().endsWith(".json")){
+                FileHandle targetChild = destiny.child(child.name());
+                child.copyTo(targetChild);
+            }
+        }
+    }
+
+    private void mergeZipsAndDirsToJar(ZipOutputStream destiny, FileHandle... sources) {
+        try {
+            for (FileHandle source: sources){
+                // If it is a jar or zip file
+                if (source.path().toLowerCase().endsWith(".zip") || source.path().toLowerCase().endsWith(".jar")){
+                    InputStream fis = source.read();
+                    CheckedInputStream checksum = new CheckedInputStream( fis, new Adler32( ) );
+                    ZipInputStream zis = new ZipInputStream( new BufferedInputStream( checksum ) );
+                    ZipEntry entry = null;
+
+                    // Write the contents of the origin zip file to the destiny jar file
+                    while( ( entry = zis.getNextEntry( ) ) != null ) {
+                        //System.out.println("Extracting: " +entry);
+                        // write the files to the disk
+                        JarEntry newEntry = new JarEntry( entry.getName( ) );
+
+                        destiny.putNextEntry(newEntry);
+                        byte[] readBuffer = new byte[ 1024 ];
+                        int bytesIn = 0;
+                        while( ( bytesIn = zis.read( readBuffer ) ) != -1 ) {
+                            destiny.write(readBuffer, 0, bytesIn);
+                        }
+                        //close the Stream
+                        destiny.closeEntry();
+                    }
+                    zis.close( );
+                }
+                // IF it is a dir
+                else if (source.isDirectory()){
+                    zipDir(destiny, source, "" );
+                }
+            }
+        }
+        catch( Exception e ) {
+            //TODO
+        }
+    }
+     
+    private void zipDir( ZipOutputStream destiny, FileHandle source, String relPath ) {
+
+        try {
+            FileHandle[] children = source.list();
+            for( int i = 0; i < children.length; i++ ) {
+                FileHandle child = children[i];
+                String childName = child.name();
+                if( child.isDirectory( ) ) {
+
+
+                    if( relPath != null && !relPath.equals( "" ) )
+                        zipDir( destiny, child, relPath + "/" + childName );
+                    else
+                        zipDir( destiny, child, childName );
+                    //loop again
+                    continue;
+                }
+
+                InputStream fis = child.read();
+
+                // Take the path of the file relative to the dirOrigen
+                String entryName = childName;
+                if( relPath != null && !relPath.equals( "" ) ) {
+                    entryName = relPath + "/" + childName;
+                }
+
+                ZipEntry anEntry = new ZipEntry( entryName );
+
+                // Write the file into the ZIP. It is surrounded by a try-catch block to allow the loop to continue if the file
+                // cannot be written (Otherwise the external try-catch will capture the exception and no more files in the directory
+                // would be put into the ZIP
+                try {
+                    destiny.putNextEntry(anEntry);
+                    byte[] readBuffer = new byte[ 1024 ];
+                    int bytesIn = 0;
+                    while( ( bytesIn = fis.read( readBuffer ) ) != -1 ) {
+                        destiny.write(readBuffer, 0, bytesIn);
+                    }
+                } catch (ZipException zipException){
+
+                }
+
+                //close the Stream
+                fis.close( );
+                destiny.closeEntry();
+            }
+        }
+        catch( Exception e ) {
+            //handle exception
+        }
+    }
+
+ 	private void saveGame(Object game) {
 		// Update the appVersion and model version for this game
 		((EditorGame) game).setAppVersion(controller.getAppVersion());
 		((EditorGame) game).setModelVersion(controller.getModelVersion());
@@ -317,4 +520,11 @@ public class EditorIO implements LoadedCallback {
 					scenes));
 		}
 	}
+
+
+    public interface ExportCallback{
+        public void error(String errorMessage);
+        public void progress(int percentage, String currentTask);
+        public void complete(String completionMessage);
+    }
 }
