865c530c41d0a9be4588d9fdf16df5520064276f
anserran <angelserranolaguna@gmail.com>
2014-04-01 14:41:25 +0200

    Move loaders to package loaders

--- editor/core/src/main/java/es/eucm/ead/editor/assets/EditorGameAssets.java
+++ editor/core/src/main/java/es/eucm/ead/editor/assets/EditorGameAssets.java
@@ -42,6 +42,8 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.JsonWriter.OutputType;
+import es.eucm.ead.editor.assets.loaders.EditorGameLoader;
+import es.eucm.ead.editor.assets.loaders.EditorSceneLoader;
 import es.eucm.ead.engine.GameAssets;
 import es.eucm.ead.engine.assets.SimpleLoaderParameters;
 import es.eucm.ead.schema.editor.actors.EditorScene;
