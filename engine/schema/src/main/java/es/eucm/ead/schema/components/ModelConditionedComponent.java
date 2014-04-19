426be3d2cf99761232e9418b5c3a920bd2c617e7
jtorrente <jtorrente@e-ucm.es>
2014-04-19 22:34:01 +0200

    Basic model (+engine) component that can holds an expression. Convenient for implementing a "conditioned visibility" component (next commit)

---
+++ engine/schema/src/main/java/es/eucm/ead/schema/components/ModelConditionedComponent.java
@@ -0,0 +1,72 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package es.eucm.ead.schema.components;
+
+import javax.annotation.Generated;
+
+/**
+ * A component that can hold an expression (see
+ * https://github.com/e-ucm/ead/wiki/Expressions).
+ * 
+ */
+@Generated("org.jsonschema2pojo")
+public class ModelConditionedComponent extends ModelComponent {
+
+	/**
+	 * The expression that serves as condition
+	 * 
+	 */
+	private String expression;
+
+	/**
+	 * The expression that serves as condition
+	 * 
+	 */
+	public String getExpression() {
+		return expression;
+	}
+
+	/**
+	 * The expression that serves as condition
+	 * 
+	 */
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+
+}
