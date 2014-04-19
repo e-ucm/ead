0c37602cd892c599438858e0704317bfba39bfa6
jtorrente <jtorrente@e-ucm.es>
2014-04-19 22:42:50 +0200

    Reusable base system for conditions.

---
+++ engine/core/src/main/java/es/eucm/ead/engine/systems/ConditionalSystem.java
@@ -0,0 +1,104 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.systems;
+
+import ashley.core.Family;
+import ashley.core.PooledEngine;
+import ashley.systems.IteratingSystem;
+import es.eucm.ead.engine.systems.variables.VariablesSystem;
+
+/**
+ * Convenient system that provides functionality for evaluating conditions. Any
+ * system needing to (1) iterate through entities and (2) check if one or more
+ * conditions are met would profit from extending this class.
+ * 
+ * Known subclasses: {@link VisibilitySystem}
+ * 
+ * Created by Javier Torrente on 19/04/14.
+ */
+public abstract class ConditionalSystem extends IteratingSystem {
+
+	// Engine for creating new components and accessing other systems
+	// (VariablesSystem)
+	protected PooledEngine engine;
+
+	public ConditionalSystem(PooledEngine engine, Family family) {
+		super(family);
+		this.engine = engine;
+	}
+
+	/**
+	 * Evaluates the given condition using the underlying
+	 * {@link VariablesSystem}. If for whatever reason this system is not
+	 * available, or if the {@code expression} provided for evaluation is null,
+	 * it returns {@link #getDefaultValueForCondition()}.
+	 * 
+	 * @param expression
+	 *            The condition to be evaluated
+	 * @return The results of evaluating {@code expression} or
+	 *         {@link #getDefaultValueForCondition()} if it is null or there's
+	 *         no {@code VariablesSystem} registered.
+	 */
+	protected boolean evaluateCondition(String expression) {
+		// Get the variables system
+		VariablesSystem variablesSystem = engine
+				.getSystem(VariablesSystem.class);
+
+		if (variablesSystem != null) {
+			try {
+				return variablesSystem.evaluateCondition(expression,
+						getDefaultValueForCondition());
+			} catch (IllegalArgumentException e) {
+				return getDefaultValueForCondition();
+			}
+		}
+
+		return getDefaultValueForCondition();
+	}
+
+	/**
+	 * Default value for expressions that are undefined or which cannot be
+	 * evaluated (e.g. due to syntax errors or because the VariablesSystem was
+	 * not registered). {@code true} by default. Subclasses may want to redefine
+	 * this method to return {@code false} if they want to avoid the execution
+	 * of code that depends on expressions when these are not present, for
+	 * example.
+	 */
+	protected boolean getDefaultValueForCondition() {
+		return true;
+	}
+}
---
+++ engine/core/src/test/java/es/eucm/ead/engine/tests/systems/ConditionalSystemTest.java
@@ -0,0 +1,143 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.tests.systems;
+
+import ashley.core.Entity;
+import ashley.core.Family;
+import ashley.core.PooledEngine;
+import es.eucm.ead.engine.EntitiesLoader;
+import es.eucm.ead.engine.GameLoop;
+import es.eucm.ead.engine.processors.VisibilityProcessor;
+import es.eucm.ead.engine.systems.ConditionalSystem;
+import es.eucm.ead.engine.systems.VisibilitySystem;
+import es.eucm.ead.engine.systems.variables.VariablesSystem;
+import es.eucm.ead.schema.components.VariableDef;
+import es.eucm.ead.schema.components.Visibility;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Javier Torrente on 19/04/14.
+ */
+public class ConditionalSystemTest {
+
+	protected GameLoop gameLoop;
+
+	private VariablesSystem variablesSystem;
+
+	private VariableDef variableDef = new VariableDef();
+
+	private VariableDef variableDef2 = new VariableDef();
+
+	@Before
+	public void setUp() {
+		gameLoop = new GameLoop();
+		variablesSystem = new VariablesSystem();
+
+		gameLoop.addSystem(variablesSystem);
+		gameLoop.addSystem(new VisibilitySystem(gameLoop));
+
+		// Add variables that will be referenced in the expressions of this
+		// test
+		variableDef.setType(VariableDef.Type.INTEGER);
+		variableDef.setInitialValue("-1");
+		variableDef.setName("testVariable");
+
+		variableDef2.setType(VariableDef.Type.STRING);
+		variableDef2.setInitialValue("a string");
+		variableDef2.setName("testVariable2");
+
+		List<VariableDef> variableDefList = new ArrayList<VariableDef>();
+		variableDefList.add(variableDef);
+		variableDefList.add(variableDef2);
+		variablesSystem.registerVariables(variableDefList);
+	}
+
+	@Test
+	public void test() {
+		ConditionalSystemForTest conditionalSystemForTest = new ConditionalSystemForTest(
+				gameLoop);
+		conditionalSystemForTest.testCondition("btrue", true, true);
+		conditionalSystemForTest.testCondition("btrue", false, true);
+
+		conditionalSystemForTest.testCondition("true", false, false);
+		conditionalSystemForTest.testCondition("true", true, true);
+
+		conditionalSystemForTest.testCondition("$" + variableDef.getName(),
+				true, false);
+		conditionalSystemForTest.testCondition("$" + variableDef.getName(),
+				false, false);
+
+		conditionalSystemForTest.testCondition("$" + variableDef2.getName(),
+				true, true);
+		conditionalSystemForTest.testCondition("$" + variableDef2.getName(),
+				false, false);
+
+		conditionalSystemForTest.testCondition(null, false, false);
+		conditionalSystemForTest.testCondition(null, true, true);
+	}
+
+	public static class ConditionalSystemForTest extends ConditionalSystem {
+
+		private boolean defaultValue;
+
+		public ConditionalSystemForTest(PooledEngine engine) {
+			super(engine, Family.getFamilyFor());
+		}
+
+		@Override
+		public void processEntity(Entity entity, float deltaTime) {
+		}
+
+		protected boolean getDefaultValueForCondition() {
+			return defaultValue;
+		}
+
+		public void testCondition(String expression, boolean defaultValue,
+				boolean expectedValue) {
+			this.defaultValue = defaultValue;
+			assertEquals(
+					"The value of the expression and the returned do not match",
+					expectedValue, evaluateCondition(expression));
+		}
+	}
+}
