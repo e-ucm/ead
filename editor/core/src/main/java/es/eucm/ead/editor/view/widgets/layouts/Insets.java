607bdadfe63d3b28ea2330273358b462e0c0c807
anserran <angelserranolaguna@gmail.com>
2014-04-27 14:36:33 +0200

    Added a horizontal layout

---
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/layouts/Insets.java
@@ -0,0 +1,106 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.view.widgets.layouts;
+
+public class Insets {
+
+	public float left;
+
+	public float right;
+
+	public float top;
+
+	public float bottom;
+
+	public void set(float value) {
+		left = right = top = bottom = value;
+	}
+
+	public float getLeft() {
+		return left;
+	}
+
+	public void setLeft(float left) {
+		this.left = left;
+	}
+
+	public float getRight() {
+		return right;
+	}
+
+	public void setRight(float right) {
+		this.right = right;
+	}
+
+	public float getTop() {
+		return top;
+	}
+
+	public void setTop(float top) {
+		this.top = top;
+	}
+
+	public float getBottom() {
+		return bottom;
+	}
+
+	public void setBottom(float bottom) {
+		this.bottom = bottom;
+	}
+
+	public void set(float left, float top, float right, float bottom) {
+		this.left = left;
+		this.top = top;
+		this.right = right;
+		this.bottom = bottom;
+	}
+
+	public float getWidth() {
+		return left + right;
+	}
+
+	public float getHeight() {
+		return top + bottom;
+	}
+
+	public void set(Insets insets) {
+		this.left = insets.left;
+		this.right = insets.right;
+		this.top = insets.top;
+		this.bottom = insets.bottom;
+	}
+}
