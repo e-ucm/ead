0ecda41ab417afd13785b52a982b63ed9ec481d3
anserran <angelserranolaguna@gmail.com>
2014-04-29 11:35:09 +0200

    Added test for IconButton and Separator widgets

---
+++ editor/desktop/src/test/java/es/eucm/ead/editor/widgets/IconButtonTest.java
@@ -0,0 +1,82 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.widgets;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.view.widgets.AbstractWidget;
+import es.eucm.ead.editor.view.widgets.IconButton;
+import es.eucm.ead.editor.view.widgets.Separator;
+import es.eucm.ead.editor.view.widgets.layouts.LeftRightLayout;
+
+/**
+ * Created by angel on 29/04/14.
+ */
+public class IconButtonTest extends AbstractWidgetTest {
+
+	private final String[] icons = new String[] { "save", null, "cut", "copy",
+			"paste", null, "undo", "redo" };
+
+	@Override
+	public AbstractWidget createWidget(Controller controller) {
+		controller.getApplicationAssets().loadSkin("skins/light/skin");
+		Skin skin = controller.getApplicationAssets().getSkin();
+
+		LeftRightLayout leftRightLayout = new LeftRightLayout(
+				skin.getDrawable("blank"));
+
+		for (String icon : icons) {
+			if (icon == null) {
+				leftRightLayout.left(new Separator(false, skin));
+			} else {
+				IconButton button = new IconButton(skin.getDrawable(icon
+						+ "24x24"), 5, skin);
+				leftRightLayout.left(button);
+			}
+		}
+
+		return leftRightLayout;
+	}
+
+	public static void main(String args[]) {
+		new LwjglApplication(new IconButtonTest(), "Test for TextArea", 800,
+				600);
+	}
+
+}
