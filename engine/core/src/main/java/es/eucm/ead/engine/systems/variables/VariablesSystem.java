79f5041e0051ea18b884e0ae95a731695f6c61ab
jtorrente <jtorrente@e-ucm.es>
2014-05-01 20:36:04 +0200

    Adding support in VariablesSystem for having local contexts. See javadoc for a detailed description on how it works.

--- engine/core/src/main/java/es/eucm/ead/engine/systems/variables/VariablesSystem.java
+++ engine/core/src/main/java/es/eucm/ead/engine/systems/variables/VariablesSystem.java
@@ -42,32 +42,78 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
 import es.eucm.ead.engine.EntitiesLoader;
+import com.badlogic.gdx.utils.Pools;
 import es.eucm.ead.engine.expressions.Expression;
 import es.eucm.ead.engine.expressions.ExpressionEvaluationException;
 import es.eucm.ead.engine.expressions.Parser;
 import es.eucm.ead.engine.expressions.operators.OperatorFactory;
-import es.eucm.ead.engine.systems.variables.VarsContext.Variable;
 import es.eucm.ead.schema.data.VariableDef;
 
 import java.util.List;
-import java.util.Map.Entry;
 
 /**
- * Deals with variables and expressions. Can set values and handles
- * {@link es.eucm.ead.engine.systems.variables.VariablesSystem.VariableListener}
- * . It also can evaluate expressions.
+ * Deals with variables and expressions. Can set variable values (
+ * {@link #setValue(String, String)}), which triggers notifications through
+ * {@link VariableListener}. It also can evaluate expressions (
+ * {@link #evaluateExpression(String)}) and conditions (
+ * {@link #evaluateCondition(String, boolean)}), which are just boolean
+ * expressions.
+ * 
+ * It also handles local and global {@link VarsContext}. By default, it creates
+ * a global {@code VarsContext} to hold user-defined variables and global scope
+ * variables like current language ({@link VarsContext#LANGUAGE_VAR}). New local
+ * contexts can be created to register local variables by calling
+ * {@link #pushLocalContext()} and {@link #localVar(String, Object)} afterwards.
+ * This is useful for setting the current owner {@code Entity} that is being
+ * processed as a variable ({@link VarsContext#THIS_VAR}) so its properties can
+ * be referenced in expressions to be evaluated. Once the current local context
+ * is not needed anymore, {@link #popLocalContext()} must be called to get it
+ * removed.
+ * 
+ * This is a typical usage example: Let's suppose the expression
+ * "(hastag $_this sTag1)" has to be evaluated, where "$_this" refers to the
+ * entity that wherever holds the expression. First, the
+ * {@link #pushLocalContext()} must be invoked to create a new local context on
+ * top of the current varsContext, which may be the global context if no other
+ * local context has been pushed or if those were already popped out. Then
+ * {@link #localVar(String, Object)} or simply
+ * {@link #localOwnerVar(ashley.core.Entity)} can be called to setup the entity
+ * variable in the recently created context. Since these methods return the same
+ * {@code VariablesSystem}, calls can be chained.
+ * 
+ * Then, {@code evaluateCondition("(hastag $_this sTag1)")} can be called and
+ * the system will be able to resolve $_this, since it has been registered as a
+ * local variable.
+ * 
+ * Finally, once the result of the condition evaluation is returned, the local
+ * context must be popped since it is not needed anymore using
+ * {@link #popLocalContext()}.
+ * 
+ * <pre>
+ *     VariablesSystem variablesSystem = ...
+ *     String expression = "(hastag $_this sTag1)";
+ *     Entity owner = ... //Entity that holds the expression. For example, if the expression is in an effect, it may be the entity that contains the EffectsComponent.
+ * 
+ *     boolean conditionResult =
+ *          variablesSystem.pushLocalContext().localOwnerVar(owner).evaluateCondition(expression, false);
+ *     variablesSystem.popLocalContext();
+ * 
+ *     if (conditionResult){
+ *         ...
+ *     } else {
+ *         ...
+ *     }
+ * </pre>
  */
 public class VariablesSystem extends EntitySystem {
 
-	/**
-	 * Reserved word for identifying the entity that holds an expression that is
-	 * being evaluated
-	 */
-	public static final String THIS = "this";
+	private static final String LOG_TAG = "VariablesSystem";
 
 	private Array<VariableListener> listeners;
 
 	private VarsContext varsContext;
+
+	private VarsContext globalContext;
 
 	private ObjectMap<String, Expression> expressionMap;
 
@@ -77,7 +123,8 @@ public class VariablesSystem extends EntitySystem {
 
 	public VariablesSystem(EntitiesLoader entitiesLoader) {
 		this.operatorFactory = new OperatorFactory(entitiesLoader);
-		this.varsContext = new VarsContext();
+		this.varsContext = Pools.obtain(VarsContext.class);
+		this.globalContext = this.varsContext;
 		this.expressionMap = new ObjectMap<String, Expression>();
 		this.pendingToNotify = new Array<String>();
 		this.listeners = new Array<VariableListener>();
@@ -85,28 +132,169 @@ public class VariablesSystem extends EntitySystem {
 	}
 
 	/**
-	 * Updates the context with the given "$this" entity.
+	 * Creates a new local {@code VarsContext} on top of current
+	 * {@link #varsContext}. Example: At any moment, the {@code VarsContext}
+	 * stack available may look like this:
 	 * 
-	 * @param dis
-	 *            New context. If {@code null}, nothing is done
+	 * <pre>
+	 *     varsContext-->localContext1
+	 *                        |
+	 *                   localContext2
+	 *                        |
+	 *                       ...
+	 *                        |
+	 *                   globalContext
+	 * </pre>
+	 * 
+	 * When {@link #pushLocalContext()} is called, then a new context is created
+	 * and it is linked to the context on top:
+	 * 
+	 * <pre>
+	 *     varsContext-->newLocalContext
+	 *                        |
+	 *                   localContext1
+	 *                        |
+	 *                   localContext2
+	 *                        |
+	 *                       ...
+	 *                        |
+	 *                   globalContext
+	 * </pre>
+	 * 
+	 * New local contexts predominate over other existing contexts when
+	 * resolving variables, since the resolving process always starts by the
+	 * context on top. If the variable is not found, then the next context is
+	 * checked. The global context is always the latest to be checked.
+	 * 
+	 * @return This VariablesSystem so {@link #pushLocalContext()},
+	 *         {@link #localVar(String, Object)} and
+	 *         {@link #setValue(String, String)} calls can be chained.
 	 */
-	private void setThisVariable(Entity dis) {
-		if (dis == null)
-			return;
-		if (!varsContext.hasVariable(THIS)) {
-			varsContext.registerVariable(THIS, dis, Entity.class);
+	public VariablesSystem pushLocalContext() {
+		VarsContext newLocalContext = Pools.obtain(VarsContext.class);
+		newLocalContext.setChild(varsContext);
+		varsContext = newLocalContext;
+		return this;
+	}
+
+	/**
+	 * Removes the current local {@code VarsContext} on top so its variables
+	 * won't be available. Example: Assuming the current {@code VarsContext}
+	 * stack looks like this:
+	 * 
+	 * <pre>
+	 *     varsContext-->localContext1
+	 *                        |
+	 *                   localContext2
+	 *                        |
+	 *                       ...
+	 *                        |
+	 *                   globalContext
+	 * </pre>
+	 * 
+	 * Calling {@link #popLocalContext()} results in getting localContext1 freed
+	 * and setting varsContext pointer to localContext2:
+	 * 
+	 * <pre>
+	 *     varsContext-->localContext2
+	 *                        |
+	 *                       ...
+	 *                        |
+	 *                   globalContext
+	 * </pre>
+	 * 
+	 * However, this is not performed if varsContext is pointing to
+	 * globalContext. This prevents the global context getting removed by
+	 * accident. Should this be attempted, an exception is thrown.
+	 * 
+	 * @throws java.lang.UnsupportedOperationException
+	 *             If there is no local context to be popped out, since the
+	 *             global context cannot be removed.
+	 */
+	public void popLocalContext() {
+		if (varsContext != globalContext) {
+			VarsContext child = varsContext.removeChild();
+			Pools.free(varsContext);
+			varsContext = child;
 		} else {
-			varsContext.setValue(THIS, dis);
+			Gdx.app.debug(LOG_TAG, "Cannot pop the global context!");
+			throw new UnsupportedOperationException(
+					"Cannot pop the global context! You may need to call pushLocalContext() first.");
 		}
 	}
 
 	/**
-	 * Clears entity context once it is not accessible
+	 * Registers a new local var to the current local vars context on top so it
+	 * can be used in further expression or condition evaluation.
+	 * 
+	 * @param name
+	 *            The name of the variable. Examples:
+	 *            {@link VarsContext#THIS_VAR},
+	 *            {@link VarsContext#RESERVED_ENTITY_VAR}.
+	 * @param value
+	 *            The object value for the variable.
+	 * @return This VariablesSystem so {@link #pushLocalContext()},
+	 *         {@link #localVar(String, Object)} and
+	 *         {@link #setValue(String, String)} calls can be chained.
+	 * @throws java.lang.UnsupportedOperationException
+	 *             if there is no local var context available, since
+	 *             globalContext cannot hold local variables.
 	 */
-	private void clearThisVariable() {
-		if (varsContext.hasVariable(THIS)) {
-			varsContext.setValue(THIS, null);
+	public VariablesSystem localVar(String name, Object value) {
+		if (varsContext != globalContext) {
+			varsContext.registerVariable(name, value);
+		} else {
+			Gdx.app.debug(LOG_TAG,
+					"Cannot add a local var to the global context!");
+			throw new UnsupportedOperationException(
+					"Cannot add a local var to the global context! You may need to call pushLocalContext() first.");
 		}
+		return this;
+	}
+
+	/**
+	 * Registers the given {@code owner} entity to the "$_this" reserved local
+	 * variable.
+	 * 
+	 * @param owner
+	 *            The entity that owns the expression. Registering the owner
+	 *            entity as a variable allows the expression to resolve entity's
+	 *            properties (e.g. a given tag) by using "$_this".
+	 * @return This VariablesSystem so {@link #pushLocalContext()},
+	 *         {@link #localVar(String, Object)} and
+	 *         {@link #setValue(String, String)} calls can be chained.
+	 * @throws java.lang.UnsupportedOperationException
+	 *             if there is no local var context available, since
+	 *             globalContext cannot hold local variables.
+	 */
+	public VariablesSystem localOwnerVar(Entity owner) {
+		localVar(VarsContext.THIS_VAR, owner);
+		return this;
+	}
+
+	/**
+	 * Registers the given {@code owner} entity to the "$_this" reserved local
+	 * variable and {@code otherEntity} to the "$_entity" reserved variable.
+	 * 
+	 * @param owner
+	 *            The entity that owns the expression. Registering the owner
+	 *            entity as a variable allows the expression to resolve entity's
+	 *            properties (e.g. a given tag) by using "$_this".
+	 * @param otherEntity
+	 *            Other entity whose properties may be needed for later
+	 *            expression evaluation.
+	 * @return This VariablesSystem so {@link #pushLocalContext()},
+	 *         {@link #localVar(String, Object)} and
+	 *         {@link #setValue(String, String)} calls can be chained.
+	 * @throws java.lang.UnsupportedOperationException
+	 *             if there is no local var context available, since
+	 *             globalContext cannot hold local variables.
+	 */
+	public VariablesSystem localOwnerAndEntityVars(Entity owner,
+			Entity otherEntity) {
+		localVar(VarsContext.THIS_VAR, owner);
+		localVar(VarsContext.RESERVED_ENTITY_VAR, otherEntity);
+		return this;
 	}
 
 	/**
@@ -138,14 +326,13 @@ public class VariablesSystem extends EntitySystem {
 	 *            the variable name. Cannot be {@code null}.
 	 * @param expression
 	 *            a valid expression. Cannot be {@code null}.
-	 * @param dis
-	 *            The entity that owns the expression (can be {@code null}).
-	 *            Passing a not {@code null} entity allows the expression to
-	 *            resolve entity's properties (e.g. a given tag).
+	 * @return This VariablesSystem so {@link #pushLocalContext()},
+	 *         {@link #localVar(String, Object)} and
+	 *         {@link #setValue(String, String)} calls can be chained.
 	 */
-	public void setValue(String variable, String expression, Entity dis) {
+	public VariablesSystem setValue(String variable, String expression) {
 		if (variable != null) {
-			Object value = evaluateExpression(expression, dis);
+			Object value = evaluateExpression(expression);
 			if (value != null) {
 				Object oldValue = varsContext.getValue(variable);
 				if (!value.equals(oldValue)) {
@@ -161,9 +348,10 @@ public class VariablesSystem extends EntitySystem {
 		}
 
 		else {
-			Gdx.app.error("VariablesSystem",
+			Gdx.app.error(LOG_TAG,
 					"Error setting value for variable: It cannot be null");
 		}
+		return this;
 	}
 
 	/**
@@ -173,13 +361,8 @@ public class VariablesSystem extends EntitySystem {
 	 * @param expression
 	 *            A valid not-null expression (see the wiki for more details on
 	 *            valid expressions).
-	 * @param dis
-	 *            The entity that owns the expression (can be {@code null}).
-	 *            Passing a not {@code null} entity allows the expression to
-	 *            resolve entity's properties (e.g. a given tag) by referring to
-	 *            reserved variable "$this".
 	 */
-	public Object evaluateExpression(String expression, Entity dis) {
+	public Object evaluateExpression(String expression) {
 		if (expression != null) {
 			// Variable assignation
 			Expression e = expressionMap.get(expression);
@@ -189,19 +372,15 @@ public class VariablesSystem extends EntitySystem {
 			}
 
 			try {
-				// Set reserved variable "this"
-				setThisVariable(dis);
 				Object value = e.evaluate(varsContext);
-				clearThisVariable();
 				return value;
 			} catch (ExpressionEvaluationException e1) {
-				Gdx.app.error("VariablesSystem", "Error evaluating "
-						+ expression, e1);
+				Gdx.app.error(LOG_TAG, "Error evaluating " + expression, e1);
 			}
 
 		} else {
 			Gdx.app.error(
-					"VariablesSystem",
+					LOG_TAG,
 					"Error setting value for variable: Neither variable nor expression should be null");
 		}
 		return null;
@@ -214,22 +393,17 @@ public class VariablesSystem extends EntitySystem {
 	 * 
 	 * @param expression
 	 *            The boolean expression
-	 * @param dis
-	 *            The entity that owns the expression (can be {@code null}).
-	 *            Passing a not {@code null} entity allows the expression to
-	 *            resolve the entity's properties (e.g. a given tag).
 	 * @param defaultValue
 	 *            The value to be returned if the expression is null (usually
 	 *            because it was not defined in the model) or if the expression
 	 *            cannot be evaluated to a boolean
 	 * @return The result of the evaluation
 	 */
-	public boolean evaluateCondition(String expression, Entity dis,
-			boolean defaultValue) {
+	public boolean evaluateCondition(String expression, boolean defaultValue) {
 		if (expression == null)
 			return defaultValue;
 
-		Object result = evaluateExpression(expression, dis);
+		Object result = evaluateExpression(expression);
 
 		if (result == null)
 			return defaultValue;
@@ -267,7 +441,7 @@ public class VariablesSystem extends EntitySystem {
 	 * Removes all variables from the system
 	 */
 	public void clear() {
-		varsContext.clear();
+		varsContext.reset();
 	}
 
 	/**
