d3131f43815a31a22b12ebc1018820d5a28ad890
RotaruDan <drotaru@ucm.es>
2014-05-05 23:43:37 +0200

    RepositoryGallery refactoring and javadocs. Now resources are fetched locally before downloading them.

--- editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/gallery/RepositoryGallery.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/builders/mockup/gallery/RepositoryGallery.java
@@ -42,7 +42,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -64,12 +66,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
-import com.badlogic.gdx.utils.ObjectMap.Entry;
 
+import es.eucm.ead.editor.assets.ApplicationAssets;
+import es.eucm.ead.editor.assets.EditorGameAssets;
 import es.eucm.ead.editor.control.Controller;
 import es.eucm.ead.editor.control.actions.editor.ChangeView;
 import es.eucm.ead.editor.control.actions.model.AddSceneElement;
 import es.eucm.ead.editor.model.Model;
+import es.eucm.ead.editor.model.Model.ModelListener;
+import es.eucm.ead.editor.model.events.LoadEvent;
 import es.eucm.ead.editor.view.builders.mockup.edition.SceneEdition;
 import es.eucm.ead.editor.view.builders.mockup.menu.InitialScreen;
 import es.eucm.ead.editor.view.listeners.ActionOnClickListener;
@@ -85,22 +90,44 @@ import es.eucm.ead.schema.entities.ModelEntity;
  * gallery grid.
  */
 public class RepositoryGallery extends BaseGallery<ElementButton> {
+	
+	/**
+	 * 25 s.
+	 */
+	private static final int TIMEOUT = 25000;
 
 	public static final String NAME = "mockup_repository_gallery";
 
 	private static final String IC_GO_BACK = "ic_goback";
-	private static final String REPOSITORY_FOLDER = "/onlineRepository";
-	private static final String THUMBNAILS_FOLDER = REPOSITORY_FOLDER
-			+ "/thumbnails";
-	private static final String THUMBNAIL_BINDINGS_FILE = "bindings.properties";
-	private static final String REPOSITORY_INDEX_URL = "http://repo-justusevim.rhcloud.com/elements.json";
+	private static final String ONLINE_REPO_TAG = "RepositoryGallery";
+
+	private static final String MOCKUP_PROJECTS_PATH = InitialScreen.MOCKUP_PROJECT_FILE
+			.file().getAbsolutePath();
+	private static final String REPOSITORY_FOLDER_NAME = "/onlineRepository";
+	private static final String REPOSITORY_FOLDER_PATH = MOCKUP_PROJECTS_PATH
+			+ REPOSITORY_FOLDER_NAME;
+	private static final String THUMBNAILS_FOLDER_NAME = "/thumbnails";
+	private static final String THUMBNAILS_FOLDER_PATH = REPOSITORY_FOLDER_PATH
+			+ THUMBNAILS_FOLDER_NAME;
+	private static final String THUMBNAIL_BINDINGS_FILE_NAME = "/bindings.properties";
+	private static final String THUMBNAIL_BINDINGS_FILE_PATH = THUMBNAILS_FOLDER_PATH
+			+ THUMBNAIL_BINDINGS_FILE_NAME;
+	private static final String ELEMENTS_FILE = REPOSITORY_FOLDER_PATH
+			+ "/elements.json";
+	private static final String REPOSITORY_ELEMENTS_URL = "http://repo-justusevim.rhcloud.com/elements.json";
 	private static final String REPOSITORY_THUMBNAILS_URL = "http://repo-justusevim.rhcloud.com/thumbnails.zip";
 
-	private String previousElements;
+	private String previousElements = "";
 
+	/**
+	 * Key {@link ModelEntity}'s title, value the {@link ElementButton} that
+	 * displays the {@link ModelEntity}. Used to process correctly
+	 * {@value #THUMBNAIL_BINDINGS_FILE_NAME}.
+	 */
 	private final ObjectMap<String, ElementButton> onlineElements = new ObjectMap<String, ElementButton>();
 
 	private TextButton updateButton;
+	private boolean listenerAdded = false;
 
 	@Override
 	public String getName() {
@@ -111,6 +138,17 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 	protected WidgetGroup centerWidget(Vector2 viewport, I18N i18n, Skin skin,
 			Controller controller) {
 		setSelectable(false);
+		if(!listenerAdded){
+			listenerAdded = true;
+			controller.getModel().addLoadListener(new ModelListener<LoadEvent>() {
+
+				@Override
+				public void modelChanged(LoadEvent event) {
+					previousElements = "";
+				}
+
+			});
+		}
 		return super.centerWidget(viewport, i18n, skin, controller);
 	}
 
@@ -165,8 +203,8 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 			Array<ElementButton> elements, Vector2 viewport, I18N i18n,
 			Skin skin) {
 		elements.clear();
-		for (Entry<String, ElementButton> elem : onlineElements.entries()) {
-			elements.add(elem.value);
+		for (ElementButton elem : onlineElements.values()) {
+			elements.add(elem);
 		}
 		return true;
 	}
@@ -192,15 +230,18 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 		super.release(controller);
 	}
 
+	/**
+	 * Tries to update the repository either by downloading new information when
+	 * changes are detected or by loading from local cache.
+	 * 
+	 * @param controller
+	 */
 	private void update(final Controller controller) {
 		setButtonDisabled(true, updateButton);
-		String url;
-		String httpMethod = Net.HttpMethods.GET;
-		String requestContent = null;
-		url = REPOSITORY_INDEX_URL;
-		HttpRequest httpRequest = new HttpRequest(httpMethod);
-		httpRequest.setUrl(url);
-		httpRequest.setContent(requestContent);
+		HttpRequest httpRequest = new HttpRequest(Net.HttpMethods.GET);
+		httpRequest.setUrl(REPOSITORY_ELEMENTS_URL);
+		httpRequest.setContent(null);
+		httpRequest.setTimeOut(TIMEOUT);
 		Gdx.net.sendHttpRequest(httpRequest, new HttpResponseListener() {
 
 			@Override
@@ -210,45 +251,26 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 				// need to post to main thread for UI updates
 
 				if (statusCode != 200) {
-					Gdx.app.log("NetAPITest",
+					Gdx.app.log(ONLINE_REPO_TAG,
 							"An error ocurred since statusCode is not OK, "
 									+ httpResponse);
 					return;
 				}
 
 				final String res = httpResponse.getResultAsString();
-				Gdx.app.log("NetAPITest", "Success ~> " + httpResponse + ", "
-						+ res);
+				Gdx.app.log(ONLINE_REPO_TAG, "Success");
 
-				if (previousElements == null || !previousElements.equals(res)) {
-					Gdx.app.postRunnable(new Runnable() {
-						public void run() {
-							@SuppressWarnings("unchecked")
-							ArrayList<ModelEntity> elems = controller
-									.getEditorGameAssets().fromJson(
-											ArrayList.class, res);
-							onlineElements.clear();
-							for (ModelEntity elem : elems) {
-								onlineElements.put(
-										Model.getComponent(elem, Note.class)
-												.getTitle(), new ElementButton(
-												controller.getPlatform()
-														.getSize(), controller
-														.getApplicationAssets()
-														.getI18N(), elem, null,
-												controller
-														.getApplicationAssets()
-														.getSkin(), controller,
-												AddSceneElement.class, elem));
-								// TODO download element resources and, when
-								// it's clicked, copy them to /images folder if
-								// they weren't previously there
-							}
-							RepositoryGallery.super.initialize(controller);
-						}
-					});
+				if (("".equals(previousElements) && !loadFromLocal(controller,
+						res)) || !previousElements.equals(res)) {
+					EditorGameAssets gameAssets = controller
+							.getEditorGameAssets();
+					gameAssets.absolute(ELEMENTS_FILE).writeString(res, false);
+					createFromString(res, controller);
 
-					sendDownloadRequest(controller);
+					sendDownloadRequest(
+							REPOSITORY_THUMBNAILS_URL,
+							gameAssets.absolute(REPOSITORY_FOLDER_PATH
+									+ "/thumbnails.zip"), controller);
 				} else {
 					setButtonDisabled(false, updateButton);
 				}
@@ -258,27 +280,95 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 
 			@Override
 			public void failed(Throwable t) {
-				Gdx.app.log("NetAPITest",
+				Gdx.app.log(ONLINE_REPO_TAG,
 						"Failed to perform the HTTP Request: ", t);
 
+				if (!loadFromLocal(controller, previousElements)) {
+					// TODO show notification to the user informing that we
+					// couldn't refresh the elements
+				}
 			}
 
 			@Override
 			public void cancelled() {
-				Gdx.app.log("NetAPITest", "HTTP request cancelled");
+				Gdx.app.log(ONLINE_REPO_TAG, "HTTP request cancelled");
 
 			}
 		});
 	}
 
-	private void sendDownloadRequest(final Controller controller) {
-		String url;
-		String httpMethod = Net.HttpMethods.GET;
-		String requestContent = null;
-		url = REPOSITORY_THUMBNAILS_URL;
-		HttpRequest httpRequest = new HttpRequest(httpMethod);
-		httpRequest.setUrl(url);
-		httpRequest.setContent(requestContent);
+	/**
+	 * 
+	 * @param controller
+	 * @param updatedJson
+	 *            the most updated {@link #ELEMENTS_FILE} info. Usually the most
+	 *            recently downloaded.
+	 * @return true if we could load the elements from local path.
+	 */
+	private boolean loadFromLocal(Controller controller, String updatedJson) {
+		EditorGameAssets gameAssets = controller.getEditorGameAssets();
+		FileHandle elementsFile = gameAssets.absolute(ELEMENTS_FILE);
+		if (elementsFile.exists()) {
+			String localJson = elementsFile.readString();
+			if (!"".equals(updatedJson) && !localJson.equals(updatedJson)) {
+				return false;
+			}
+			createFromString(localJson, controller);
+			processBindings(gameAssets.absolute(THUMBNAIL_BINDINGS_FILE_PATH),
+					controller);
+			previousElements = updatedJson;
+			setButtonDisabled(false, updateButton);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Tries to fill the {@link #onlineElements} by creating {@link ModelEntity
+	 * ModelEntities} from the jsonString.
+	 * 
+	 * @param jsonString
+	 *            must be correctly formated as a {@link List list of
+	 *            ModelEntities}.
+	 * @param controller
+	 */
+	private void createFromString(final String jsonString,
+			final Controller controller) {
+		EditorGameAssets gameAssets = controller.getEditorGameAssets();
+
+		@SuppressWarnings("unchecked")
+		ArrayList<ModelEntity> elems = gameAssets.fromJson(ArrayList.class,
+				jsonString);
+
+		onlineElements.clear();
+		ApplicationAssets appAssets = controller.getApplicationAssets();
+		I18N i18n = appAssets.getI18N();
+		Skin skin = appAssets.getSkin();
+		for (ModelEntity elem : elems) {
+			onlineElements.put(Model.getComponent(elem, Note.class).getTitle(),
+					new ElementButton(controller.getPlatform().getSize(), i18n,
+							elem, null, skin, controller,
+							AddSceneElement.class, elem));
+			// TODO download element resources and, when
+			// it's clicked, copy them to /images folder if
+			// they weren't previously there
+		}
+		RepositoryGallery.super.initialize(controller);
+	}
+
+	/**
+	 * Sends a download request from an URL, saving it to dstFile.
+	 * 
+	 * @param fromURL
+	 * @param dstFile
+	 * @param controller
+	 */
+	private void sendDownloadRequest(String fromURL, final FileHandle dstFile,
+			final Controller controller) {
+		HttpRequest httpRequest = new HttpRequest(Net.HttpMethods.GET);
+		httpRequest.setUrl(fromURL);
+		httpRequest.setContent(null);
+		httpRequest.setTimeOut(TIMEOUT);
 		Gdx.net.sendHttpRequest(httpRequest, new HttpResponseListener() {
 
 			@Override
@@ -288,45 +378,42 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 				// need to post to main thread for UI updates
 
 				if (statusCode != 200) {
-					Gdx.app.log("NetAPITest",
-							"An error ocurred since statusCode is not OK, "
-									+ httpResponse);
+					Gdx.app.log(ONLINE_REPO_TAG,
+							"An error ocurred since statusCode(" + statusCode
+							+ ") is not OK(200)");
 					return;
 				}
 
-				download(controller, httpResponse);
+				download(dstFile, controller, httpResponse);
 			}
 
 			@Override
 			public void failed(Throwable t) {
-				Gdx.app.log("NetAPITest",
+				Gdx.app.log(ONLINE_REPO_TAG,
 						"Failed to perform the HTTP Request: ", t);
 
 			}
 
 			@Override
 			public void cancelled() {
-				Gdx.app.log("NetAPITest", "HTTP request cancelled");
+				Gdx.app.log(ONLINE_REPO_TAG, "HTTP request cancelled");
 
 			}
 		});
 	}
 
-	private void download(Controller controller, HttpResponse httpResponse) {
+	private void download(FileHandle dstFile, Controller controller,
+			HttpResponse httpResponse) {
+		int count = -1;
+		byte data[] = new byte[4096];
+
 		InputStream input = null;
 		OutputStream output = null;
 		try {
 			input = httpResponse.getResultAsStream();
-			FileHandle outFile = Gdx.files
-					.absolute(InitialScreen.MOCKUP_PROJECT_FILE.file()
-							.getAbsolutePath()
-							+ REPOSITORY_FOLDER
-							+ "/thumbnails.zip");
-			output = outFile.write(false);
+			output = dstFile.write(false);
 
-			byte data[] = new byte[4096];
 			// long total = 0;
-			int count;
 			while ((count = input.read(data)) != -1) {
 				// allow canceling with back button
 				/*
@@ -341,74 +428,18 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 				output.write(data, 0, count);
 			}
 
-			FileHandle unzippedThumbnails = Gdx.files
-					.absolute(InitialScreen.MOCKUP_PROJECT_FILE.file()
-							.getAbsolutePath() + THUMBNAILS_FOLDER);
-			if (!unzippedThumbnails.exists()) {
-				unzippedThumbnails.mkdirs();
-			}
-			ZipInputStream zin = null;
-			FileOutputStream fout = null;
-			try {
-				zin = new ZipInputStream(outFile.read());
+			FileHandle unzippedThumbnails = controller.getEditorGameAssets()
+					.absolute(THUMBNAILS_FOLDER_PATH);
 
-				ZipEntry ze = null;
-				while ((ze = zin.getNextEntry()) != null) {
-					fout = new FileOutputStream(unzippedThumbnails.file()
-							.getAbsolutePath() + "/" + ze.getName());
-					while ((count = zin.read(data)) != -1) {
-						fout.write(data, 0, count);
-					}
-
-					zin.closeEntry();
-					fout.close();
-					fout = null;
-				}
-			} catch (Exception e) {
-				Gdx.app.error("NetAPITest", "Exception", e);
-			} finally {
-				try {
-					if (fout != null)
-						fout.close();
-					if (zin != null)
-						zin.close();
-				} catch (IOException ignored) {
-					Gdx.app.error("NetAPITest",
-							"This exception should be ignored", ignored);
-				}
-			}
-			outFile.delete();
+			unzipFile(dstFile, unzippedThumbnails, data, true);
 
 			FileHandle bindings = unzippedThumbnails
-					.child(THUMBNAIL_BINDINGS_FILE);
-			InputStream is = null;
-			Properties props = null;
-			try {
-				props = new Properties();
-
-				props.load(is = bindings.read());
-			} catch (IOException ioe) {
-				Gdx.app.error("NetAPITest",
-						"Error loading bindings properties", ioe);
-			} finally {
-				try {
-					if (is != null)
-						is.close();
-				} catch (IOException ignored) {
-					Gdx.app.error("NetAPITest",
-							"This exception should be ignored", ignored);
-				}
-			}
-
-			for (java.util.Map.Entry<Object, Object> prop : props.entrySet()) {
-				controller.getEditorGameAssets().get(
-						".." + THUMBNAILS_FOLDER + "/"
-								+ prop.getValue().toString(), Texture.class,
-						new ThumbnailLoadedListener(prop.getKey().toString()));
-			}
+					.child(THUMBNAIL_BINDINGS_FILE_NAME);
+			processBindings(bindings, controller);
 
 		} catch (Exception e) {
-			Gdx.app.error("NetAPITest", "Exception", e);
+			Gdx.app.error(ONLINE_REPO_TAG, "Exception while downloading file "
+					+ dstFile.toString(), e);
 		} finally {
 			try {
 				if (output != null)
@@ -416,25 +447,120 @@ public class RepositoryGallery extends BaseGallery<ElementButton> {
 				if (input != null)
 					input.close();
 			} catch (IOException ignored) {
-				Gdx.app.error("NetAPITest", "This exception should be ignored",
-						ignored);
+				Gdx.app.error(ONLINE_REPO_TAG,
+						"This exception should be ignored", ignored);
 			}
 		}
 
 		setButtonDisabled(false, updateButton);
 	}
 
+	/**
+	 * Unzips a .zip file.
+	 * 
+	 * @param zipFile
+	 * @param outDir
+	 * @param data
+	 * @param deleteZipFile
+	 */
+	private void unzipFile(FileHandle zipFile, FileHandle outDir, byte data[],
+			boolean deleteZipFile) {
+		if (!outDir.exists()) {
+			outDir.mkdirs();
+		}
+		String outPath = outDir.file().getAbsolutePath() + "/";
+
+		ZipEntry ze = null;
+		ZipInputStream zin = null;
+		FileOutputStream fout = null;
+		int count = -1;
+		try {
+			zin = new ZipInputStream(zipFile.read());
+			while ((ze = zin.getNextEntry()) != null) {
+				fout = new FileOutputStream(outPath + ze.getName());
+				while ((count = zin.read(data)) != -1) {
+					fout.write(data, 0, count);
+				}
+
+				zin.closeEntry();
+				fout.close();
+				fout = null;
+			}
+		} catch (Exception e) {
+			Gdx.app.error(ONLINE_REPO_TAG, "Exception while unzipping file "
+					+ zipFile.toString(), e);
+		} finally {
+			try {
+				if (fout != null)
+					fout.close();
+				if (zin != null)
+					zin.close();
+			} catch (IOException ignored) {
+				Gdx.app.error(ONLINE_REPO_TAG,
+						"This exception should be ignored", ignored);
+			}
+		}
+		if (deleteZipFile)
+			zipFile.delete();
+	}
+
+	/**
+	 * Reads and loads bindingsFile to display the thumbnails.
+	 * 
+	 * @param bindingsFile
+	 * @param controller
+	 */
+	private void processBindings(FileHandle bindingsFile, Controller controller) {
+		InputStream is = null;
+		Properties props = null;
+		try {
+			props = new Properties();
+
+			props.load(is = bindingsFile.read());
+		} catch (IOException ioe) {
+			Gdx.app.error(ONLINE_REPO_TAG, "Error loading bindings properties",
+					ioe);
+		} finally {
+			try {
+				if (is != null)
+					is.close();
+			} catch (IOException ignored) {
+				Gdx.app.error(ONLINE_REPO_TAG,
+						"This exception should be ignored", ignored);
+			}
+		}
+
+		String thumbnailsPath = ".." + REPOSITORY_FOLDER_NAME
+				+ THUMBNAILS_FOLDER_NAME + "/";
+		EditorGameAssets gameAssets = controller.getEditorGameAssets();
+		Set<java.util.Map.Entry<Object, Object>> propsSet = props.entrySet();
+		for (java.util.Map.Entry<Object, Object> prop : propsSet) {
+			gameAssets.get(thumbnailsPath + prop.getValue().toString(),
+					Texture.class, new ThumbnailLoadedListener(prop.getKey()
+							.toString()));
+		}
+	}
+
+	/**
+	 * This listener sets the thumbnail icon to the linked {@link ElementButton}
+	 * . The binding relation is defined via {@link #onlineElements}.
+	 */
 	private class ThumbnailLoadedListener implements
-			AssetLoadedCallback<Texture> {
+	AssetLoadedCallback<Texture> {
 		private String title;
 
+		/**
+		 * This listener sets the thumbnail icon to the linked
+		 * {@link ElementButton}. The binding relation is defined via
+		 * {@link #onlineElements}.
+		 */
 		public ThumbnailLoadedListener(String title) {
 			this.title = title;
 		}
 
 		@Override
 		public void loaded(String fileName, Texture asset) {
-			onlineElements.get(title).setIcon(asset);
+			onlineElements.get(this.title).setIcon(asset);
 		}
 	}
 }
