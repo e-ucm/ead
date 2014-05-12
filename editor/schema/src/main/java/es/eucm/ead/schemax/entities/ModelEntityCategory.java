7f4518235fbb21b84617654bee9078fb3300aeed
anserran <angelserranolaguna@gmail.com>
2014-05-12 20:59:21 +0200

    Model now uses complete paths as ids. Also, some convenient methods

--- editor/schema/src/main/java/es/eucm/ead/schemax/entities/ModelEntityCategory.java
+++ editor/schema/src/main/java/es/eucm/ead/schemax/entities/ModelEntityCategory.java
@@ -36,179 +36,66 @@
  */
 package es.eucm.ead.schemax.entities;
 
-import java.util.Iterator;
-import java.util.regex.Pattern;
 import es.eucm.ead.schema.entities.ModelEntity;
 import es.eucm.ead.schemax.GameStructure;
-import es.eucm.ead.schemax.JsonExtension;
 
 /**
- * Utility to identify different types of {@link ModelEntity} categories. Also
- * provides convenient object and class methods related to entity categories:
- * 
- * <ul>
- * <li>For getting relative paths for entities of a given category. See:
- * <ul>
- * <li>{@link #getRelativePath()}</li>
- * <li>{@link #getRelativeEntityPath(String)}</li>
- * <li>{@link #getRelativeEntityPath()}</li>
- * <li>{@link #getRelativePathOf(String)}</li>
- * </ul>
- * </li>
- * <li>For finding the category of an entity, or if an entity belongs to a given
+ * Utility to identify different types of {@link ModelEntity} categories. For
+ * finding the category of an entity, or if an entity belongs to a given
  * category. See:
  * <ul>
  * <li>{@link #isOfCategory(String)}</li>
  * <li>{@link #getCategoryOf(String)}</li>
  * </ul>
- * </li>
- * <li>For building new Ids for a given {@code ModelEntityCategory}. See
- * {@link #getIdIterator()}</li>
- * </ul>
  * 
- * Each {@code ModelEntityCategory} has also the next properties that can be
- * accessed (read-only):
- * <ul>
- * <li>{@link #getCategoryName()}. The name of the category (e.g. "game" or
- * "scene"), which is used to define valid ids for entities of this type.</li>
- * <li>{@link #getGreedy()}. The number of entities of this category that a game
- * can have. It can take values {@link #GREEDY_ONE} (e.g. "game") or
- * {@link #GREEDY_MANY} (e.g. "scene").</li>
- * <li>{@link #getRelativePath()}. The relative path for the directory that
- * stores all the entities of this type in the game directory. Examples: "" for
- * {@link #GAME}, "scenes/" for {@link #SCENE}.</li>
- * </ul>
+ * Normally, ids will be project relative paths, pointing to where entities are
+ * stored. {@link #getNamePrefix()} returns a hint to create new ids of this
+ * category. This prefix appended to category prefix can be used as starter to
+ * create new ids, although is not mandatory. The id only requirement to belong
+ * to a category is that it starts with the category prefix.
  */
 public enum ModelEntityCategory implements GameStructure {
 
-	SCENE("scene", SCENES_PATH, getGreedyMany()),
+	SCENE(SCENES_PATH, "scene"),
 
-	GAME("game", "", getGreedyOne());
+	GAME(GAME_FILE, null);
 
-	/**
-	 * Indicates that this entity type can appear only once in the game project.
-	 */
-	public static final int GREEDY_ONE = 1;
+	private String categoryPrefix;
 
-	/**
-	 * This entity type can appear one or more times in the game project.
-	 */
-	public static final int GREEDY_MANY = Integer.MAX_VALUE;
+	private String namePrefix;
 
-	private String categoryName;
-	private Pattern regex;
-	private String relativePath;
-	private int greedy;
-
-	private ModelEntityCategory(String categoryName, String relativePath,
-			int greedy) {
-		this.categoryName = categoryName;
-		this.relativePath = toCanonicalPath(relativePath);
-		this.greedy = greedy;
-		buildRegex();
-	}
-
-	private String toCanonicalPath(String path) {
-		return path == null ? null : path.replaceAll("\\\\", "/");
-	}
-
-	/*
-	 * Builds the regex for the category. GREEDY_ONE -> categoryName(.json)?
-	 * GREEDY_MANY -> categoryNameXXX(.json)? where XXX is an integer
-	 */
-	private void buildRegex() {
-		String jsonRegexString = getJsonRegexString();
-		String greedyPart = "";
-		if (greedy == GREEDY_MANY) {
-			greedyPart = "\\d+";
-		}
-		String optionalJsonPart = "(" + jsonRegexString + ")?";
-		String regexString = categoryName + greedyPart + optionalJsonPart;
-		this.regex = Pattern.compile(regexString);
+	private ModelEntityCategory(String categoryPrefix, String namePrefix) {
+		this.categoryPrefix = categoryPrefix;
+		this.namePrefix = namePrefix;
 	}
 
 	@Override
 	public String toString() {
-		return getCategoryName();
+		return getCategoryPrefix();
 	}
 
 	/**
-	 * Returns the name for this category (e.g. "game" or "scene"). The id of
-	 * all entities belonging to this category will start by this categoryName
-	 * (e.g. "game", "scene0").
+	 * Returns the prefix for this category (e.g. "game.json" or "scenes/"). The
+	 * id of all entities belonging to this category will start by this
+	 * categoryPrefix (e.g. "scenes/scene0.json").
 	 */
-	public String getCategoryName() {
-		return categoryName;
+	public String getCategoryPrefix() {
+		return categoryPrefix;
 	}
 
 	/**
-	 * Returns the number of entities of this category a game can have. Possible
-	 * values: {@link #GREEDY_ONE}: appears only once. Example: {@link #GAME}
-	 * {@link #GREEDY_MANY}: appears once or many times. Example: {@link #SCENE}
+	 * @return a hint to create new ids of this category. This prefix appended
+	 *         to category prefix can be used as starter to create new ids,
+	 *         although is not mandatory. The id only requirement to belong to a
+	 *         category is that it starts with the category prefix
 	 */
-	public int getGreedy() {
-		return greedy;
-	}
-
-	/**
-	 * Returns the relative path for the main directory that stores all entities
-	 * of this category in the game project folder. Examples:
-	 * 
-	 * "" for {@link #GAME} (root dir) "scenes/" for {@link #SCENE}
-	 */
-	public String getRelativePath() {
-		return relativePath;
-	}
-
-	/**
-	 * Returns the relative path for the file that stores the entity with the
-	 * given {@code entityId} in the game project folder, including json
-	 * extension.
-	 * 
-	 * Note: this method does not check that the provided {@code entityId} is
-	 * valid, just appends it.
-	 * 
-	 * @param entityId
-	 *            The id of the entity which relative path is being resolved
-	 * @return The relative path including json extension. Examples.
-	 *         "game.json", "scenes/scene0.json"
-	 */
-	public String getRelativeEntityPath(String entityId) {
-		String entityIdRelativePath;
-		entityId = toCanonicalPath(entityId);
-		if (entityId.startsWith("/"))
-			entityId = entityId.substring(1, entityId.length());
-
-		entityIdRelativePath = relativePath + entityId;
-		entityIdRelativePath = JsonExtension.addJsonEnd(entityIdRelativePath);
-		return entityIdRelativePath;
-	}
-
-	/**
-	 * Returns the relative path of the single entity of this category. Can only
-	 * be invoked in categories where {@link #getGreedy()} is
-	 * {@link #GREEDY_ONE}.
-	 * 
-	 * @return The relative path for the single entity. Example: "game.json"
-	 * @throws java.lang.UnsupportedOperationException
-	 *             if this category may contain more than one entity.
-	 */
-	public String getRelativeEntityPath() {
-		if (greedy != GREEDY_ONE) {
-			throw new UnsupportedOperationException(
-					"The method getRelativeEntityPath() is only available for types of entities that can appear only once.");
-		}
-		return getRelativeEntityPath(categoryName);
+	public String getNamePrefix() {
+		return namePrefix;
 	}
 
 	/**
 	 * Determines if the given {@code id} is of the entity type represented by
-	 * this {@link ModelEntityCategory}.
-	 * 
-	 * Internally uses a regex that is associated to each
-	 * {@link ModelEntityCategory}.
-	 * 
-	 * Examples:
+	 * this {@link ModelEntityCategory}. Examples:
 	 * 
 	 * <table>
 	 * <tr>
@@ -217,7 +104,7 @@ public enum ModelEntityCategory implements GameStructure {
 	 * </tr>
 	 * <tr>
 	 * <td>
-	 * ModelEntityCategory.SCENE.isOfCategory("scene3");</td>
+	 * ModelEntityCategory.SCENE.isOfCategory("scenes/scene3.json");</td>
 	 * <td>
 	 * true</td>
 	 * </tr>
@@ -229,68 +116,33 @@ public enum ModelEntityCategory implements GameStructure {
 	 * </tr>
 	 * <tr>
 	 * <td>
-	 * ModelEntityCategory.SCENE.isOfCategory("scene3.JSON");</td>
+	 * ModelEntityCategory.SCENE.isOfCategory("scenes/scene3.JSON");</td>
 	 * <td>
 	 * true</td>
 	 * </tr>
 	 * <tr>
 	 * <td>
-	 * ModelEntityCategory.GAME.isOfCategory("game");</td>
+	 * ModelEntityCategory.GAME.isOfCategory("game.json");</td>
 	 * <td>
 	 * true</td>
 	 * </tr>
 	 * </table>
 	 * 
+	 * @param id
+	 *            id to test. Can not be null
 	 * @return {@code true} if the given {@code id} is of this type of entity,
 	 *         false otherwise. The test is non-case sensitive
-	 * @throws NullPointerException
-	 *             if {@code id} is null
 	 */
 	public boolean isOfCategory(String id) {
-		if (id == null) {
-			throw new NullPointerException(
-					"ModelEntityCategory.isOfCategory(String) does not accept nulls");
-		}
-		return regex.matcher(id.toLowerCase()).matches();
-	}
-
-	/**
-	 * Returns an {@link Iterator} for getting valid ids for entities of this
-	 * category. Convenient for creating new entities without manipulating ids.
-	 */
-	public Iterator<String> getIdIterator() {
-		return new IdIterator();
-	}
-
-	/**
-	 * Resolves the relative path in the game folder for the entity with the
-	 * given {@code id}. First, it determines which category this id belongs to.
-	 * For example, if {@code id} is "scene0" this method will assume it is a
-	 * scene and returns "scenes/scene0.json".
-	 * 
-	 * @param id
-	 *            The id of the {@link ModelEntity} whose relative path is to be
-	 *            found (e.g. "scene0"). Note: the validity of the id given is
-	 *            not checked.
-	 * @return The relative path for the given {@code id} (e.g.
-	 *         scenes/scene0.json). Returns {@code null} if the id does not
-	 *         correspond to any known {@code ModelEntity}.
-	 */
-	public static String getRelativePathOf(String id) {
-		for (ModelEntityCategory category : ModelEntityCategory.values()) {
-			if (category.isOfCategory(id)) {
-				return category.getRelativeEntityPath(id);
-			}
-		}
-		return null;
+		return id.startsWith(categoryPrefix);
 	}
 
 	/**
 	 * Finds the category the given {@code id} belongs to.
 	 * 
 	 * @param id
-	 *            The id that is being searched (e.g. "scene0", "game.json",
-	 *            etc.).
+	 *            The id that is being searched (e.g. "scenes/scene0.json",
+	 *            "game.json", etc.).
 	 * @return The matching category, or {@code null} if the id does not match
 	 *         any {@link ModelEntityCategory}.
 	 */
@@ -302,43 +154,4 @@ public enum ModelEntityCategory implements GameStructure {
 		}
 		return null;
 	}
-
-	private static int getGreedyOne() {
-		return GREEDY_ONE;
-	}
-
-	private static int getGreedyMany() {
-		return GREEDY_MANY;
-	}
-
-	private static String getJsonRegexString() {
-		return Pattern.quote(JsonExtension.DOT_JSON);
-	}
-
-	public class IdIterator implements Iterator<String> {
-
-		private int counter = 0;
-
-		@Override
-		public boolean hasNext() {
-			return counter < greedy;
-		}
-
-		@Override
-		public String next() {
-			String nextId = null;
-			if (greedy == GREEDY_ONE) {
-				nextId = categoryName;
-			} else if (greedy == GREEDY_MANY) {
-				nextId = categoryName + counter;
-			}
-			counter++;
-			return nextId;
-		}
-
-		@Override
-		public void remove() {
-		}
-	};
-
 }
