7419f1e33f626d12203bd2fed820904e2433504a
RotaruDan <drotaru@ucm.es>
2014-05-20 19:04:16 +0200

    Moved Ttf2FntGenerator to ead-maven-plugin.

--- build-tools/maven/src/main/java/es/eucm/ead/maven/Ttf2FntGenerator.java
+++ build-tools/maven/src/main/java/es/eucm/ead/maven/Ttf2FntGenerator.java
@@ -34,7 +34,7 @@
  *      You should have received a copy of the GNU Lesser General Public License
  *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
  */
-package es.eucm.ead.buildtools;
+package es.eucm.ead.maven;
 
 import java.io.File;
 
@@ -48,9 +48,9 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFont
 import com.badlogic.gdx.tools.bmfont.BitmapFontWriter;
 
 /**
- * Writing a generated font to files allows the possibility of only generating
- * the fonts when they are missing, otherwise loading from a previously
- * generated file. Which is faster than generating a new font every time.
+ * Writing a generated font to files allows the possibility of
+ * only generating the fonts when they are missing, otherwise loading from a
+ * previously generated file. Which is faster than generating a new font every time.
  */
 public class Ttf2FntGenerator {
 	private static final String TAG = "Ttf2FntGenerator";
@@ -71,9 +71,8 @@ public class Ttf2FntGenerator {
 	 *            names
 	 * @param fontSize
 	 *            size of font
-	 * 
-	 * @param destiny
-	 *            location of the destiny folder
+	 *            
+	 * @param destiny location of the destiny folder
 	 */
 	public void createFont(FileHandle fontFile, String fontName, int fontSize,
 			FileHandle destiny) {
@@ -114,7 +113,7 @@ public class Ttf2FntGenerator {
 			String fontName, PixmapPacker packer, FileHandle destiny) {
 		FileHandle fontFile = Gdx.files.absolute(destiny.file()
 				.getAbsolutePath() + File.separator + fontName + ".fnt"); // .fnt
-		// path
+		// path		
 
 		BitmapFontWriter.setOutputFormat(BitmapFontWriter.OutputFormat.Text);
 
@@ -129,9 +128,8 @@ public class Ttf2FntGenerator {
 
 	/**
 	 * Set the width and height of the png files to which the fonts will be
-	 * saved.
-	 * 
-	 * @return
+	 * saved. 
+	 * @return 
 	 */
 	public Ttf2FntGenerator setPageSize(int size) {
 		pageSize = size;
@@ -140,7 +138,6 @@ public class Ttf2FntGenerator {
 
 	/**
 	 * See {@link #setPageSize(int)}.
-	 * 
 	 * @return
 	 */
 	public int getPageSize() {
