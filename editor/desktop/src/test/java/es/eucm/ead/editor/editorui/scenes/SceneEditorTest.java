1fae1823ef3b5416ef807229691a3990efd86098
anserran <angelserranolaguna@gmail.com>
2014-05-21 17:35:36 +0200

    Added executable test for SceneEditor

---
+++ editor/desktop/src/test/java/es/eucm/ead/editor/editorui/scenes/SceneEditorTest.java
@@ -0,0 +1,58 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.editorui.scenes;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+import es.eucm.ead.editor.editorui.EditorUITest;
+import es.eucm.ead.editor.ui.scenes.SceneEditor;
+
+public class SceneEditorTest extends EditorUITest {
+	@Override
+	protected void builUI(Group root) {
+		SceneEditor sceneEditor = new SceneEditor(controller);
+		sceneEditor.setFillParent(true);
+		root.addActor(sceneEditor);
+	}
+
+	public static void main(String[] args) {
+		new LwjglApplication(new SceneEditorTest(), "Scene Editor test", 1000,
+				600);
+	}
+
+}
