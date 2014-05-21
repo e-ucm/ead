55e0808d72c2b69a1b0bcd0e60b002aacf4e65cb
anserran <angelserranolaguna@gmail.com>
2014-05-21 17:31:26 +0200

    Added ModelActions to perform changes in groups edited in GroupEditor

---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/model/scene/CreateGroup.java
@@ -0,0 +1,141 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions.model.scene;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.utils.Array;
+
+import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.control.actions.ModelAction;
+import es.eucm.ead.editor.control.commands.CompositeCommand;
+import es.eucm.ead.editor.control.commands.ListCommand.AddToListCommand;
+import es.eucm.ead.editor.model.Model;
+import es.eucm.ead.engine.GameLoop;
+import es.eucm.ead.engine.entities.EngineEntity;
+import es.eucm.ead.schema.entities.ModelEntity;
+
+/**
+ * Creates a group performing the necessary commands over the
+ * {@link ModelEntity}s associated to the arguments.
+ * <dl>
+ * <dt><strong>Arguments</strong></dt>
+ * <dd><strong>args[0]</strong> <em>{@link Group}</em> the parent of the new
+ * group</dd>
+ * <dd><strong>args[1]</strong> <em>{@link Group}</em> the recently created
+ * group</dd>
+ * <dd><strong>args[2]</strong> <em>{@link Array}</em> an array with
+ * {@link Actor}s forming the new group</dd>
+ * </dl>
+ */
+public class CreateGroup extends ModelAction {
+
+	private Array<ModelEntity> tmpEntities = new Array<ModelEntity>();
+
+	private GameLoop gameLoop;
+
+	private RemoveChildrenFromEntity removeChildrenFromEntity;
+
+	private MultipleActorTransformToEntity multipleActorTransformToEntity;
+
+	public CreateGroup() {
+		super(true, false, Group.class, Group.class, Array.class);
+	}
+
+	@Override
+	public void initialize(Controller controller) {
+		super.initialize(controller);
+		gameLoop = controller.getGameLoop();
+		removeChildrenFromEntity = controller.getActions().getAction(
+				RemoveChildrenFromEntity.class);
+		multipleActorTransformToEntity = controller.getActions().getAction(
+				MultipleActorTransformToEntity.class);
+	}
+
+	@Override
+	public CompositeCommand perform(Object... args) {
+		Group parent = (Group) args[0];
+		Group newGroup = (Group) args[1];
+		Array<Actor> grouped = (Array<Actor>) args[2];
+
+		ModelEntity parentEntity = Model.getModelEntity(parent);
+
+		tmpEntities.clear();
+		tmpEntities.add(parentEntity);
+
+		// Remove grouped children from their old parent
+		for (Actor actor : grouped) {
+			ModelEntity entity = Model.getModelEntity(actor);
+			if (entity != null) {
+				tmpEntities.add(entity);
+			}
+		}
+		CompositeCommand command = removeChildrenFromEntity
+				.perform(tmpEntities.items);
+
+		// Create entity for new group
+		ModelEntity newGroupEntity = new ModelEntity();
+		newGroupEntity.setX(newGroup.getX());
+		newGroupEntity.setY(newGroup.getY());
+		newGroupEntity.setOriginX(newGroup.getOriginX());
+		newGroupEntity.setOriginY(newGroup.getOriginY());
+		newGroupEntity.setScaleY(newGroup.getScaleY());
+		newGroupEntity.setScaleX(newGroup.getScaleX());
+		newGroupEntity.setRotation(newGroup.getRotation());
+
+		EngineEntity engineEntity = gameLoop.createEntity();
+		engineEntity.setGroup(newGroup);
+		engineEntity.setModelEntity(newGroupEntity);
+
+		// Add new group to parent. To listen correctly to events, the group
+		// must be added first to the parent, and then children to the new group
+		command.addCommand(new AddToListCommand(parentEntity.getChildren(),
+				newGroupEntity));
+
+		for (Actor actor : grouped) {
+			ModelEntity entity = Model.getModelEntity(actor);
+			if (entity != null) {
+				command.addCommand(new AddToListCommand(newGroupEntity
+						.getChildren(), entity));
+			}
+		}
+
+		command.addAll(multipleActorTransformToEntity.perform(grouped)
+				.getCommandList());
+		return command;
+	}
+}
---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/model/scene/Ungroup.java
@@ -0,0 +1,110 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions.model.scene;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.utils.Array;
+import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.control.actions.ModelAction;
+import es.eucm.ead.editor.control.commands.Command;
+import es.eucm.ead.editor.control.commands.CompositeCommand;
+import es.eucm.ead.editor.control.commands.ListCommand.AddToListCommand;
+import es.eucm.ead.editor.control.commands.ListCommand.RemoveFromListCommand;
+import es.eucm.ead.editor.model.Model;
+import es.eucm.ead.schema.entities.ModelEntity;
+
+/**
+ * Creates a group performing the necessary commands over the
+ * {@link ModelEntity}s associated to the arguments.
+ * <dl>
+ * <dt><strong>Arguments</strong></dt>
+ * <dd><strong>args[0]</strong> <em>{@link Group}</em> the parent of the
+ * ungrouped actors</dd>
+ * <dd><strong>args[1]</strong> <em>{@link Group}</em> the old group, removed
+ * from the parent</dd>
+ * <dd><strong>args[2]</strong> <em>{@link Array}</em> an array with
+ * {@link Actor}s forming the old group</dd>
+ * </dl>
+ */
+public class Ungroup extends ModelAction {
+
+	private MultipleActorTransformToEntity multipleActorTransformToEntity;
+
+	public Ungroup() {
+		super(true, false, Group.class, Group.class, Array.class);
+	}
+
+	@Override
+	public void initialize(Controller controller) {
+		super.initialize(controller);
+		multipleActorTransformToEntity = controller.getActions().getAction(
+				MultipleActorTransformToEntity.class);
+	}
+
+	@Override
+	public Command perform(Object... args) {
+
+		Group parent = (Group) args[0];
+		Group oldGroup = (Group) args[1];
+		Array<Actor> ungrouped = (Array<Actor>) args[2];
+
+		ModelEntity parentEntity = Model.getModelEntity(parent);
+		ModelEntity oldGroupEntity = Model.getModelEntity(oldGroup);
+
+		// Copy actors transformations
+		CompositeCommand command = multipleActorTransformToEntity
+				.perform(ungrouped);
+
+		// Remove from old group and add it to new group
+		for (Actor actor : ungrouped) {
+			ModelEntity actorEntity = Model.getModelEntity(actor);
+
+			command.addCommand(new RemoveFromListCommand(oldGroupEntity
+					.getChildren(), actorEntity));
+
+			command.addCommand(new AddToListCommand(parentEntity.getChildren(),
+					actorEntity));
+		}
+
+		// Remove old group
+		command.addCommand(new RemoveFromListCommand(
+				parentEntity.getChildren(), oldGroupEntity));
+
+		return command;
+	}
+}
