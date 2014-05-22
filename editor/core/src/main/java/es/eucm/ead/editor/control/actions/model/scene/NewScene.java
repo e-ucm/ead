d948e512d8857ef1f8c016854a9bc4b1606dd839
anserran <angelserranolaguna@gmail.com>
2014-05-22 18:11:21 +0200

    Added new scene model action

---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/model/scene/NewScene.java
@@ -0,0 +1,73 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions.model.scene;
+
+import es.eucm.ead.editor.control.actions.ModelAction;
+import es.eucm.ead.editor.control.commands.CompositeCommand;
+import es.eucm.ead.editor.control.commands.EntityCommand.AddEntityCommand;
+import es.eucm.ead.editor.control.commands.FieldCommand;
+import es.eucm.ead.editor.model.Model;
+import es.eucm.ead.schema.editor.components.EditState;
+import es.eucm.ead.schema.entities.ModelEntity;
+import es.eucm.ead.schemax.FieldNames;
+import es.eucm.ead.schemax.entities.ModelEntityCategory;
+
+/**
+ * Creates a new empty scene and sets it as the current edited scene. This
+ * actions receives no arguments
+ */
+public class NewScene extends ModelAction {
+
+	@Override
+	public CompositeCommand perform(Object... args) {
+		Model model = controller.getModel();
+
+		String id = model.createId(ModelEntityCategory.SCENE);
+		ModelEntity scene = controller.getTemplates().createScene(id);
+
+		EditState editState = Model.getComponent(model.getGame(),
+				EditState.class);
+
+		CompositeCommand compositeCommand = new CompositeCommand();
+		compositeCommand.addCommand(new AddEntityCommand(model, id, scene,
+				ModelEntityCategory.SCENE));
+		compositeCommand.addCommand(new FieldCommand(editState,
+				FieldNames.EDIT_SCENE, id));
+
+		return compositeCommand;
+	}
+}
---
+++ editor/core/src/test/java/es/eucm/ead/editor/actions/model/scene/NewSceneTest.java
@@ -0,0 +1,88 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.actions.model.scene;
+
+import es.eucm.ead.editor.actions.ActionTest;
+import es.eucm.ead.editor.control.actions.editor.Undo;
+import es.eucm.ead.editor.control.actions.model.scene.NewScene;
+import es.eucm.ead.editor.model.Model;
+import es.eucm.ead.editor.model.Model.FieldListener;
+import es.eucm.ead.editor.model.events.FieldEvent;
+import es.eucm.ead.schema.editor.components.EditState;
+import es.eucm.ead.schemax.FieldNames;
+import es.eucm.ead.schemax.entities.ModelEntityCategory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class NewSceneTest extends ActionTest implements FieldListener {
+
+	private boolean received;
+
+	@Test
+	public void testNewScene() {
+		openEmpty();
+
+		received = false;
+		Model model = mockController.getModel();
+		model.addFieldListener(
+				Model.getComponent(model.getGame(), EditState.class), this);
+
+		int scenes = model.getEntities(ModelEntityCategory.SCENE).size();
+		mockController.action(NewScene.class);
+
+		assertEquals(model.getEntities(ModelEntityCategory.SCENE).size(),
+				scenes + 1);
+		assertTrue(received);
+
+		mockController.action(Undo.class);
+
+		assertEquals(model.getEntities(ModelEntityCategory.SCENE).size(),
+				scenes);
+	}
+
+	@Override
+	public boolean listenToField(FieldNames fieldName) {
+		return fieldName == FieldNames.EDIT_SCENE;
+	}
+
+	@Override
+	public void modelChanged(FieldEvent event) {
+		received = true;
+	}
+}
