5c0fd766a7752fb5ed7c1c136f9ce9ad711a7e22
RotaruDan <drotaru@ucm.es>
2014-05-27 00:23:34 +0200

    Added ReplaceEntity action.

---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/model/ReplaceEntity.java
@@ -0,0 +1,81 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions.model;
+
+import java.util.List;
+
+import com.badlogic.gdx.utils.Array;
+
+import es.eucm.ead.editor.control.actions.ModelAction;
+import es.eucm.ead.editor.control.commands.CompositeCommand;
+import es.eucm.ead.editor.control.commands.ListCommand.AddToListCommand;
+import es.eucm.ead.editor.control.commands.ListCommand.RemoveFromListCommand;
+import es.eucm.ead.editor.model.Model;
+import es.eucm.ead.schema.editor.components.Parent;
+import es.eucm.ead.schema.entities.ModelEntity;
+
+/**
+ * Replaces an {@link ModelEntity} with another.
+ * <dl>
+ * <dt><strong>Arguments</strong></dt>
+ * <dd><strong>args[0]</strong> <em>{@link ModelEntity}</em> the entity to be
+ * replaced</dd>
+ * <dd><strong>args[1]</strong> <em>{@link ModelEntity}</em> the entity that
+ * replaces the first one</dd>
+ * </dl>
+ */
+public class ReplaceEntity extends ModelAction {
+
+	public ReplaceEntity() {
+		super(true, false, ModelEntity.class, Array.class);
+	}
+
+	@Override
+	public CompositeCommand perform(Object... args) {
+		CompositeCommand compositeCommand = new CompositeCommand();
+		ModelEntity current = (ModelEntity) args[0];
+		ModelEntity newEntity = (ModelEntity) args[1];
+
+		List<ModelEntity> children = Model.getComponent(current, Parent.class)
+				.getParent().getChildren();
+		compositeCommand
+				.addCommand(new RemoveFromListCommand(children, current));
+		compositeCommand.addCommand(new AddToListCommand(children, newEntity));
+
+		return compositeCommand;
+	}
+}
