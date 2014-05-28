e5e49e228f7c95ff8c318776b967c4133975266b
anserran <angelserranolaguna@gmail.com>
2014-05-28 17:13:02 +0200

    Added scene view

---
+++ editor/desktop/src/main/java/es/eucm/ead/editor/control/views/SceneView.java
@@ -0,0 +1,75 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.views;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.control.actions.model.EditScene;
+import es.eucm.ead.editor.ui.scenes.DesktopSceneEditor;
+import es.eucm.ead.editor.ui.scenes.ribbon.SceneRibbon;
+import es.eucm.ead.editor.view.builders.ViewBuilder;
+import es.eucm.ead.editor.view.widgets.layouts.LinearLayout;
+import es.eucm.ead.editor.view.widgets.scenes.SceneEditor;
+
+public class SceneView implements ViewBuilder {
+
+	private Controller controller;
+
+	private LinearLayout sceneView;
+
+	@Override
+	public void initialize(Controller controller) {
+		this.controller = controller;
+		sceneView = new LinearLayout(false);
+		SceneEditor sceneEditor = new DesktopSceneEditor(controller);
+
+		sceneView.add(new SceneRibbon(controller)).expandX();
+		sceneView.add(sceneEditor).expand(true, true).getActor().toBack();
+		sceneView.setFillParent(true);
+	}
+
+	@Override
+	public Actor getView(Object... args) {
+		controller.action(EditScene.class, args);
+		return sceneView;
+	}
+
+	@Override
+	public void release(Controller controller) {
+
+	}
+}
