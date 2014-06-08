729f7dc5b5a36d19aea194803d4b12038eac5649
jtorrente <jtorrente@e-ucm.es>
2014-06-08 14:15:33 +0200

    New shape renderer. Internally It is just a special type of ImageComponent.

---
+++ engine/core/src/main/java/es/eucm/ead/engine/processors/renderers/ShapeRendererProcessor.java
@@ -0,0 +1,73 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.processors.renderers;
+
+import ashley.core.Component;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.utils.Array;
+import es.eucm.ead.engine.GameLoop;
+import es.eucm.ead.engine.components.renderers.shape.ShapeRendererComponent;
+import es.eucm.ead.engine.components.renderers.shape.ShapeToPixmap;
+import es.eucm.ead.engine.utils.ShapeToCollider;
+import es.eucm.ead.schema.renderers.ShapeRenderer;
+
+/**
+ * Created by Javier Torrente on 8/06/14.
+ */
+public class ShapeRendererProcessor extends RendererProcessor<ShapeRenderer> {
+	private ShapeToPixmap shapeToPixmap;
+
+	public ShapeRendererProcessor(GameLoop engine) {
+		super(engine, null);
+		shapeToPixmap = new ShapeToPixmap();
+	}
+
+	@Override
+	public Component getComponent(ShapeRenderer component) {
+		ShapeRendererComponent shapeRendererComponent = engine
+				.createComponent(ShapeRendererComponent.class);
+		// Set collider
+		Array<Polygon> collider = new Array<Polygon>();
+		collider.add(ShapeToCollider.buildShapeCollider(component.getShape()));
+		shapeRendererComponent.setCollider(collider);
+		// Set pixmap
+		shapeRendererComponent.setTexture(new Texture(shapeToPixmap
+				.createShape(component)));
+		return shapeRendererComponent;
+	}
+}
