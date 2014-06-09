a37258d9fcdb91b8f3ee5bb016d071487b92b49a
jtorrente <jtorrente@e-ucm.es>
2014-06-09 16:48:43 +0200

    schema

---
+++ engine/schema/src/main/java/es/eucm/ead/schema/renderers/EmptyRenderer.java
@@ -0,0 +1,74 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package es.eucm.ead.schema.renderers;
+
+import javax.annotation.Generated;
+import es.eucm.ead.schema.data.shape.Shape;
+
+/**
+ * An empty renderer represented through a polygon. Points are stored as
+ * consecutive x and y coordinates, i.e., [x0, y0, x1, y1, x2, y2, ...]. Useful
+ * for rendering interactive areas.
+ * 
+ */
+@Generated("org.jsonschema2pojo")
+public class EmptyRenderer extends Renderer {
+
+	/**
+	 * A common parent class for all shapes
+	 * 
+	 */
+	private Shape shape;
+
+	/**
+	 * A common parent class for all shapes
+	 * 
+	 */
+	public Shape getShape() {
+		return shape;
+	}
+
+	/**
+	 * A common parent class for all shapes
+	 * 
+	 */
+	public void setShape(Shape shape) {
+		this.shape = shape;
+	}
+
+}
