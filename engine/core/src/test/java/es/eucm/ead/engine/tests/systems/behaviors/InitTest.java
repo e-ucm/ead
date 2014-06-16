fe0e909f539ac0d3339689e3adca2445cec566ac
anserran <angelserranolaguna@gmail.com>
2014-06-16 17:00:11 +0200

    Test

---
+++ engine/core/src/test/java/es/eucm/ead/engine/tests/systems/behaviors/InitTest.java
@@ -0,0 +1,104 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.tests.systems.behaviors;
+
+import es.eucm.ead.engine.GameLoop;
+import es.eucm.ead.engine.mock.schema.MockEffect;
+import es.eucm.ead.engine.mock.schema.MockEffect.MockEffectListener;
+import es.eucm.ead.engine.processors.ComponentProcessor;
+import es.eucm.ead.engine.processors.behaviors.BehaviorsProcessor;
+import es.eucm.ead.engine.systems.EffectsSystem;
+import es.eucm.ead.schema.components.ModelComponent;
+import es.eucm.ead.schema.components.behaviors.Behavior;
+import es.eucm.ead.schema.components.behaviors.Behaviors;
+import es.eucm.ead.schema.components.behaviors.events.Init;
+import es.eucm.ead.schema.entities.ModelEntity;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+public class InitTest extends BehaviorTest implements MockEffectListener {
+
+	private boolean executed;
+
+	@Override
+	protected void registerComponentProcessors(
+			GameLoop gameLoop,
+			Map<Class<? extends ModelComponent>, ComponentProcessor> componentProcessors) {
+		componentProcessors.put(Behaviors.class, new BehaviorsProcessor(
+				gameLoop));
+	}
+
+	@Override
+	public void addSystems(GameLoop gameLoop) {
+		gameLoop.addSystem(new EffectsSystem(gameLoop, variablesManager));
+	}
+
+	@Test
+	public void testInitEvent() {
+		executed = false;
+
+		ModelEntity modelEntity = new ModelEntity();
+
+		Behaviors behaviors = new Behaviors();
+		Behavior behavior = new Behavior();
+		Init initEvent = new Init();
+
+		MockEffect mockEffect = new MockEffect(this);
+
+		behavior.setEvent(initEvent);
+		behavior.getEffects().add(mockEffect);
+
+		behaviors.getBehaviors().add(behavior);
+
+		modelEntity.getComponents().add(behaviors);
+
+		addEntity(modelEntity);
+
+		gameLoop.update(1);
+
+		assertTrue(executed);
+
+	}
+
+	@Override
+	public void executed() {
+		executed = true;
+	}
+}
