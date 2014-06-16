053b74d90d10b64ca36de116ef64099d3817e226
angeldelblanco <angel.dba@fdi.ucm.es>
2014-06-16 16:05:08 +0200

    After rebase, change import to new location of ActionListener

--- editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/edition/MoreComponent.java
+++ editor/core/src/main/java/es/eucm/ead/editor/view/widgets/mockup/edition/MoreComponent.java
@@ -45,13 +45,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import es.eucm.ead.editor.control.Controller;
-import es.eucm.ead.editor.control.actions.Action.ActionListener;
 import es.eucm.ead.editor.control.actions.editor.Clone;
 import es.eucm.ead.editor.control.actions.model.Rename;
 import es.eucm.ead.editor.model.Model;
 import es.eucm.ead.editor.model.events.FieldEvent;
 import es.eucm.ead.editor.view.builders.mockup.edition.EditionWindow;
 import es.eucm.ead.editor.view.listeners.ActionForTextFieldListener;
+import es.eucm.ead.editor.view.listeners.ActionListener;
 import es.eucm.ead.editor.view.listeners.ChangeNoteFieldListener;
 import es.eucm.ead.editor.view.widgets.mockup.Notification;
 import es.eucm.ead.editor.view.widgets.mockup.buttons.BottomProjectMenuButton;
