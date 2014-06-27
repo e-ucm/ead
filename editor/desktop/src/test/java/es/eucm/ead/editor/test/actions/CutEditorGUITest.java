40fa6f1d37ab99dc92b56c7e77ea9b09a27a9c49
anserran <angelserranolaguna@gmail.com>
2014-06-27 18:42:35 +0200

    Cut Copy Test (reproducing the bug)

---
+++ editor/desktop/src/test/java/es/eucm/ead/editor/test/actions/CutEditorGUITest.java
@@ -0,0 +1,84 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.test.actions;
+
+import com.badlogic.gdx.utils.Array;
+import es.eucm.ead.editor.control.actions.editor.ChangeView;
+import es.eucm.ead.editor.control.views.SceneView;
+import es.eucm.ead.editor.nogui.EditorGUITest;
+import es.eucm.ead.editor.nogui.actions.OpenMockGame;
+import es.eucm.ead.editor.nogui.actions.OpenMockGame.MockGame;
+import es.eucm.ead.schema.editor.components.Parent;
+import es.eucm.ead.schema.entities.ModelEntity;
+
+import static org.junit.Assert.assertEquals;
+
+public class CutEditorGUITest extends EditorGUITest {
+
+	@Override
+	protected void collectRunnables(Array<Runnable> runnables) {
+		runnables.add(new Runnable() {
+			@Override
+			public void run() {
+				MockGame game = new MockGame();
+				game.setGame(new ModelEntity());
+				ModelEntity scene = new ModelEntity();
+				ModelEntity sceneElement = new ModelEntity();
+				scene.getChildren().add(sceneElement);
+				Parent parent = new Parent();
+				parent.setParent(scene);
+				sceneElement.getComponents().add(parent);
+
+				game.addScene("scene1", scene);
+
+				controller.action(OpenMockGame.class, game);
+				controller.action(ChangeView.class, SceneView.class, "scene1");
+				setSelection(sceneElement);
+
+				click("cut");
+
+				assertEquals(0, scene.getChildren().size());
+
+				for (int i = 0; i < 10; i++) {
+					assertEquals(i, scene.getChildren().size());
+					click("paste");
+				}
+
+			}
+		});
+	}
+}
