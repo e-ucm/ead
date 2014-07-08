60c05dfbe0bc444aec382374164e5ff1e73065e0
RotaruDan <drotaru@ucm.es>
2014-07-08 17:32:50 +0200

    Renamed FramesTimelineTest to AnimationEditorTest.

--- editor/desktop/src/test/java/es/eucm/ead/editor/editorui/resources/AnimationEditorTest.java
+++ editor/desktop/src/test/java/es/eucm/ead/editor/editorui/resources/AnimationEditorTest.java
@@ -37,18 +37,17 @@
 package es.eucm.ead.editor.editorui.resources;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 
 import es.eucm.ead.editor.assets.EditorGameAssets;
 import es.eucm.ead.editor.editorui.EditorUITest;
-import es.eucm.ead.editor.ui.resources.frames.FramesTimeline;
+import es.eucm.ead.editor.ui.resources.frames.AnimationEditor;
 import es.eucm.ead.schema.renderers.Frame;
 import es.eucm.ead.schema.renderers.Frames;
 import es.eucm.ead.schema.renderers.Image;
 
-public class FramesTimelineTest extends EditorUITest {
+public class AnimationEditorTest extends EditorUITest {
 
 	@Override
 	protected void builUI(Group root) {
@@ -56,31 +55,32 @@ public class FramesTimelineTest extends EditorUITest {
 		EditorGameAssets gameAssets = controller.getEditorGameAssets();
 
 		// Prepare some images and frames...
-		gameAssets.setLoadingPath("framesTest", true);
+		gameAssets.setLoadingPath("cooldemo", true);
 		controller.getCommands().pushStack();
 
 		Frames frames = new Frames();
-		for (int i = 0; i < 20; ++i) {
+		for (int i = 1; i < 11; ++i) {
 			Frame frame = new Frame();
 			Image image = new Image();
-			image.setUri("externalResources/ic_" + (i % 10) + ".png");
+			image.setUri("images/p1_walk" + (i < 10 ? "0" + i : i) + ".png");
 
 			frame.setRenderer(image);
-			frame.setTime(MathUtils.random(.1f, .5f));
+			frame.setTime(.05f);
 			frames.getFrames().add(frame);
 		}
 
 		// Create the widget
-		FramesTimeline widget = new FramesTimeline(controller);
-		widget.setFrames(frames.getFrames());
+		AnimationEditor animEditor = new AnimationEditor(controller);
+		animEditor.setFrames(frames);
 
-		Container container = new Container(widget);
+		Container container = new Container(animEditor);
+		container.fill();
 		container.setFillParent(true);
 		root.addActor(container);
 	}
 
 	public static void main(String[] args) {
-		new LwjglApplication(new FramesTimelineTest(), "Frames Timeline test",
-				400, 600);
+		new LwjglApplication(new AnimationEditorTest(),
+				"Animation Editor test", 450, 700);
 	}
 }
