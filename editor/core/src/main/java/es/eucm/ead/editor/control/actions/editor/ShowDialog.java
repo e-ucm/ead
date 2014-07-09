820e4c3e4ee1a710957955678c587e52a3b2e7dd
anserran <angelserranolaguna@gmail.com>
2014-07-09 18:19:16 +0200

    Show dialog action

---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/editor/ShowDialog.java
@@ -0,0 +1,64 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions.editor;
+
+import es.eucm.ead.editor.control.actions.EditorAction;
+
+/**
+ * <p>
+ * Shows a dialog
+ * </p>
+ * <dl>
+ * <dt><strong>Arguments</strong></dt>
+ * <dd><strong>args[0]</strong> <em>Class</em> The dialog class</dd>
+ * <dd><strong>args[1..n]</strong> <em>Object</em> Arguments for the dialog</dd>
+ * </dl>
+ */
+public class ShowDialog extends EditorAction {
+
+	@Override
+	public boolean validate(Object... args) {
+		return args.length > 0 && args[0] instanceof Class;
+	}
+
+	@Override
+	public void perform(Object... args) {
+		Object[] dialogArguments = new Object[args.length - 1];
+		System.arraycopy(args, 1, dialogArguments, 0, dialogArguments.length);
+		controller.getViews().showDialog((Class) args[0], dialogArguments);
+	}
+}
