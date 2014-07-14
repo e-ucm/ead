6c6f417b9e795b288498ac76da29e54760c995c5
anserran <angelserranolaguna@gmail.com>
2014-07-14 15:42:17 +0200

    GUI test for back next buttons

---
+++ editor/desktop/src/test/java/es/eucm/ead/editor/test/actions/BackNextEditorGUITest.java
@@ -0,0 +1,70 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.test.actions;
+
+import es.eucm.ead.editor.nogui.EditorGUITest;
+import es.eucm.ead.editor.ui.perspectives.PerspectiveButtons;
+import es.eucm.ead.editor.view.widgets.IconButton;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BackNextEditorGUITest extends EditorGUITest {
+
+	@Override
+	protected void runTest() {
+		openEmptyGame();
+		IconButton back = (IconButton) getActor("back");
+		IconButton next = (IconButton) getActor("next");
+		assertTrue(back.isDisabled());
+		assertTrue(next.isDisabled());
+		click(PerspectiveButtons.SCENE_SELECTOR);
+		click(PerspectiveButtons.SCENE_SELECTOR + "0");
+		click(PerspectiveButtons.SCENE_SELECTOR);
+		click(PerspectiveButtons.SCENE_SELECTOR + "1");
+		assertFalse(back.isDisabled());
+		assertTrue(next.isDisabled());
+
+		click("back");
+		assertTrue(back.isDisabled());
+		assertFalse(next.isDisabled());
+		click("next");
+		assertFalse(back.isDisabled());
+		assertTrue(next.isDisabled());
+
+	}
+}
