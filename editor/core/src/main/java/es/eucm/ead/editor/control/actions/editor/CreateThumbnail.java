c7c27b29ebe08c5c6f25b3e7dc5beb0bc768ad6a
RotaruDan <drotaru@ucm.es>
2014-07-22 18:56:31 +0200

    Fixes CreateThumbnail action. Adds Thumbnail component.

--- editor/core/src/main/java/es/eucm/ead/editor/control/actions/editor/CreateThumbnail.java
+++ editor/core/src/main/java/es/eucm/ead/editor/control/actions/editor/CreateThumbnail.java
@@ -36,32 +36,37 @@
  */
 package es.eucm.ead.editor.control.actions.editor;
 
+import java.nio.ByteBuffer;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import es.eucm.ead.editor.assets.EditorGameAssets;
 import es.eucm.ead.editor.control.Controller;
 import es.eucm.ead.editor.control.actions.EditorAction;
+import es.eucm.ead.editor.model.Q;
 import es.eucm.ead.engine.EntitiesLoader;
 import es.eucm.ead.engine.GameLoop;
 import es.eucm.ead.engine.entities.EngineEntity;
+import es.eucm.ead.schema.editor.components.Thumbnail;
 import es.eucm.ead.schema.entities.ModelEntity;
+import es.eucm.ead.schemax.GameStructure;
 
 /**
  * Creates a thumbnail for a {@link ModelEntity}.
  * <dl>
  * <dt><strong>Arguments</strong></dt>
- * <dd><strong>args[0]</strong> <em>String</em> Project relative path for the
- * thumbnail</dd>
- * <dd><strong>args[1]</strong> <em>{@link ModelEntity}</em> model entity for</dd>
- * <dd><strong>args[2]</strong> <em>{@link Integer}</em> width of the thumbnail</dd>
- * <dd><strong>args[3]</strong> <em>{@link Integer}</em> height for the
+ * <dd><strong>args[0]</strong> <em>{@link ModelEntity}</em> model entity for</dd>
+ * <dd><strong>args[1]</strong> <em>{@link Integer}</em> width of the thumbnail</dd>
+ * <dd><strong>args[2]</strong> <em>{@link Integer}</em> height for the
  * thumbnail</dd>
  * </dl>
  */
@@ -76,8 +81,7 @@ public class CreateThumbnail extends EditorAction {
 	private EditorGameAssets editorGameAssets;
 
 	public CreateThumbnail() {
-		super(true, false, String.class, ModelEntity.class, Integer.class,
-				Integer.class);
+		super(true, false, ModelEntity.class, Integer.class, Integer.class);
 	}
 
 	@Override
@@ -91,37 +95,64 @@ public class CreateThumbnail extends EditorAction {
 
 	@Override
 	public void perform(Object... args) {
-		String dstPath = (String) args[0];
-		ModelEntity modelEntity = (ModelEntity) args[1];
-		int width = (Integer) args[2];
-		int height = (Integer) args[3];
+		ModelEntity modelEntity = (ModelEntity) args[0];
 
-		Gdx.gl.glViewport(0, 0, width, height);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Thumbnail thumbnail = Q.getComponent(modelEntity, Thumbnail.class);
+		if (thumbnail.getThumbnail() == null) {
+			int width = (Integer) args[1];
+			int height = (Integer) args[2];
 
-		EngineEntity engineEntity = entitiesLoader.toEngineEntity(modelEntity);
-		editorGameAssets.finishLoading();
-		batch.begin();
+			String thumbSavingPath = GameStructure.THUMBNAILS_PATH;
+			FileHandle thumbSavingDir = editorGameAssets
+					.resolve(thumbSavingPath);
+			if (!thumbSavingDir.exists()) {
+				thumbSavingDir.mkdirs();
+			}
+			String id = controller.getModel().getIdFor(modelEntity);
+			FileHandle temp = editorGameAssets.resolve(id);
+			thumbSavingPath += temp.nameWithoutExtension();
+			FileHandle thumbSavingImage = editorGameAssets
+					.resolve(thumbSavingPath + ".png");
 
-		// Prepare group transformations, so thumbnail is centered
-		Group group = engineEntity.getGroup();
-		group.setPosition(0, 0);
-		group.setSize(width, height);
-		group.setScale(1, -1);
-		group.setOrigin(width / 2.0f, height / 2.0f);
+			EngineEntity engineEntity = entitiesLoader
+					.toEngineEntity(modelEntity);
+			editorGameAssets.finishLoading();
 
-		group.draw(batch, 1.0f);
+			// Prepare group transformations, so thumbnail is centered
+			Group group = engineEntity.getGroup();
+			Vector2 scl = Scaling.fit.apply(group.getWidth(),
+					group.getHeight(), width, height);
+			group.setScale(scl.x / group.getWidth(), scl.y / group.getHeight());
+			group.setPosition((width - scl.x) * .5f, (height - scl.y) * .5f);
 
-		batch.end();
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			batch.begin();
+			group.draw(batch, 1.0f);
+			batch.end();
+			batch.flush();
 
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(),
-				Gdx.graphics.getHeight());
+			Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(0, 0, width,
+					height);
 
-		Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(0, 0, width, height);
-		FileHandle dstFh = editorGameAssets.resolve(dstPath);
-		PixmapIO.writePNG(dstFh, pixmap);
-		pixmap.dispose();
+			// We must convert the OpenGL ES coordinates of the pixels (y-down)
+			// to an y-up coordinate system before saving.
+			final int w = pixmap.getWidth();
+			final int h = pixmap.getHeight();
+			final ByteBuffer pixels = pixmap.getPixels();
+			byte[] lines = new byte[w * h * 4];
+			final int numBytesPerLine = w * 4, height_index = h - 1;
+			for (int i = 0; i < h; ++i) {
+				pixels.position((height_index - i) * numBytesPerLine);
+				pixels.get(lines, i * numBytesPerLine, numBytesPerLine);
+			}
+			pixels.clear();
+			pixels.put(lines);
 
-		gameLoop.removeEntity(engineEntity);
+			PixmapIO.writePNG(thumbSavingImage, pixmap);
+			pixmap.dispose();
+
+			gameLoop.removeEntity(engineEntity);
+			thumbnail.setThumbnail(thumbSavingImage.path());
+		}
 	}
 }
