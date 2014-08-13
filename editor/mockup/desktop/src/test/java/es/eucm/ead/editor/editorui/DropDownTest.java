29168992cb91b6d7e82b715547abdd09aad2dc9e
RotaruDan <drotaru@ucm.es>
2014-08-13 17:17:04 +0200

    Fixed and improved DropDownWidget.

--- editor/mockup/desktop/src/test/java/es/eucm/ead/editor/editorui/DropDownTest.java
+++ editor/mockup/desktop/src/test/java/es/eucm/ead/editor/editorui/DropDownTest.java
@@ -37,13 +37,23 @@
 package es.eucm.ead.editor.editorui;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Array;
 
+import es.eucm.ead.editor.assets.EditorGameAssets;
 import es.eucm.ead.editor.view.widgets.DropDown;
 import es.eucm.ead.engine.I18N;
+import es.eucm.ead.engine.assets.Assets.AssetLoadedCallback;
 
 public class DropDownTest extends MockupUITest {
 
@@ -51,17 +61,55 @@ public class DropDownTest extends MockupUITest {
 	protected Actor builUI(Skin skin, I18N i18n) {
 
 		Container container = new Container();
+		EditorGameAssets gameAssets = controller.getEditorGameAssets();
+
+		// Prepare some images
+		gameAssets.setLoadingPath("dropDown", true);
+
+		Table table = new Table(skin);
 
 		DropDown dropDown = new DropDown(skin);
-		dropDown.setItems(new Label("123", skin), new Label("456", skin));
+		dropDown.getList().pad(10f, 50f, 90f, 140f);
+		dropDown.pad(10f, 50f, 90f, 140f);
+		dropDown.setItems(new Label("123", skin), new Label("456", skin),
+				new Label("789", skin));
 
-		container.setWidget(dropDown);
+		SelectBox<String> selectBox = new SelectBox<String>(skin);
+		selectBox.setItems("123", "456", "789");
+
+		Array<Actor> images = new Array<Actor>();
+		for (int i = 0; i < 4; ++i) {
+			final Image image = new Image();
+			image.setColor(Color.BLACK);
+			gameAssets.get(i + ".png", Texture.class,
+					new AssetLoadedCallback<Texture>() {
+
+						@Override
+						public void loaded(String fileName, Texture asset) {
+							image.setDrawable(new TextureRegionDrawable(
+									new TextureRegion(asset)));
+						}
+
+					}, true);
+			images.add(image);
+		}
+		DropDown dropDown2 = new DropDown(skin);
+		dropDown2.setItems(images);
+
+		table.add("Drop Down  ");
+		table.add("  Select Box");
+		table.row();
+		table.add(dropDown, selectBox);
+		table.row();
+		table.add(dropDown2);
+
+		container.setWidget(table);
 		container.setFillParent(true);
 
 		return container;
 	}
 
 	public static void main(String[] args) {
-		new LwjglApplication(new DropDownTest(), "Drop Down test", 1000, 600);
+		new LwjglApplication(new DropDownTest(), "Drop Down test", 700, 700);
 	}
 }
