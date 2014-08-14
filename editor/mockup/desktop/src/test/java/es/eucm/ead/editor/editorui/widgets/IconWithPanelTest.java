aa7ca45c00c8613caedbc2e75522ab707c917f98
RotaruDan <drotaru@ucm.es>
2014-08-14 15:55:42 +0200

    Adds IconWithPanelTest.

---
+++ editor/mockup/desktop/src/test/java/es/eucm/ead/editor/editorui/widgets/IconWithPanelTest.java
@@ -0,0 +1,105 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.editorui.widgets;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+import es.eucm.ead.editor.editorui.MockupUITest;
+import es.eucm.ead.editor.view.widgets.iconwithpanel.IconWithFadePanel;
+import es.eucm.ead.editor.view.widgets.iconwithpanel.IconWithPanel;
+import es.eucm.ead.editor.view.widgets.iconwithpanel.IconWithScalePanel;
+import es.eucm.ead.engine.I18N;
+
+public class IconWithPanelTest extends MockupUITest {
+
+	@Override
+	protected Actor builUI(Skin skin, I18N i18n) {
+
+		Container container = new Container();
+		Table table = new Table(skin);
+
+		IconWithPanel fade = new IconWithFadePanel(
+				skin.getDrawable("conversation80x80"), 0f, skin);
+		fade.getPanel().add(new Image(skin.getDrawable("conversation80x80")));
+
+		IconWithPanel scaleRight = new IconWithScalePanel(
+				skin.getDrawable("conversation80x80"), 0f, skin);
+		scaleRight.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+		scaleRight.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+		scaleRight.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+		scaleRight.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+
+		IconWithPanel scaleLeft = new IconWithScalePanel(
+				skin.getDrawable("conversation80x80"), 0f, skin);
+		scaleLeft.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+		scaleLeft.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+		scaleLeft.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+		scaleLeft.getPanel().add(
+				new Image(skin.getDrawable("conversation80x80")));
+
+		table.defaults().expand();
+		table.add("Icon With Fade Panel");
+		table.row();
+		table.add(fade);
+		table.row();
+		table.add("Icon With Scale Panel: left <-> right");
+		table.row();
+		table.add(scaleLeft, scaleRight);
+
+		container.setWidget(table);
+		container.setFillParent(true);
+
+		return container;
+	}
+
+	public static void main(String[] args) {
+		new LwjglApplication(new IconWithPanelTest(), "Icon WIth Panel test",
+				700, 700);
+	}
+}
