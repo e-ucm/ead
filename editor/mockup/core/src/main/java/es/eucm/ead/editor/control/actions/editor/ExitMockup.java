48a6672b0663e3b475e065a80ace987d751745b9
RotaruDan <drotaru@ucm.es>
2014-08-19 18:19:01 +0200

    Adds two mockup actions.

---
+++ editor/mockup/core/src/main/java/es/eucm/ead/editor/control/actions/editor/ExitMockup.java
@@ -0,0 +1,71 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions.editor;
+
+import com.badlogic.gdx.Gdx;
+
+import es.eucm.ead.editor.control.actions.EditorAction;
+
+/**
+ * <p>
+ * Closes the entire editor, finalizing the application completely. Performs
+ * {@link Save} action before exiting.
+ * </p>
+ * 
+ * <dl>
+ * <dt><strong>Arguments</strong></dt>
+ * <dd>None.</dd>
+ * </dl>
+ * 
+ */
+public class ExitMockup extends EditorAction {
+
+	public ExitMockup() {
+		super(true, false);
+	}
+
+	@Override
+	public void perform(Object... args) {
+		controller.action(Save.class);
+		Gdx.app.postRunnable(new Runnable() {
+			@Override
+			public void run() {
+				Gdx.app.exit();
+			}
+		});
+	}
+}
