93584fad256b5b70180e493e2a14ab3d96a416f4
gorco <antcal01@ucm.es>
2014-08-21 18:29:16 +0200

    Added some actions.

---
+++ editor/mockup/core/src/main/java/es/eucm/ead/editor/control/actions/RemoveTween.java
@@ -0,0 +1,66 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.actions;
+
+import es.eucm.ead.editor.control.Selection;
+import es.eucm.ead.schema.components.tweens.Tween;
+import es.eucm.ead.schema.entities.ModelEntity;
+
+/**
+ * Removes a {@link Tween} to the current scene element selected </p>
+ * <dl>
+ * <dt><strong>Arguments</strong></dt>
+ * <dd><strong>args[0]</strong> <em>{@link Tween}</em> to remove</dd>
+ * </dl>
+ */
+public class RemoveTween extends EditorAction {
+
+	public RemoveTween() {
+		super(true, false, Tween.class);
+	}
+
+	@Override
+	public void perform(Object... args) {
+		Tween tween = (Tween) args[0];
+
+		ModelEntity modelEntity = (ModelEntity) controller.getModel()
+				.getSelection().getSingle(Selection.SCENE_ELEMENT);
+
+		modelEntity.getComponents().removeValue(tween, true);
+	}
+
+}
