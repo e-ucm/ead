ff37034e26c90dbb8ffa0ba84e2815a10b3cf1e1
RotaruDan <drotaru@ucm.es>
2014-08-27 10:54:42 +0200

    Adds functionality to some buttons from the TopEditionToolbar.

--- editor/mockup/core/src/main/java/es/eucm/ead/editor/view/widgets/editionview/TopEditionToolbar.java
+++ editor/mockup/core/src/main/java/es/eucm/ead/editor/view/widgets/editionview/TopEditionToolbar.java
@@ -36,13 +36,26 @@
  */
 package es.eucm.ead.editor.view.widgets.editionview;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+import es.eucm.ead.editor.control.Actions;
 import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.editor.control.actions.editor.AddSceneElementFromResource;
+import es.eucm.ead.editor.control.actions.editor.ChangeView;
+import es.eucm.ead.editor.control.actions.editor.Redo;
+import es.eucm.ead.editor.control.actions.editor.TakePicture;
+import es.eucm.ead.editor.control.actions.editor.Undo;
+import es.eucm.ead.editor.view.builders.LibrariesView;
+import es.eucm.ead.editor.view.builders.gallery.PlayView;
+import es.eucm.ead.editor.view.listeners.ActionListener;
 import es.eucm.ead.editor.view.widgets.IconButton;
 import es.eucm.ead.editor.view.widgets.Toolbar;
+import es.eucm.ead.editor.view.widgets.editionview.draw.PaintToolbar;
 
 public class TopEditionToolbar extends Toolbar {
 
@@ -50,8 +63,9 @@ public class TopEditionToolbar extends Toolbar {
 
 	private float height;
 
-	public TopEditionToolbar(Controller controller, String style, float height,
-			float iconSize, float PAD) {
+	public TopEditionToolbar(final Controller controller, String style,
+			float height, float iconSize, float PAD,
+			final PaintToolbar paintToolbar) {
 		super(controller.getApplicationAssets().getSkin(), style);
 
 		Skin skin = controller.getApplicationAssets().getSkin();
@@ -61,22 +75,76 @@ public class TopEditionToolbar extends Toolbar {
 		align(Align.right);
 
 		// TODO change for widgets
-		IconButton play = new IconButton("play80x80", 0, skin);
+		final IconButton play = new IconButton("play80x80", 0, skin);
 
-		IconButton undo = new IconButton("undo80x80", 0, skin);
-		IconButton redo = new IconButton("redo80x80", 0, skin);
+		final IconButton undo = new IconButton("undo80x80", 0, skin);
+		final IconButton redo = new IconButton("redo80x80", 0, skin);
 
-		IconButton camera = new IconButton("camera80x80", 0, skin);
-		IconButton repository = new IconButton("repository80x80", 0, skin);
-		IconButton android = new IconButton("android_gallery80x80", 0, skin);
+		final IconButton camera = new IconButton("camera80x80", 0, skin);
+		final IconButton repository = new IconButton("repository80x80", 0, skin);
+		final IconButton android = new IconButton("android_gallery80x80", 0,
+				skin);
 
-		IconButton paint = new IconButton("paint80x80", 0, skin);
-		IconButton text = new IconButton("text80x80", 0, skin);
+		final IconButton paint = new IconButton("paint80x80", 0, skin);
+		final IconButton text = new IconButton("text80x80", 0, skin);
 
 		IconButton zones = new IconButton("interactive80x80", 0, skin);
 		IconButton gate = new IconButton("gateway80x80", 0, skin);
 
 		IconButton others = new IconButton("others80x80", 0, skin);
+
+		ClickListener buttonsListener = new ClickListener() {
+
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Actor listenerActor = event.getListenerActor();
+				if (listenerActor == play) {
+					controller.action(ChangeView.class, PlayView.class);
+				} else if (listenerActor == undo) {
+					controller.action(Undo.class);
+				} else if (listenerActor == redo) {
+					controller.action(Redo.class);
+				} else if (listenerActor == camera) {
+					controller.action(TakePicture.class, getStage());
+				} else if (listenerActor == repository) {
+					controller.action(ChangeView.class, LibrariesView.class);
+				} else if (listenerActor == android) {
+					controller.action(AddSceneElementFromResource.class);
+				} else if (listenerActor == paint) {
+					if (paintToolbar.isShowing()) {
+						paintToolbar.hide();
+					} else {
+						paintToolbar.show();
+					}
+				} else if (listenerActor == text) {
+
+				}
+			}
+		};
+		play.addListener(buttonsListener);
+		undo.addListener(buttonsListener);
+		redo.addListener(buttonsListener);
+		camera.addListener(buttonsListener);
+		repository.addListener(buttonsListener);
+		android.addListener(buttonsListener);
+		paint.addListener(buttonsListener);
+		text.addListener(buttonsListener);
+		ActionListener undoRedo = new ActionListener() {
+
+			@Override
+			public void enableChanged(Class actionClass, boolean enable) {
+				if (actionClass == Undo.class) {
+					undo.setDisabled(!enable);
+				} else {
+					redo.setDisabled(!enable);
+				}
+			}
+		};
+		Actions actions = controller.getActions();
+		actions.addActionListener(Undo.class, undoRedo);
+		actions.addActionListener(Redo.class, undoRedo);
+		undo.setDisabled(true);
+		redo.setDisabled(true);
 
 		Image logo = new Image(skin, "eAdventure");
 		add(logo).size(iconSize * 4.5f, iconSize).padRight(NORMAL_PAD * 2);
