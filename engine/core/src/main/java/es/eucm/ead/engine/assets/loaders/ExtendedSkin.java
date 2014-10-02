ad96223f58fcce914e9994b6e03197a91bee0499
RotaruDan <drotaru@ucm.es>
2014-10-02 13:21:37 +0200

    ExtendedeSkin loads BitmapFonts that have multiple pages.

--- engine/core/src/main/java/es/eucm/ead/engine/assets/loaders/ExtendedSkin.java
+++ engine/core/src/main/java/es/eucm/ead/engine/assets/loaders/ExtendedSkin.java
@@ -44,12 +44,15 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.Json.ReadOnlySerializer;
 import com.badlogic.gdx.utils.Json.Serializer;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.SerializationException;
 import es.eucm.ead.engine.assets.Assets;
+
+import static com.badlogic.gdx.graphics.g2d.BitmapFont.BitmapFontData;
 
 /**
  * Extension of skin to load some custom resources
@@ -68,9 +71,46 @@ public class ExtendedSkin extends Skin {
 		Json json = super.getJsonLoader(skinFile);
 		Serializer<BitmapFont> defaultSerializer = json
 				.getSerializer(BitmapFont.class);
-		json.setSerializer(BitmapFont.class, new TTFBitmapFontSerializer(
+		json.setSerializer(BitmapFont.class, new TTFBitmapFontSerializer(this,
 				assets, defaultSerializer, skinFile));
 		return json;
+	}
+
+	/**
+	 * Adds all named texture regions from the atlas. The atlas will not be
+	 * automatically disposed when the skin is disposed.
+	 */
+	public void addRegions(TextureAtlas atlas) {
+		Array<TextureAtlas.AtlasRegion> regions = atlas.getRegions();
+		for (int i = 0, n = regions.size; i < n; i++) {
+			TextureAtlas.AtlasRegion region = regions.get(i);
+			String name = region.name;
+			if (region.index != -1) {
+				name += String.valueOf(region.index);
+			}
+			add(name, region, TextureRegion.class);
+		}
+	}
+
+	/**
+	 * 
+	 * @param regionName
+	 * @return an array with the {@link TextureRegion} that have an index != -1,
+	 *         or null if none are found.
+	 * 
+	 */
+	private Array<TextureRegion> findRegionsWithIndex(String regionName) {
+		Array<TextureRegion> regions = null;
+		int i = 0;
+		TextureRegion region = optional(regionName + (i++), TextureRegion.class);
+		if (region != null) {
+			regions = new Array<TextureRegion>(true, 5, TextureRegion.class);
+			while (region != null) {
+				regions.add(region);
+				region = optional(regionName + (i++), TextureRegion.class);
+			}
+		}
+		return regions;
 	}
 
 	/**
@@ -80,15 +120,17 @@ public class ExtendedSkin extends Skin {
 			ReadOnlySerializer<BitmapFont> {
 
 		private final Assets assets;
+		private final ExtendedSkin skin;
 		private Serializer<BitmapFont> defaultSerializer;
 
 		private FileHandle skinFile;
 
-		private TTFBitmapFontSerializer(Assets assets,
+		private TTFBitmapFontSerializer(ExtendedSkin skin, Assets assets,
 				Serializer<BitmapFont> defaultSerializer, FileHandle skinFile) {
 			this.defaultSerializer = defaultSerializer;
 			this.skinFile = skinFile;
 			this.assets = assets;
+			this.skin = skin;
 		}
 
 		@Override
@@ -96,10 +138,36 @@ public class ExtendedSkin extends Skin {
 			String path = json.readValue("file", String.class, jsonValue);
 			if (path == null
 					|| !(path.endsWith(".ttf") || path.endsWith(".otf"))) {
-				return defaultSerializer.read(json, jsonValue, aClass);
+
+				try {
+					return defaultSerializer.read(json, jsonValue, aClass);
+				} catch (SerializationException ex) {
+
+					FileHandle fontFile = skinFile.parent().child(path);
+					Array<TextureRegion> regionsWithIndex = skin
+							.findRegionsWithIndex(fontFile
+									.nameWithoutExtension());
+					if (regionsWithIndex != null) {
+						int scaledSize = json.readValue("scaledSize",
+								int.class, -1, jsonValue);
+						Boolean flip = json.readValue("flip", Boolean.class,
+								false, jsonValue);
+
+						BitmapFont font = new BitmapFont(new BitmapFontData(
+								fontFile, flip), regionsWithIndex.items, true);
+						// Scaled size is the desired cap height to scale the
+						// font
+						// to.
+						if (scaledSize != -1)
+							font.setScale(scaledSize / font.getCapHeight());
+
+						return font;
+					}
+					throw new SerializationException(ex);
+				}
 			} else {
 				FileHandle fontFile = skinFile.parent().child(path);
-				if (!fontFile.exists()) {
+				if (!assets.checkFileExistence(fontFile)) {
 					fontFile = Gdx.files.internal(path);
 				}
 				if (!assets.checkFileExistence(fontFile)) {
