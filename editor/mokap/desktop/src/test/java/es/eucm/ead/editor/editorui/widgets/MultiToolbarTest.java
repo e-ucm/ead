2697d48364b5e2087dadd68da952950d0e92424f
gorco <antcal01@ucm.es>
2014-10-31 17:31:39 +0100

    Test view with compose toolbars.

--- editor/mokap/desktop/src/test/java/es/eucm/ead/editor/editorui/widgets/MultiToolbarTest.java
+++ editor/mokap/desktop/src/test/java/es/eucm/ead/editor/editorui/widgets/MultiToolbarTest.java
@@ -38,7 +38,6 @@ package es.eucm.ead.editor.editorui.widgets;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -48,9 +47,9 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import es.eucm.ead.editor.control.MockupController.Dpi;
 import es.eucm.ead.editor.editorui.MockupUITest;
-import es.eucm.ead.editor.view.widgets.Toolbar;
 import es.eucm.ead.editor.view.widgets.IconButton;
 import es.eucm.ead.editor.view.widgets.MultiToolbar;
+import es.eucm.ead.editor.view.widgets.Toolbar;
 import es.eucm.ead.engine.I18N;
 
 public class MultiToolbarTest extends MockupUITest {
@@ -63,22 +62,73 @@ public class MultiToolbarTest extends MockupUITest {
 
 		final MultiToolbar multiTop = new MultiToolbar(skin);
 
-		Toolbar topBar1 = new Toolbar(skin, 40, "white_top");
-		topBar1.backgroundColor(Color.YELLOW);
-		IconButton icon1 = new IconButton("menu", skin);
-		topBar1.leftAdd(icon1);
+		Toolbar insertBar = new Toolbar(skin);
 
-		Toolbar topBar2 = new Toolbar(skin, 40, "white_top");
-		topBar2.backgroundColor(Color.ORANGE);
-		IconButton icon2 = new IconButton("menu", skin);
-		topBar2.rightAdd(icon2);
+		IconButton redo = new IconButton("ic_redo", 0, skin, "toolbar");
+		insertBar.leftAdd(redo);
 
-		Toolbar topBar3 = new Toolbar(skin, 40, "white_top");
-		topBar3.backgroundColor(Color.RED);
-		IconButton icon3 = new IconButton("menu", skin);
-		topBar3.leftAdd(icon3);
+		IconButton undo = new IconButton("ic_undo", 0, skin, "toolbar");
+		insertBar.leftAdd(undo);
 
-		multiTop.addToolbars(topBar1, topBar2, topBar3);
+		IconButton compose = new IconButton("ic_compose", 0, skin, "toolbar");
+		insertBar.leftAdd(compose);
+
+		IconButton menu = new IconButton("ic_menu", 0, skin, "toolbar");
+		insertBar.leftAdd(menu);
+
+		IconButton paste = new IconButton("ic_paste", 0, skin, "toolbar");
+		insertBar.rightAdd(paste);
+
+		IconButton add = new IconButton("ic_add", 0, skin, "toolbar");
+		insertBar.rightAdd(add);
+
+		Toolbar transformBar = new Toolbar(skin);
+
+		IconButton redo2 = new IconButton("ic_redo", 0, skin, "toolbar");
+		transformBar.leftAdd(redo2);
+
+		IconButton undo2 = new IconButton("ic_undo", 0, skin, "toolbar");
+		transformBar.leftAdd(undo2);
+
+		IconButton check = new IconButton("ic_check", 0, skin, "toolbar");
+		transformBar.leftAdd(check);
+
+		IconButton group = new IconButton("ic_group", 0, skin, "toolbar");
+		transformBar.rightAdd(group);
+
+		IconButton edit = new IconButton("ic_edit", 0, skin, "toolbar");
+		transformBar.rightAdd(edit);
+
+		IconButton toBack = new IconButton("ic_to_back", 0, skin, "toolbar");
+		transformBar.rightAdd(toBack);
+
+		IconButton toFront = new IconButton("ic_to_front", 0, skin, "toolbar");
+		transformBar.rightAdd(toFront);
+
+		IconButton moreTrans = new IconButton("ic_more", 0, skin, "toolbar");
+		transformBar.rightAdd(moreTrans);
+
+		Toolbar paintBar = new Toolbar(skin);
+
+		IconButton redo3 = new IconButton("ic_redo", 0, skin, "toolbar");
+		paintBar.leftAdd(redo3);
+
+		IconButton undo3 = new IconButton("ic_undo", 0, skin, "toolbar");
+		paintBar.leftAdd(undo3);
+
+		IconButton circle = new IconButton("ic_circle", 0, skin, "toolbar");
+		paintBar.leftAdd(circle);
+
+		IconButton brush = new IconButton("ic_brush", 0, skin, "toolbar");
+		paintBar.leftAdd(brush);
+
+		IconButton save = new IconButton("ic_check", 0, skin, "toolbar");
+		paintBar.leftAdd(save);
+
+		IconButton cancel = new IconButton("ic_close", 0, skin, "toolbar");
+		paintBar.rightAdd(cancel);
+
+		multiTop.addToolbars(insertBar, transformBar, paintBar);
 
 		ClickListener topListener = new ClickListener() {
 			@Override
@@ -89,14 +139,17 @@ public class MultiToolbarTest extends MockupUITest {
 			}
 		};
 
-		icon1.addListener(topListener);
-		icon1.setUserObject(1);
+		compose.addListener(topListener);
+		compose.setUserObject(1);
 
-		icon2.addListener(topListener);
-		icon2.setUserObject(2);
+		check.addListener(topListener);
+		check.setUserObject(2);
 
-		icon3.addListener(topListener);
-		icon3.setUserObject(0);
+		save.addListener(topListener);
+		save.setUserObject(0);
+
+		cancel.addListener(topListener);
+		cancel.setUserObject(0);
 
 		table.add(multiTop).expandX().fill();
 		table.row();
