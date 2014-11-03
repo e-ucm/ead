a321b21c0dd02ad64bd2e2527b4fa65c4359c892
anserran <angelserranolaguna@gmail.com>
2014-11-03 17:01:16 +0100

    App listens to context changes to update the current view

--- editor/mokap/core/src/main/java/es/eucm/ead/editor/MokapApplicationListener.java
+++ editor/mokap/core/src/main/java/es/eucm/ead/editor/MokapApplicationListener.java
@@ -40,15 +40,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import es.eucm.ead.editor.control.Controller;
 import es.eucm.ead.editor.control.MokapController;
+import es.eucm.ead.editor.control.Selection;
 import es.eucm.ead.editor.control.actions.editor.ChangeView;
 import es.eucm.ead.editor.control.actions.editor.OpenLastGame;
 import es.eucm.ead.editor.control.actions.model.EditScene;
 import es.eucm.ead.editor.model.Model.ModelListener;
+import es.eucm.ead.editor.model.Model.SelectionListener;
 import es.eucm.ead.editor.model.Q;
 import es.eucm.ead.editor.model.events.LoadEvent;
 import es.eucm.ead.editor.model.events.LoadEvent.Type;
+import es.eucm.ead.editor.model.events.SelectionEvent;
 import es.eucm.ead.editor.platform.Platform;
 import es.eucm.ead.editor.view.builders.home.HomeView;
+import es.eucm.ead.editor.view.builders.project.ProjectView;
 import es.eucm.ead.editor.view.builders.scene.SceneView;
 import es.eucm.ead.editor.view.widgets.WidgetBuilder;
 import es.eucm.ead.schema.editor.components.GameData;
@@ -71,7 +75,23 @@ public class MokapApplicationListener extends EditorApplicationListener {
 							EditScene.class,
 							Q.getComponent(controller.getModel().getGame(),
 									GameData.class).getInitialScene());
-					controller.action(ChangeView.class, SceneView.class);
+					controller.action(ChangeView.class, ProjectView.class);
+				}
+			}
+		});
+
+		controller.getModel().addSelectionListener(new SelectionListener() {
+			@Override
+			public boolean listenToContext(String contextId) {
+				return true;
+			}
+
+			@Override
+			public void modelChanged(SelectionEvent event) {
+				if (event.getType() == SelectionEvent.Type.FOCUSED) {
+					if (Selection.EDITED_GROUP.equals(event.getContextId())) {
+						controller.action(ChangeView.class, SceneView.class);
+					}
 				}
 			}
 		});
