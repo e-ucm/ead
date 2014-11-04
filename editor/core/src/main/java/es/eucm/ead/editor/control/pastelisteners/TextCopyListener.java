423d5c01915ad554ff1c69ea64b3c593c8144b13
gorco <antcal01@ucm.es>
2014-11-04 16:06:11 +0100

    Added TextCopyListener

---
+++ editor/core/src/main/java/es/eucm/ead/editor/control/pastelisteners/TextCopyListener.java
@@ -0,0 +1,74 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.control.pastelisteners;
+
+import com.badlogic.gdx.utils.Array;
+
+import es.eucm.ead.editor.control.Clipboard.CopyListener;
+import es.eucm.ead.editor.control.actions.editor.AddLabel;
+import es.eucm.ead.editor.control.Controller;
+import es.eucm.ead.schema.components.controls.Label;
+
+public class TextCopyListener implements CopyListener<String> {
+
+	private static final int MAX_CHARACTERS = 750;
+
+	private Controller controller;
+
+	public TextCopyListener(Controller controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void cut(String object) {
+	}
+
+	@Override
+	public void paste(String object) {
+		Label label = new Label();
+		if (object.length() > MAX_CHARACTERS) {
+			object = object.substring(0, MAX_CHARACTERS);
+		}
+		label.setText(object);
+		controller.action(AddLabel.class, label);
+	}
+
+	@Override
+	public void paste(Array<String> object) {
+	}
+
+}
