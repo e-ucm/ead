4944c7f7fab938ea26cbe649e0801f707b3f21b5
RotaruDan <drotaru@ucm.es>
2014-11-18 11:04:14 +0100

    Adds Slice and None transitions.

---
+++ engine/core/src/main/java/es/eucm/ead/engine/systems/effects/transitions/None.java
@@ -0,0 +1,76 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.systems.effects.transitions;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import es.eucm.ead.engine.systems.effects.transitions.TransitionManager.Transition;
+
+/**
+ * This transition does nothing.
+ */
+public class None implements Transition {
+
+	private static None instance;
+
+	public static None init() {
+		if (instance == null) {
+			instance = new None();
+		}
+		return instance;
+	}
+
+	public None() {
+	}
+
+	@Override
+	public float getDuration() {
+		return 0;
+	}
+
+	@Override
+	public void render(Batch batch, TextureRegion currScreen,
+			Region currScreenRegion, TextureRegion nextScreen,
+			Region nextScreenRegion, float alpha) {
+	}
+
+	@Override
+	public void end() {
+
+	}
+}
