1dcd2f7a9ef167cc8ca452ebff8d26670204ea19
RotaruDan <drotaru@ucm.es>
2014-11-30 15:09:20 +0100

    Download/unzip and import scene element when an element is clicked in SearchGallery.

---
+++ editor/core/src/main/java/es/eucm/ead/editor/view/listeners/workers/UnzipFileListener.java
@@ -0,0 +1,80 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.view.listeners.workers;
+
+import com.badlogic.gdx.files.FileHandle;
+
+import es.eucm.ead.editor.control.workers.Worker.WorkerListener;
+
+public abstract class UnzipFileListener implements WorkerListener {
+
+	private FileHandle outputFolder;
+
+	public UnzipFileListener(FileHandle outputFolder) {
+		this.outputFolder = outputFolder;
+	}
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void result(Object... results) {
+		if ((Boolean) results[0]) {
+			unzipped();
+		} else {
+			cancelled();
+		}
+	}
+
+	public abstract void unzipped();
+
+	@Override
+	public void error(Throwable ex) {
+		cancelled();
+	}
+
+	@Override
+	public void done() {
+	}
+
+	@Override
+	public void cancelled() {
+		outputFolder.deleteDirectory();
+	}
+
+}
