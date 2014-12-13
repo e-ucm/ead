fdb80e4edf8a7ff0febf979b6ce86bead2fef005
Javier Torrente <jtorrente@e-ucm.es>
2014-12-13 22:38:34 +0100

    Library from game findingajob

---
+++ repo-builder/src/main/java/es/eucm/ead/repobuilder/libs/FindingAJob.java
@@ -0,0 +1,64 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.repobuilder.libs;
+
+/**
+ * Created by Javier Torrente on 30/09/14.
+ */
+public class FindingAJob extends EAd1XLibBuilder {
+	public FindingAJob() {
+		super("findingajob");
+
+		setCommonProperty(AUTHOR_NAME, "CATEDU/Héctor Montoya Camacho");
+		setCommonProperty(AUTHOR_URL,
+				"http://www.catedu.es/webcateduantigua/index.php/descargas/e-adventures");
+		setCommonProperty(TAGS,
+				"eAdventure;eAdventure,CATEDU;CATEDU,Finding a job;Finding a job");
+		setCommonProperty(LIB_NAME, "eadventure-findingajob");
+	}
+
+	@Override
+	protected String getLibraryNameEn() {
+		return "Finding a Job";
+	}
+
+	@Override
+	protected String getLibraryNameEs() {
+		return getLibraryNameEn();
+	}
+
+}
