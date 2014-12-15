f7be83629b3f06facc8270324f7361590fe1da6e
anserran <angelserranolaguna@gmail.com>
2014-12-15 17:27:57 +0100

    Spinner modal

---
+++ editor/mokap/core/src/main/java/es/eucm/ead/editor/view/widgets/modals/ModalContainer.java
@@ -0,0 +1,104 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.view.widgets.modals;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
+import es.eucm.ead.editor.control.Views;
+import es.eucm.ead.editor.view.Modal;
+
+public class ModalContainer extends Container<Actor> implements Modal {
+
+	public ModalContainer(Skin skin, Actor actor) {
+		this(skin.get(ModalContainerStyle.class), actor);
+	}
+
+	public ModalContainer(ModalContainerStyle style, Actor actor) {
+		super(actor);
+		setBackground(style.background);
+		setTouchable(Touchable.enabled);
+	}
+
+	@Override
+	public void show(Views views) {
+		clearActions();
+		getColor().a = 0.0f;
+		addAction(Actions.alpha(1.0f, 0.25f));
+		if (getActor() instanceof Modal) {
+			((Modal) getActor()).show(views);
+		}
+	}
+
+	@Override
+	public void hide(Runnable runnable) {
+		clearActions();
+		if (getActor() instanceof Modal) {
+			addAction(Actions.alpha(0.0f, 0.25f));
+			((Modal) getActor()).hide(runnable);
+		} else {
+			addAction(Actions.sequence(Actions.alpha(0.0f, 0.25f),
+					Actions.run(runnable)));
+		}
+	}
+
+	@Override
+	public float getPrefWidth() {
+		return Gdx.graphics.getWidth();
+	}
+
+	@Override
+	public float getPrefHeight() {
+		return Gdx.graphics.getHeight();
+	}
+
+	@Override
+	public boolean hideAlways() {
+		return false;
+	}
+
+	public static class ModalContainerStyle {
+
+		public Drawable background;
+
+	}
+}
---
+++ editor/mokap/desktop/src/test/java/es/eucm/ead/editor/editorui/widgets/SpinnerModalTest.java
@@ -0,0 +1,104 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.editorui.widgets;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import es.eucm.ead.editor.control.actions.editor.ShowSpinner;
+import es.eucm.ead.editor.editorui.UITest;
+import es.eucm.ead.editor.view.SkinConstants;
+import es.eucm.ead.editor.view.widgets.IconButton;
+import es.eucm.ead.editor.view.widgets.WidgetBuilder;
+import es.eucm.ead.editor.view.widgets.layouts.LinearLayout;
+import es.eucm.ead.editor.view.widgets.modals.SpinnerModal.SpinnerModalListener;
+import es.eucm.ead.engine.I18N;
+
+public class SpinnerModalTest extends UITest implements SpinnerModalListener {
+
+	private Label label;
+
+	@Override
+	protected Actor buildUI(Skin skin, I18N i18n) {
+		LinearLayout container = new LinearLayout(false).background(skin
+				.getDrawable(SkinConstants.DRAWABLE_BLACK_BG));
+		IconButton iconButton = WidgetBuilder.toolbarIcon(SkinConstants.IC_ADD,
+				null);
+		container.add(iconButton);
+		container.add(label = new Label("Not set", skin,
+				SkinConstants.STYLE_TOAST_ACTION));
+		container.addSpace();
+
+		iconButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				int value = 0;
+				try {
+					value = Integer.parseInt(label.getText().toString());
+				} catch (NumberFormatException e) {
+
+				}
+				controller.action(ShowSpinner.class, "Repeat", "times", value,
+						1, 100, SpinnerModalTest.this);
+			}
+		});
+
+		return container;
+	}
+
+	@Override
+	public void value(int value) {
+		label.setText(value + "");
+	}
+
+	@Override
+	public void cancelled() {
+	}
+
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 640;
+		config.height = 360;
+		config.overrideDensity = 160;
+		new LwjglApplication(new SpinnerModalTest(), config);
+	}
+}
