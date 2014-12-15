4cae24667a8a940bbb9ab5b1937a93bc522c4a15
anserran <angelserranolaguna@gmail.com>
2014-12-15 17:18:49 +0100

    States button

---
+++ editor/mokap/desktop/src/test/java/es/eucm/ead/editor/editorui/widgets/StatesButtonTest.java
@@ -0,0 +1,74 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.editor.editorui.widgets;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import es.eucm.ead.editor.editorui.UITest;
+import es.eucm.ead.editor.view.SkinConstants;
+import es.eucm.ead.editor.view.widgets.StatesButton;
+import es.eucm.ead.engine.I18N;
+
+public class StatesButtonTest extends UITest {
+
+	@Override
+	protected Actor buildUI(Skin skin, I18N i18n) {
+
+		Table table = new Table();
+		table.setFillParent(true);
+
+		StatesButton widget = new StatesButton(skin,
+				SkinConstants.STYLE_CONTEXT,
+				SkinConstants.IC_MULTIPLE_SELECTION,
+				SkinConstants.IC_SINGLE_SELECTION, SkinConstants.IC_ADD,
+				SkinConstants.IC_BLUR_LINEAR);
+
+		table.add(widget);
+
+		return table;
+	}
+
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 300;
+		config.height = 100;
+		new LwjglApplication(new StatesButtonTest(), config);
+	}
+}
