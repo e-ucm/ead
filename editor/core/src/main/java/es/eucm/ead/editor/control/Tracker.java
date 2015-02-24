257901650b5ad8187d5998ed8f3073c1415e56d8
anserran <angelserranolaguna@gmail.com>
2015-02-24 16:16:16 +0100

    Minor fix

--- editor/core/src/main/java/es/eucm/ead/editor/control/Tracker.java
+++ editor/core/src/main/java/es/eucm/ead/editor/control/Tracker.java
@@ -76,8 +76,7 @@ public class Tracker implements PreferenceListener {
 		bugrURL = controller.getReleaseInfo().getBugReportURL();
 		if (bugrURL == null) {
 			enabled = false;
-		} else if (!bugrURL.endsWith("/")) {
-			bugrURL += "/";
+		} else {
 			loadClientId();
 		}
 	}
