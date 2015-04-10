5d93548d568817c81a09041c8b895d51a0cda6dd
RotaruDan <drotaru@ucm.es>
2015-04-10 20:28:43 +0200

    Adds BackgroundShadersDemo.

---
+++ engine/desktop/src/test/java/es/eucm/ead/engine/demos/BackgroundShadersDemo.java
@@ -0,0 +1,128 @@
+/**
+ * eAdventure is a research project of the
+ *    e-UCM research group.
+ *
+ *    Copyright 2005-2014 e-UCM research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    e-UCM is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          CL Profesor Jose Garcia Santesmases 9,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.eucm.ead.engine.demos;
+
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import es.eucm.ead.engine.demobuilder.ExecutableDemoBuilder;
+import es.eucm.ead.engine.systems.MainSystem;
+import es.eucm.ead.schema.components.Shader;
+import es.eucm.ead.schema.components.controls.Label;
+import es.eucm.ead.schema.data.Color;
+import es.eucm.ead.schema.data.shape.Rectangle;
+import es.eucm.ead.schema.effects.GoScene;
+import es.eucm.ead.schema.entities.ModelEntity;
+import es.eucm.ead.schema.renderers.ShapeRenderer;
+
+public class BackgroundShadersDemo extends ExecutableDemoBuilder {
+
+	private String lastSceneId;
+
+	public BackgroundShadersDemo() {
+		super("backgroundShaders");
+	}
+
+	@Override
+	public String getName() {
+		return "BackgroundShaders";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Background Shaders displayed for testing";
+	}
+
+	@Override
+	protected void doBuild() {
+		ShaderProgram.pedantic = false;
+		game(800, 600);
+
+		ModelEntity initialScene = scene(1);
+		for (int i = 2; i < 34; ++i) {
+			scene(i);
+		}
+		String lastSceneId = getLastSceneId();
+		if (lastSceneId != null) {
+			entity(initialScene, "back.png", 200, 0).touchBehavior(
+					makeGoScene(lastSceneId, GoScene.Transition.SLIDE_RIGHT,
+							1.0f, true));
+		}
+	}
+
+	private ModelEntity scene(int i) {
+
+		ModelEntity scene = scene(null).getLastEntity();
+
+		ModelEntity background = new ModelEntity();
+
+		ShapeRenderer shapeRenderer = new ShapeRenderer();
+		Rectangle rectangle = new Rectangle();
+		rectangle.setWidth(800);
+		rectangle.setHeight(600);
+		shapeRenderer.setShape(rectangle);
+
+		background.getComponents().add(shapeRenderer);
+
+		Shader shader = new Shader();
+		shader.setUri("test" + i + ".fragment");
+		shader.getUniforms().add(param("time", "$" + MainSystem.TIME));
+		shader.getUniforms().add(
+				param("resolution", "$" + MainSystem.FRAME_WIDTH + ",$"
+						+ MainSystem.FRAME_HEIGHT));
+
+		background.getComponents().add(shader);
+
+		scene.getChildren().add(background);
+
+		ModelEntity label = new ModelEntity();
+		Label labelComponent = new Label();
+		labelComponent.setText("" + i);
+		label.getComponents().add(labelComponent);
+		Color color = new Color();
+		label.setColor(color);
+
+		scene.getChildren().add(label);
+
+		if (lastSceneId != null) {
+			entity(scene, "back.png", 200, 0).touchBehavior(
+					makeGoScene(lastSceneId, GoScene.Transition.SLIDE_RIGHT,
+							1.0f, true));
+		}
+		lastSceneId = getLastSceneId();
+
+		return scene;
+	}
+}
