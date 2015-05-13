622ea8cdf2a87a24c2cc86311c506825d5dbcabc
anserran <angelserranolaguna@gmail.com>
2015-05-13 21:56:58 +0200

    Updated scales

--- editor/mokap/core/src/main/java/es/eucm/ead/editor/control/MokapController.java
+++ editor/mokap/core/src/main/java/es/eucm/ead/editor/control/MokapController.java
@@ -56,7 +56,7 @@ public class MokapController extends Controller {
 
 	private Group loadingImage;
 
-	public static final String[] SCALES = new String[] { "0.8", "1.0", "1.5",
+	public static final String[] SCALES = new String[] { "1.0", "1.5",
 			"2.0", "2.5", "3.0", "4.0" };
 
 	public MokapController(Platform platform, Files files,
